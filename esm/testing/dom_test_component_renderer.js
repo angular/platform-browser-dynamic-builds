639447a223e4474602e3855bffc76eae28571d02
GitHub <noreply@github.com>
2016-06-23 23:52:27 +0000

    3d8eb8c fix(platform-browser/testing): clean up public api for platform-browser/testing (#9519)

--- esm/testing/dom_test_component_renderer.js
+++ esm/testing/dom_test_component_renderer.js
@@ -8,7 +8,6 @@
 import { TestComponentRenderer } from '@angular/compiler/testing';
 import { Inject, Injectable } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
-import { el } from '@angular/platform-browser/testing';
 import { getDOM } from '../platform_browser_private';
 export class DOMTestComponentRenderer extends TestComponentRenderer {
     constructor(_doc /** TODO #9100 */) {
@@ -16,7 +15,7 @@ export class DOMTestComponentRenderer extends TestComponentRenderer {
         this._doc = _doc;
     }
     insertRootElement(rootElId) {
-        let rootEl = el(`<div id="${rootElId}"></div>`);
+        let rootEl = getDOM().firstChild(getDOM().content(getDOM().createTemplate(`<div id="${rootElId}"></div>`)));
         // TODO(juliemr): can/should this be optional?
         let oldRoots = getDOM().querySelectorAll(this._doc, '[id^=root]');
         for (let i = 0; i < oldRoots.length; i++) {
