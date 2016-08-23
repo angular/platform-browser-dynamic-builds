bd12c80af120380ed371901efb691b15e3d6e7fe
Kara <kara@users.noreply.github.com>
2016-08-23 23:28:39 +0000

    5c93a88 fix(core): Share private types through an exported interface. (#11009)

--- private_export_testing.d.ts
+++ private_export_testing.d.ts
@@ -6,9 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import * as testing from './testing/dom_test_component_renderer';
-export declare namespace __platform_browser_dynamic_private_types__ {
-    type DOMTestComponentRenderer = testing.DOMTestComponentRenderer;
-    var DOMTestComponentRenderer: typeof testing.DOMTestComponentRenderer;
+export interface __platform_browser_dynamic_private_types__ {
+    DOMTestComponentRenderer: testing.DOMTestComponentRenderer;
 }
 export declare var __platform_browser_dynamic_private__: {
     DOMTestComponentRenderer: typeof testing.DOMTestComponentRenderer;
