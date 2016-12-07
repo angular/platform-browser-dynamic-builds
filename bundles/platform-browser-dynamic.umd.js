a30bf170c5d0b6d98daf3214dd9b9b22549100bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 00:33:19 +0000

    16efb13 fix: display framework version on bootstrapped component (#13252)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,13 +1,13 @@
 /**
- * @license Angular v2.3.0-rc.0-986abbe
+ * @license Angular v2.3.0-rc.0-16efb13
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('@angular/compiler'), require('@angular/platform-browser')) :
-    typeof define === 'function' && define.amd ? define(['exports', '@angular/core', '@angular/compiler', '@angular/platform-browser'], factory) :
-    (factory((global.ng = global.ng || {}, global.ng.platformBrowserDynamic = global.ng.platformBrowserDynamic || {}),global.ng.core,global.ng.compiler,global.ng.platformBrowser));
-}(this, function (exports,_angular_core,_angular_compiler,_angular_platformBrowser) { 'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/compiler'), require('@angular/core'), require('@angular/platform-browser')) :
+    typeof define === 'function' && define.amd ? define(['exports', '@angular/compiler', '@angular/core', '@angular/platform-browser'], factory) :
+    (factory((global.ng = global.ng || {}, global.ng.platformBrowserDynamic = global.ng.platformBrowserDynamic || {}),global.ng.compiler,global.ng.core,global.ng.platformBrowser));
+}(this, function (exports,_angular_compiler,_angular_core,_angular_platformBrowser) { 'use strict';
 
     var INTERNAL_BROWSER_PLATFORM_PROVIDERS = _angular_platformBrowser.__platform_browser_private__.INTERNAL_BROWSER_PLATFORM_PROVIDERS;
 
@@ -147,6 +147,11 @@
     };
 
     /**
+     * @stable
+     */
+    var VERSION = new _angular_core.Version('2.3.0-rc.0-16efb13');
+
+    /**
      * @experimental
      */
     var RESOURCE_CACHE_PROVIDER = [{ provide: _angular_compiler.ResourceLoader, useClass: CachedResourceLoader }];
@@ -155,14 +160,9 @@
      */
     var platformBrowserDynamic = _angular_core.createPlatformFactory(_angular_compiler.platformCoreDynamic, 'browserDynamic', INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS);
 
-    /**
-     * @stable
-     */
-    var VERSION = new _angular_core.Version('2.3.0-rc.0-986abbe');
-
-    exports.VERSION = VERSION;
     exports.RESOURCE_CACHE_PROVIDER = RESOURCE_CACHE_PROVIDER;
     exports.platformBrowserDynamic = platformBrowserDynamic;
+    exports.VERSION = VERSION;
     exports.__platform_browser_dynamic_private__ = __platform_browser_dynamic_private__;
 
 }));
