82ad4688e0f45ceb6d0b6524138e1e1b621fc32f
Chuck Jazdzewski <chuckj@google.com>
2016-12-20 02:17:07 +0000

    171a9bd feat: update to rxjs@5.0.1 and unpin the rxjs peerDeps via ^5.0.1 (#13572)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.0-6efdf84
+ * @license Angular v4.0.0-beta.0-171a9bd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -149,7 +149,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.4.0-6efdf84');
+    var VERSION = new _angular_core.Version('4.0.0-beta.0-171a9bd');
 
     /**
      * @experimental
