da78f5e0e61ea7953da58e85e9a930de39558702
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 01:43:37 +0000

    9aeb8c5 refactor(test): `<template>`/`<ng-container>`/*-directives

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-f14d549
+ * @license Angular v4.0.0-beta.2-9aeb8c5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -149,7 +149,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-beta.2-f14d549');
+    var VERSION = new _angular_core.Version('4.0.0-beta.2-9aeb8c5');
 
     /**
      * @experimental
