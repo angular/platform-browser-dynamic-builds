d77f9ba8bec2718b9afee87eb3f360c90891584d
Igor Minar <iminar@google.com>
2017-02-22 19:49:56 +0000

    88755b0 build(aio): update dgeni-packages to 0.16.8 (#14597)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-fcc1d17
+ * @license Angular v4.0.0-beta.8-88755b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -116,7 +116,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-beta.8-fcc1d17');
+const VERSION = new Version('4.0.0-beta.8-88755b0');
 
 /**
  * @experimental
