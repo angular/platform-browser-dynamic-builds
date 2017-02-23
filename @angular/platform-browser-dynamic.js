95b619d5ac12f302314da01a340b37aed48b36e4
Igor Minar <iminar@google.com>
2017-02-23 22:22:23 +0000

    4b54c0e refactor(core): enable new animations dsl

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-187f7b6
+ * @license Angular v4.0.0-beta.8-4b54c0e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -118,7 +118,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-beta.8-187f7b6');
+const VERSION = new Version('4.0.0-beta.8-4b54c0e');
 
 /**
  * @experimental
