90e2a847f871518f601da5a0331f5b7a1e9e9a00
Igor Minar <iminar@google.com>
2017-02-24 04:25:02 +0000

    e99d721 feat(compiler): Add a `enableLegacyTemplate` option to support `<template>`

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-beta.8-ee747f7');
+    var VERSION = new _core.Version('4.0.0-beta.8-e99d721');
 
     /**
      * @experimental
