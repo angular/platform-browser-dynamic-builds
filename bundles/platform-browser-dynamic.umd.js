634f139557bb258a1d1203aff7520237b87e3d00
Igor Minar <iminar@google.com>
2017-02-24 20:13:09 +0000

    5094aef fix(compiler-cli): fix flat module import on Windows (#14712)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-beta.8-93ddd38');
+    var VERSION = new _core.Version('4.0.0-beta.8-5094aef');
 
     /**
      * @experimental
