5ebd3a44a53df3bd1ff89cc7871a1da777aec64b
GitHub <noreply@github.com>
2017-02-26 04:39:31 +0000

    213e210 docs: changelog - add anchor to the FESM section

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.1-c868457');
+    var VERSION = new _core.Version('4.0.0-rc.1-213e210');
 
     /**
      * @experimental
