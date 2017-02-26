98483444cd82a1ef55fddf34c982ca8b4cfcd169
GitHub <noreply@github.com>
2017-02-26 04:36:46 +0000

    c868457 docs: improve Flat ES Modules docs

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-f32f4de
+ * @license Angular v4.0.0-rc.1-c868457
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -118,7 +118,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-f32f4de');
+const VERSION = new Version('4.0.0-rc.1-c868457');
 
 /**
  * @experimental
--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.1-f32f4de');
+    var VERSION = new _core.Version('4.0.0-rc.1-c868457');
 
     /**
      * @experimental
