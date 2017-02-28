cf7e768466b545369429e7196a6b2fa000d233ac
Igor Minar <iminar@google.com>
2017-02-28 00:38:42 +0000

    e58cb7b docs: fix animations module instructions in the changelog (#14729)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-5caab71
+ * @license Angular v4.0.0-rc.1-e58cb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -118,7 +118,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-5caab71');
+const VERSION = new Version('4.0.0-rc.1-e58cb7b');
 
 /**
  * @experimental
