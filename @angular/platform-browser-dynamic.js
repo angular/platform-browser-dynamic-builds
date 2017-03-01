566e2834bec49b6ea241ded762b0779b48d63207
Igor Minar <iminar@google.com>
2017-03-01 19:37:23 +0000

    47bdc2b fix(platform-server): don't setup Testability and TestabilityRegistry on the server (#14510)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-49b462e
+ * @license Angular v4.0.0-rc.1-47bdc2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -118,7 +118,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.1-49b462e');
+const VERSION = new Version('4.0.0-rc.1-47bdc2b');
 
 /**
  * @experimental
--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.1-49b462e');
+    var VERSION = new _core.Version('4.0.0-rc.1-47bdc2b');
 
     /**
      * @experimental
