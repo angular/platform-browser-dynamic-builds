98df3b51d1619e83be37f0a61169e543da887b4c
Igor Minar <iminar@google.com>
2017-03-01 20:37:37 +0000

    79fc1e3 fix(compiler): apply element bindings before host bindings (#14823)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.1-9402df9');
+    var VERSION = new _core.Version('4.0.0-rc.1-79fc1e3');
 
     /**
      * @experimental
