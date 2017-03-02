d21b3ca7668ce690c2253edfdbebc8277d372685
Igor Minar <iminar@google.com>
2017-03-02 08:18:01 +0000

    e6e8123 build: update to yarn 0.21.3 (#14805)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.2-49aa508');
+    var VERSION = new _core.Version('4.0.0-rc.2-e6e8123');
 
     /**
      * @experimental
