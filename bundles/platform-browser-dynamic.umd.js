9cdac22c01b7154ce36ef61798b7ce8ee1253246
Igor Minar <igor@angularjs.org>
2017-03-02 07:43:06 +0000

    49aa508 docs: add 2.4.9 release notes

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -198,7 +198,7 @@
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.2-180b705');
+    var VERSION = new _core.Version('4.0.0-rc.2-49aa508');
 
     /**
      * @experimental
