cfa2deac1c6f12f51319c1247e8408232c5c0e45
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 22:48:24 +0000

    5df998d fix(router): do not finish bootstrap until all the routes are resolved (#14762)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -7,7 +7,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 /**
- * @license Angular v4.0.0-rc.2-1cff125
+ * @license Angular v4.0.0-rc.2-5df998d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -121,7 +121,7 @@ var CachedResourceLoader = function (_ResourceLoader2) {
  */
 
 
-var VERSION = new Version('4.0.0-rc.2-1cff125');
+var VERSION = new Version('4.0.0-rc.2-5df998d');
 
 /**
  * @experimental
--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-1cff125
+ * @license Angular v4.0.0-rc.2-5df998d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.2-1cff125');
+const VERSION = new Version('4.0.0-rc.2-5df998d');
 
 /**
  * @experimental
