08fb2b3566c64b95bc2839714142dfe401ec05cb
Igor Minar <iminar@google.com>
2017-03-07 07:06:24 +0000

    ebd4463 fix(aio): reimplement the top menu dropping into the side bar

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-b7e76cc
+ * @license Angular v4.0.0-rc.2-ebd4463
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.2-b7e76cc');
+const VERSION = new Version('4.0.0-rc.2-ebd4463');
 
 /**
  * @experimental
