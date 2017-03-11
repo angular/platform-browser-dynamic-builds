5646de938c2c755b2085450bd9b367b97cddaf54
Chuck Jazdzewski <chuckj@google.com>
2017-03-11 03:39:35 +0000

    6c8638c feat(core): allow to provide multiple default testing modules (#15054)

--- @angular/platform-browser-dynamic/testing.es5.js
+++ @angular/platform-browser-dynamic/testing.es5.js
@@ -1,56 +1,47 @@
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+/**
+ * @license Angular v4.0.0-rc.3-6c8638c
+ * (c) 2010-2017 Google, Inc. https://angular.io/
+ * License: MIT
+ */
 import { platformCoreDynamicTesting } from '@angular/compiler/testing';
 import { createPlatformFactory, NgModule, Injectable, Inject } from '@angular/core';
 import { TestComponentRenderer } from '@angular/core/testing';
 import { ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from '@angular/platform-browser-dynamic';
 import { BrowserTestingModule } from '@angular/platform-browser/testing';
 import { ɵgetDOM, DOCUMENT } from '@angular/platform-browser';
-
 /**
  * A DOM based implementation of the TestComponentRenderer.
  */
-
-var DOMTestComponentRenderer = function (_TestComponentRendere) {
-    _inherits(DOMTestComponentRenderer, _TestComponentRendere);
-
+var DOMTestComponentRenderer = (function (_super) {
+    __extends(DOMTestComponentRenderer, _super);
     function DOMTestComponentRenderer(_doc /** TODO #9100 */) {
-        _classCallCheck(this, DOMTestComponentRenderer);
-
-        var _this = _possibleConstructorReturn(this, (DOMTestComponentRenderer.__proto__ || Object.getPrototypeOf(DOMTestComponentRenderer)).call(this));
-
+        var _this = _super.call(this) || this;
         _this._doc = _doc; /** TODO #9100 */
         return _this;
     }
-
-    _createClass(DOMTestComponentRenderer, [{
-        key: 'insertRootElement',
-        value: function insertRootElement(rootElId) {
-            var rootEl = ɵgetDOM().firstChild(ɵgetDOM().content(ɵgetDOM().createTemplate('<div id="' + rootElId + '"></div>')));
-            // TODO(juliemr): can/should this be optional?
-            var oldRoots = ɵgetDOM().querySelectorAll(this._doc, '[id^=root]');
-            for (var i = 0; i < oldRoots.length; i++) {
-                ɵgetDOM().remove(oldRoots[i]);
-            }
-            ɵgetDOM().appendChild(this._doc.body, rootEl);
+    DOMTestComponentRenderer.prototype.insertRootElement = function (rootElId) {
+        var rootEl = ɵgetDOM().firstChild(ɵgetDOM().content(ɵgetDOM().createTemplate("<div id=\"" + rootElId + "\"></div>")));
+        // TODO(juliemr): can/should this be optional?
+        var oldRoots = ɵgetDOM().querySelectorAll(this._doc, '[id^=root]');
+        for (var i = 0; i < oldRoots.length; i++) {
+            ɵgetDOM().remove(oldRoots[i]);
         }
-    }]);
-
+        ɵgetDOM().appendChild(this._doc.body, rootEl);
+    };
     return DOMTestComponentRenderer;
-}(TestComponentRenderer);
-
-DOMTestComponentRenderer.decorators = [{ type: Injectable }];
+}(TestComponentRenderer));
+DOMTestComponentRenderer.decorators = [
+    { type: Injectable },
+];
 /** @nocollapse */
-DOMTestComponentRenderer.ctorParameters = function () {
-    return [{ type: undefined, decorators: [{ type: Inject, args: [DOCUMENT] }] }];
-};
-
+DOMTestComponentRenderer.ctorParameters = function () { return [
+    { type: undefined, decorators: [{ type: Inject, args: [DOCUMENT,] },] },
+]; };
 /**
  * @stable
  */
@@ -60,18 +51,19 @@ var platformBrowserDynamicTesting = createPlatformFactory(platformCoreDynamicTes
  *
  * @stable
  */
-
-var BrowserDynamicTestingModule = function BrowserDynamicTestingModule() {
-    _classCallCheck(this, BrowserDynamicTestingModule);
-};
-
-BrowserDynamicTestingModule.decorators = [{ type: NgModule, args: [{
-        exports: [BrowserTestingModule],
-        providers: [{ provide: TestComponentRenderer, useClass: DOMTestComponentRenderer }]
-    }] }];
+var BrowserDynamicTestingModule = (function () {
+    function BrowserDynamicTestingModule() {
+    }
+    return BrowserDynamicTestingModule;
+}());
+BrowserDynamicTestingModule.decorators = [
+    { type: NgModule, args: [{
+                exports: [BrowserTestingModule],
+                providers: [
+                    { provide: TestComponentRenderer, useClass: DOMTestComponentRenderer },
+                ]
+            },] },
+];
 /** @nocollapse */
-BrowserDynamicTestingModule.ctorParameters = function () {
-    return [];
-};
-
+BrowserDynamicTestingModule.ctorParameters = function () { return []; };
 export { platformBrowserDynamicTesting, BrowserDynamicTestingModule, DOMTestComponentRenderer as ɵDOMTestComponentRenderer };
--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,135 +1,74 @@
+/**
+ * @license Angular v4.0.0-rc.3-6c8638c
+ * (c) 2010-2017 Google, Inc. https://angular.io/
+ * License: MIT
+ */
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define('@angular/platform-browser-dynamic', ['exports', '@angular/compiler', '@angular/core', '@angular/common', '@angular/platform-browser'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(exports, require('@angular/compiler'), require('@angular/core'), require('@angular/common'), require('@angular/platform-browser'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(mod.exports, global.ng.compiler, global.ng.core, global.ng.common, global.ng.platformBrowser);
-        global.ng = global.ng || {};
-        global.ng.platformBrowserDynamic = mod.exports;
-    }
-})(this, function (exports, _compiler, _core, _common, _platformBrowser) {
-    'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/compiler'), require('@angular/core'), require('@angular/common'), require('@angular/platform-browser')) :
+    typeof define === 'function' && define.amd ? define(['exports', '@angular/compiler', '@angular/core', '@angular/common', '@angular/platform-browser'], factory) :
+    (factory((global.ng = global.ng || {}, global.ng.platformBrowserDynamic = global.ng.platformBrowserDynamic || {}),global.ng.compiler,global.ng.core,global.ng.common,global.ng.platformBrowser));
+}(this, function (exports,_angular_compiler,_angular_core,_angular_common,_angular_platformBrowser) { 'use strict';
 
-    Object.defineProperty(exports, "__esModule", {
-        value: true
-    });
-    exports.ɵResourceLoaderImpl = exports.ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS = exports.VERSION = exports.platformBrowserDynamic = exports.RESOURCE_CACHE_PROVIDER = undefined;
-
-    function _classCallCheck(instance, Constructor) {
-        if (!(instance instanceof Constructor)) {
-            throw new TypeError("Cannot call a class as a function");
-        }
-    }
-
-    var _createClass = function () {
-        function defineProperties(target, props) {
-            for (var i = 0; i < props.length; i++) {
-                var descriptor = props[i];
-                descriptor.enumerable = descriptor.enumerable || false;
-                descriptor.configurable = true;
-                if ("value" in descriptor) descriptor.writable = true;
-                Object.defineProperty(target, descriptor.key, descriptor);
-            }
-        }
-
-        return function (Constructor, protoProps, staticProps) {
-            if (protoProps) defineProperties(Constructor.prototype, protoProps);
-            if (staticProps) defineProperties(Constructor, staticProps);
-            return Constructor;
-        };
-    }();
-
-    function _possibleConstructorReturn(self, call) {
-        if (!self) {
-            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-        }
-
-        return call && (typeof call === "object" || typeof call === "function") ? call : self;
-    }
-
-    function _inherits(subClass, superClass) {
-        if (typeof superClass !== "function" && superClass !== null) {
-            throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-        }
-
-        subClass.prototype = Object.create(superClass && superClass.prototype, {
-            constructor: {
-                value: subClass,
-                enumerable: false,
-                writable: true,
-                configurable: true
-            }
-        });
-        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-    }
-
-    var ResourceLoaderImpl = function (_ResourceLoader) {
-        _inherits(ResourceLoaderImpl, _ResourceLoader);
-
-        function ResourceLoaderImpl() {
-            _classCallCheck(this, ResourceLoaderImpl);
-
-            return _possibleConstructorReturn(this, (ResourceLoaderImpl.__proto__ || Object.getPrototypeOf(ResourceLoaderImpl)).apply(this, arguments));
-        }
-
-        _createClass(ResourceLoaderImpl, [{
-            key: 'get',
-            value: function get(url) {
-                var resolve = void 0;
-                var reject = void 0;
-                var promise = new Promise(function (res, rej) {
-                    resolve = res;
-                    reject = rej;
-                });
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', url, true);
-                xhr.responseType = 'text';
-                xhr.onload = function () {
-                    // responseText is the old-school way of retrieving response (supported by IE8 & 9)
-                    // response/responseType properties were introduced in ResourceLoader Level2 spec (supported
-                    // by IE10)
-                    var response = xhr.response || xhr.responseText;
-                    // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
-                    var status = xhr.status === 1223 ? 204 : xhr.status;
-                    // fix status code when it is 0 (0 status is undocumented).
-                    // Occurs when accessing file resources or on Android 4.1 stock browser
-                    // while retrieving files from application cache.
-                    if (status === 0) {
-                        status = response ? 200 : 0;
-                    }
-                    if (200 <= status && status <= 300) {
-                        resolve(response);
-                    } else {
-                        reject('Failed to load ' + url);
-                    }
-                };
-                xhr.onerror = function () {
-                    reject('Failed to load ' + url);
-                };
-                xhr.send();
-                return promise;
-            }
-        }]);
-
-        return ResourceLoaderImpl;
-    }(_compiler.ResourceLoader);
-
-    ResourceLoaderImpl.decorators = [{ type: _core.Injectable }];
-    /** @nocollapse */
-    ResourceLoaderImpl.ctorParameters = function () {
-        return [];
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-
-    var INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS = [_platformBrowser.ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS, {
-        provide: _core.COMPILER_OPTIONS,
-        useValue: { providers: [{ provide: _compiler.ResourceLoader, useClass: ResourceLoaderImpl }] },
-        multi: true
-    }, { provide: _core.PLATFORM_ID, useValue: _common.ɵPLATFORM_BROWSER_ID }];
-
+    var ResourceLoaderImpl = (function (_super) {
+        __extends(ResourceLoaderImpl, _super);
+        function ResourceLoaderImpl() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        ResourceLoaderImpl.prototype.get = function (url) {
+            var resolve;
+            var reject;
+            var promise = new Promise(function (res, rej) {
+                resolve = res;
+                reject = rej;
+            });
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.responseType = 'text';
+            xhr.onload = function () {
+                // responseText is the old-school way of retrieving response (supported by IE8 & 9)
+                // response/responseType properties were introduced in ResourceLoader Level2 spec (supported
+                // by IE10)
+                var response = xhr.response || xhr.responseText;
+                // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
+                var status = xhr.status === 1223 ? 204 : xhr.status;
+                // fix status code when it is 0 (0 status is undocumented).
+                // Occurs when accessing file resources or on Android 4.1 stock browser
+                // while retrieving files from application cache.
+                if (status === 0) {
+                    status = response ? 200 : 0;
+                }
+                if (200 <= status && status <= 300) {
+                    resolve(response);
+                }
+                else {
+                    reject("Failed to load " + url);
+                }
+            };
+            xhr.onerror = function () { reject("Failed to load " + url); };
+            xhr.send();
+            return promise;
+        };
+        return ResourceLoaderImpl;
+    }(_angular_compiler.ResourceLoader));
+    ResourceLoaderImpl.decorators = [
+        { type: _angular_core.Injectable },
+    ];
+    /** @nocollapse */
+    ResourceLoaderImpl.ctorParameters = function () { return []; };
+    var INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS = [
+        _angular_platformBrowser.ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS,
+        {
+            provide: _angular_core.COMPILER_OPTIONS,
+            useValue: { providers: [{ provide: _angular_compiler.ResourceLoader, useClass: ResourceLoaderImpl }] },
+            multi: true
+        },
+        { provide: _angular_core.PLATFORM_ID, useValue: _angular_common.ɵPLATFORM_BROWSER_ID },
+    ];
     /**
      * An implementation of ResourceLoader that uses a template cache to avoid doing an actual
      * ResourceLoader.
@@ -137,53 +76,43 @@
      * The template cache needs to be built and loaded into window.$templateCache
      * via a separate mechanism.
      */
-
-    var CachedResourceLoader = function (_ResourceLoader2) {
-        _inherits(CachedResourceLoader, _ResourceLoader2);
-
+    var CachedResourceLoader = (function (_super) {
+        __extends(CachedResourceLoader, _super);
         function CachedResourceLoader() {
-            _classCallCheck(this, CachedResourceLoader);
-
-            var _this2 = _possibleConstructorReturn(this, (CachedResourceLoader.__proto__ || Object.getPrototypeOf(CachedResourceLoader)).call(this));
-
-            _this2._cache = _core.ɵglobal.$templateCache;
-            if (_this2._cache == null) {
+            var _this = _super.call(this) || this;
+            _this._cache = _angular_core.ɵglobal.$templateCache;
+            if (_this._cache == null) {
                 throw new Error('CachedResourceLoader: Template cache was not found in $templateCache.');
             }
-            return _this2;
+            return _this;
         }
-
-        _createClass(CachedResourceLoader, [{
-            key: 'get',
-            value: function get(url) {
-                if (this._cache.hasOwnProperty(url)) {
-                    return Promise.resolve(this._cache[url]);
-                } else {
-                    return Promise.reject('CachedResourceLoader: Did not find cached template for ' + url);
-                }
+        CachedResourceLoader.prototype.get = function (url) {
+            if (this._cache.hasOwnProperty(url)) {
+                return Promise.resolve(this._cache[url]);
             }
-        }]);
-
+            else {
+                return Promise.reject('CachedResourceLoader: Did not find cached template for ' + url);
+            }
+        };
         return CachedResourceLoader;
-    }(_compiler.ResourceLoader);
-
+    }(_angular_compiler.ResourceLoader));
     /**
      * @stable
      */
-    var VERSION = new _core.Version('4.0.0-rc.2-5ad5301');
-
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-6c8638c');
     /**
      * @experimental
      */
-    var RESOURCE_CACHE_PROVIDER = [{ provide: _compiler.ResourceLoader, useClass: CachedResourceLoader }];
+    var RESOURCE_CACHE_PROVIDER = [{ provide: _angular_compiler.ResourceLoader, useClass: CachedResourceLoader }];
     /**
      * @stable
      */
-    var platformBrowserDynamic = (0, _core.createPlatformFactory)(_compiler.platformCoreDynamic, 'browserDynamic', INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS);
+    var platformBrowserDynamic = _angular_core.createPlatformFactory(_angular_compiler.platformCoreDynamic, 'browserDynamic', INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS);
 
     exports.RESOURCE_CACHE_PROVIDER = RESOURCE_CACHE_PROVIDER;
     exports.platformBrowserDynamic = platformBrowserDynamic;
     exports.VERSION = VERSION;
     exports.ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS = INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS;
     exports.ɵResourceLoaderImpl = ResourceLoaderImpl;
-});
+
+}));
