5868e6f9243be5e505edda844948aab59c5e96fb
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 23:05:36 +0000

    e6c81d2 Revert "refactor(platform-browser): move platform-browser/animations to animations/browser (#15043)"

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-75147ff
+ * @license Angular v4.0.0-rc.3-e6c81d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-75147ff');
+const VERSION = new Version('4.0.0-rc.3-e6c81d2');
 
 /**
  * @experimental
--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-75147ff
+ * @license Angular v4.0.0-rc.3-e6c81d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -99,7 +99,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.3-75147ff');
+    var VERSION = new _angular_core.Version('4.0.0-rc.3-e6c81d2');
     /**
      * @experimental
      */
