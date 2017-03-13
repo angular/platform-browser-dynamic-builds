08e317faade47a3894193c8ece0faff1887482bd
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 17:05:35 +0000

    ff71eff refactor(core): use flags in `Renderer2.setStyle` instead of booleans (#15045)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-fa1920a
+ * @license Angular v4.0.0-rc.3-ff71eff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-fa1920a');
+const VERSION = new Version('4.0.0-rc.3-ff71eff');
 
 /**
  * @experimental
