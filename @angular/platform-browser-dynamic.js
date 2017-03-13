746df327c48873d17629436069aba49ef570a5fc
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 16:53:41 +0000

    71cd295 ci: add wardbell and gkalpak to aio approvers (#15030)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6c8638c
+ * @license Angular v4.0.0-rc.3-71cd295
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-6c8638c');
+const VERSION = new Version('4.0.0-rc.3-71cd295');
 
 /**
  * @experimental
