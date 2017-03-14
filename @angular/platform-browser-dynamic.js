9cd33fd641ca92144b019f4c4b83987a56afc362
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 23:44:19 +0000

    13686bb fix: element injector vs module injector (#15044)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f093501
+ * @license Angular v4.0.0-rc.3-13686bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-f093501');
+const VERSION = new Version('4.0.0-rc.3-13686bb');
 
 /**
  * @experimental
