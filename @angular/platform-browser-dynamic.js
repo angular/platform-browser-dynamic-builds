40bdd07352ab5f7bbac047c3521fe4fb9765024d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 12:47:13 +0000

    d4205bb fix(aio): ignore private exports

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-1c1085b
+ * @license Angular v4.0.0-rc.3-d4205bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-1c1085b');
+const VERSION = new Version('4.0.0-rc.3-d4205bb');
 
 /**
  * @experimental
