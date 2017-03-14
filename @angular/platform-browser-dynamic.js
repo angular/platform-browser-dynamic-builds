56f3689b303aaa86ba1448a619d6adf8593e451a
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:26:17 +0000

    1c1085b feat(aio): add google analytics (#15081)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-eedca09
+ * @license Angular v4.0.0-rc.3-1c1085b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-eedca09');
+const VERSION = new Version('4.0.0-rc.3-1c1085b');
 
 /**
  * @experimental
