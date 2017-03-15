3455969201649d215b5f09fcc760d2de36d3f0d2
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 16:47:02 +0000

    36ce0af fix(animations): support multiple state names per state() call (#15147)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-b00fe20
+ * @license Angular v4.0.0-rc.3-36ce0af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-b00fe20');
+const VERSION = new Version('4.0.0-rc.3-36ce0af');
 
 /**
  * @experimental
