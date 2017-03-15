ad82517cfd5d2fbf749b0d8e012d53ce399dd75f
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 04:04:23 +0000

    4f7d62a fix(platform-server): fix an exception when HostListener('window:scroll') is used on the server (#15019)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-c10c060
+ * @license Angular v4.0.0-rc.3-4f7d62a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-c10c060');
+const VERSION = new Version('4.0.0-rc.3-4f7d62a');
 
 /**
  * @experimental
