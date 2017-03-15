97407d53205bd8a2da24f54778516cb4b786ff7f
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 03:59:06 +0000

    5fe2d8f fix(core): update peer dep on zone.js to ^0.8.2 (#15078)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-5c34066
+ * @license Angular v4.0.0-rc.3-5fe2d8f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-5c34066');
+const VERSION = new Version('4.0.0-rc.3-5fe2d8f');
 
 /**
  * @experimental
