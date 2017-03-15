d15414cf42f570fd49509c2763d49657991edeb2
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 23:41:01 +0000

    6772c91 fix(aio): scroll to hash fragment element on URL change

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-bf98d9d
+ * @license Angular v4.0.0-rc.3-6772c91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-bf98d9d');
+const VERSION = new Version('4.0.0-rc.3-6772c91');
 
 /**
  * @experimental
