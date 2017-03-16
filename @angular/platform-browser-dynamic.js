44267dcf625abdeefae1274ce8567207873ac74e
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:11 +0000

    6e98757 fix(core): use presence of .subscribe to detect observables rather then Symbol.observable (#15171)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3131581
+ * @license Angular v4.0.0-rc.3-6e98757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -88,7 +88,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.3-3131581');
+const VERSION = new Version('4.0.0-rc.3-6e98757');
 
 /**
  * @experimental
