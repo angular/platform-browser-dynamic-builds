16f41695be97d9bc95b0b6b104ae811705722a1f
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 02:51:03 +0000

    6b57928 docs: add changelog for 2.4.10

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.9-b0ae464
+ * @license Angular v2.4.10-6b57928
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -149,7 +149,7 @@
     /**
      * @stable
      */
-    var VERSION = new _angular_core.Version('2.4.9-b0ae464');
+    var VERSION = new _angular_core.Version('2.4.10-6b57928');
 
     /**
      * @experimental
