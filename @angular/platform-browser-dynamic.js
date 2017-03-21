ba3964cf1d7abb3328acc390ae21f3532a325a32
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 11:50:53 +0000

    1b9c809 refactor(aio): removed dead code

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-b7ba331
+ * @license Angular v4.0.0-rc.5-1b9c809
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-rc.5-b7ba331');
+const VERSION = new Version('4.0.0-rc.5-1b9c809');
 
 /**
  * @license
