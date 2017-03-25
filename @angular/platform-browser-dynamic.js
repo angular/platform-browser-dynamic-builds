dee6812208b51e5dd82a9f0322c53a327b17a0b5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 21:50:31 +0000

    1616cae build(aio): add renderExamples processor

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-c9e51d9');
+const VERSION = new Version('4.0.0-1616cae');
 
 /**
  * @license
