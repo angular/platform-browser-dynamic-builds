470fd5a91cd0cf6c98c0a4256e047e678c67dacf
Victor Berchet <victor@suumit.com>
2017-03-27 19:13:27 +0000

    15662ef build(aio): update project config for @angular/cli v1.0.0

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-816b389
+ * @license Angular v4.0.0-15662ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-816b389');
+const VERSION = new Version('4.0.0-15662ef');
 
 /**
  * @license
