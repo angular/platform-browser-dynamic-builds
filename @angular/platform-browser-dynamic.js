1bcbb018e84b1c4570fd5e99fc80ec4013a146ec
Victor Berchet <victor@suumit.com>
2017-03-29 17:37:01 +0000

    331b9f6 fix(language-service): don't require `reflect-metadata` module to be provided (#15569)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d58a242
+ * @license Angular v4.0.0-331b9f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.0.0-d58a242');
+const VERSION = new Version('4.0.0-331b9f6');
 
 /**
  * @license
