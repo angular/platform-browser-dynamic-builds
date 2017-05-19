c27b18d8431d821d7b855530cab9f6c73bec0ba3
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 21:04:24 +0000

    f1a9e3c feat(animations): introduce routeable animation support

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-b10029c
+ * @license Angular v4.2.0-beta.1-f1a9e3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -142,7 +142,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-beta.1-b10029c');
+var VERSION = new Version('4.2.0-beta.1-f1a9e3c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
