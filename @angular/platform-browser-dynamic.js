ce435be6f4105fedc262b1c28cff847df04c1700
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 18:09:34 +0000

    a80ac0a fix(core): make decorators closure safe (#16905)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-5af143e
+ * @license Angular v4.2.0-rc.0-a80ac0a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.0-5af143e');
+const VERSION = new Version('4.2.0-rc.0-a80ac0a');
 
 /**
  * @license
