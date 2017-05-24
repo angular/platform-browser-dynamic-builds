4d11efb37f020b124dfc9ee22fee8e690ffad168
Chuck Jazdzewski <chuckj@google.com>
2017-05-24 20:30:18 +0000

    966eb2f aio: add h1 title to floating table of contents (#16959)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-b0c5018
+ * @license Angular v4.2.0-rc.0-966eb2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.0-b0c5018');
+const VERSION = new Version('4.2.0-rc.0-966eb2f');
 
 /**
  * @license
