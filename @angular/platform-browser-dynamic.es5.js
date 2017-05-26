1dc4c26a73a2c7e3ae6dfb5ef3521e7486a086fc
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 01:19:13 +0000

    5aa53d7 refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-3ab86bd
+ * @license Angular v4.2.0-rc.0-5aa53d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.0-3ab86bd');
+var VERSION = new Version('4.2.0-rc.0-5aa53d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
