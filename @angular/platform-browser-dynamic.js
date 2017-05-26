a6cfe7fe838cbc95c292f5408f7125b3e522b4a8
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 20:58:40 +0000

    d108479 fix(animations): use a lightweight renderer for non-animation components (#17003)

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-cd5bc64
+ * @license Angular v4.2.0-rc.0-d108479
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.0-cd5bc64');
+const VERSION = new Version('4.2.0-rc.0-d108479');
 
 /**
  * @license
