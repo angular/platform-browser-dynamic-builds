b8fd17766270350df512fe7c3ef01735eaf613c3
Victor Berchet <victor@suumit.com>
2017-05-30 20:38:24 +0000

    e513808 build: update router package.json placeholder (#17060)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-8ffa483
+ * @license Angular v4.2.0-rc.1-e513808
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-8ffa483');
+var VERSION = new Version('4.2.0-rc.1-e513808');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
