7c922091493310b1b831998866ae62149b03adda
Victor Berchet <victor@suumit.com>
2017-05-30 18:05:45 +0000

    535d9da fix(common): always use 'other' case for locales with no plural rules (#16990)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-18bf772
+ * @license Angular v4.2.0-rc.1-535d9da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-18bf772');
+var VERSION = new Version('4.2.0-rc.1-535d9da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
