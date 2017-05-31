396290342185d65747b0d3b67c322fec6376392f
GitHub <noreply@github.com>
2017-05-31 11:05:43 +0000

    1f9a3dd fix(aio): fix typo in web worker check (#17133)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-11505fa');
+var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
