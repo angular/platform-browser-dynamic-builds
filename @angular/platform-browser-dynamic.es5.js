131c072fbf706c84aca8518095e428123bcf616e
GitHub <noreply@github.com>
2017-06-01 20:44:05 +0000

    598fdad docs: update the release schedule (#17162)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-ad6a57e
+ * @license Angular v4.2.0-rc.1-598fdad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.1-ad6a57e');
+var VERSION = new Version('4.2.0-rc.1-598fdad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-ad6a57e
+ * @license Angular v4.2.0-rc.1-598fdad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.1-ad6a57e');
+const VERSION = new Version('4.2.0-rc.1-598fdad');
 
 /**
  * @license
