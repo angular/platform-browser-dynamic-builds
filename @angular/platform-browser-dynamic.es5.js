358d9c406ccdbb45d9335267958f768c9ea944a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-03 00:22:59 +0000

    819514a fix(animations): ensure web-animations understands a numeric CSS perspective value

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-b55adee
+ * @license Angular v4.2.0-rc.2-819514a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-b55adee');
+var VERSION = new Version('4.2.0-rc.2-819514a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
