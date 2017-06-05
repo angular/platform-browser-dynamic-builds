b9334a8c3f2f624781627ed35f56900b3a9cdfe4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:38:19 +0000

    47c2a2e ci: switch CircleCI to 2.0

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-47c2a2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-7c78282');
+var VERSION = new Version('4.2.0-rc.2-47c2a2e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
