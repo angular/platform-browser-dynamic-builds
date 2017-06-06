027f4db83ce646e1eb7a93b7e6d10d091c9e968c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:58:58 +0000

    3a121a6 build(aio): migrate contribute page to html

--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-c06f4fc
+ * @license Angular v4.2.0-rc.2-3a121a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.2.0-rc.2-c06f4fc');
+const VERSION = new Version('4.2.0-rc.2-3a121a6');
 
 /**
  * @license
