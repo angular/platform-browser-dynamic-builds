7ab3c5271176de19f8267d76de33530b8bfef98f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 14:14:04 +0000

    95f1ea2 fix(aio): topnav toolbar position styles

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-784347f
+ * @license Angular v4.2.0-rc.2-95f1ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-784347f');
+var VERSION = new Version('4.2.0-rc.2-95f1ea2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
