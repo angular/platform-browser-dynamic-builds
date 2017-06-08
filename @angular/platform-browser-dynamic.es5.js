d9afa6f3335b5bc6878171dac220f7fa78f7da49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 22:55:42 +0000

    cf0a9e0 docs: add changelog for 4.2.0

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-576d6d8
+ * @license Angular v4.2.0-cf0a9e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-576d6d8');
+var VERSION = new Version('4.2.0-cf0a9e0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
