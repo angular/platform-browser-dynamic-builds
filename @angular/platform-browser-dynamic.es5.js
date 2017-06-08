ed993783a83f1b83960e413e4a9df6f52e64c642
Igor Minar <iminar@google.com>
2017-06-08 19:47:03 +0000

    75a311f fix(aio): style and fix hover color for features cta button (#17342)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-4d2ee51
+ * @license Angular v4.2.0-rc.2-75a311f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-rc.2-4d2ee51');
+var VERSION = new Version('4.2.0-rc.2-75a311f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
