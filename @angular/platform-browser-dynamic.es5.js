5df1c27b8da39722cf7a0298ff75a3501baa28c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-09 23:16:17 +0000

    90b0713 refactor(compiler): don’t write summaries for jit by default

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-d56b7ed
+ * @license Angular v4.2.0-90b0713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-d56b7ed');
+var VERSION = new Version('4.2.0-90b0713');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
