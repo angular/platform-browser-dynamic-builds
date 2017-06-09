e2e33768ef71643899a3e467f945740cf2e33c0e
Igor Minar <iminar@google.com>
2017-06-09 21:54:01 +0000

    0020dad docs(aio): update examples to 4.2 (#17367)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-65d49d5
+ * @license Angular v4.2.0-0020dad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.0-65d49d5');
+var VERSION = new Version('4.2.0-0020dad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
