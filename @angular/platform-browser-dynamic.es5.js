711886f08079046f121763d331121b039878a9b3
GitHub <noreply@github.com>
2017-06-10 01:52:13 +0000

    c8c0cee build(bazel): fix the typescript path in the BUILD file (#17316)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-afbc2b0
+ * @license Angular v4.2.1-c8c0cee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-afbc2b0');
+var VERSION = new Version('4.2.1-c8c0cee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
