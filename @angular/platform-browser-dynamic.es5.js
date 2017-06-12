9c5103d55ac16bbaa8346a3f8d1cea202b899f32
Igor Minar <igor@angularjs.org>
2017-06-12 23:50:15 +0000

    5e79449 build(aio): make deploy-to-firebase.sh executable

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-0268818
+ * @license Angular v4.2.2-5e79449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.1-0268818');
+var VERSION = new Version('4.2.2-5e79449');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
