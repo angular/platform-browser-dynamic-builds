2cd36f65000ff95ba1ab4c0a989ab5ce3a01e05a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 17:47:33 +0000

    98308cd docs(aio): update typescript for `examples/webpack` to same as cli

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-052331f
+ * @license Angular v4.2.2-98308cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-052331f');
+var VERSION = new Version('4.2.2-98308cd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
