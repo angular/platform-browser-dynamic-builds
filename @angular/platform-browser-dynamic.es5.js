e94e85daf17975fd95f0238982c702e965647b2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:08:19 +0000

    93d1b4e refactor(compiler): remove duplicate code

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-fa81c8e
+ * @license Angular v4.2.2-93d1b4e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fa81c8e');
+var VERSION = new Version('4.2.2-93d1b4e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
