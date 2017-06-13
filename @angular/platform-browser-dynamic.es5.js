006d7e5039187406ceb2ade60c3b3784d5fc71e7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 10:51:29 +0000

    7caa0a8 fix(aio): show search results when search box gets focus

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-fc774a1
+ * @license Angular v4.2.2-7caa0a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-fc774a1');
+var VERSION = new Version('4.2.2-7caa0a8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
