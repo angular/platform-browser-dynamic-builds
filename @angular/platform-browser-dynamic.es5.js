434822f7dcd6235a77e426de3f5084e7358b5d03
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 15:04:12 +0000

    2bdf2fe test(aio): add async beforeEach to prevent Chrome disconnects

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-607fb1f
+ * @license Angular v4.2.2-2bdf2fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-607fb1f');
+var VERSION = new Version('4.2.2-2bdf2fe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
