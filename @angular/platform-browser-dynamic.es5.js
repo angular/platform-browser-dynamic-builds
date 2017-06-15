c9420db440eaebc7cc7a2697ea936442747db516
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:08:50 +0000

    ba0f6de build(aio): extra redirect rule

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-01173b9
+ * @license Angular v4.2.2-ba0f6de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-01173b9');
+var VERSION = new Version('4.2.2-ba0f6de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-01173b9
+ * @license Angular v4.2.2-ba0f6de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-01173b9
+ * @license Angular v4.2.2-ba0f6de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -165,7 +165,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.2.2-01173b9');
+var VERSION = new _angular_core.Version('4.2.2-ba0f6de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
