4c66c04df11a8de6c173358376d1663c389436e6
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-15 21:47:34 +0000

    96aa3bb docs(aio): update about page

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-64fa100
+ * @license Angular v4.2.2-96aa3bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-64fa100');
+var VERSION = new Version('4.2.2-96aa3bb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
