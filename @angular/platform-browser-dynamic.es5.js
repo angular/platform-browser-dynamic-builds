f4d2626560ebd0a287562976de7a5ee69f355ae3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:05:44 +0000

    01173b9 fix(aio): do not log messages in production

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-06a0cf2
+ * @license Angular v4.2.2-01173b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-06a0cf2');
+var VERSION = new Version('4.2.2-01173b9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
