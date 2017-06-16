70d8993e4324ca416c85801ce2a2a70a8ce6eb2e
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:23:54 +0000

    dbc6a4c build(aio): do not fail if `check-env` for the main angular project fails

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-b192dd5');
+var VERSION = new Version('4.2.2-dbc6a4c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
