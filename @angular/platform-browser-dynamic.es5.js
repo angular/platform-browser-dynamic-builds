b73ccef716daf10c6c2537fbe22f5b16b7f00904
Hans <hans@hansl.ca>
2017-06-16 19:34:45 +0000

    adc1b12 fix(aio): correctly redirect `/docs/ts/latest` and `/styleguide`

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-f58211d
+ * @license Angular v4.2.2-adc1b12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-f58211d');
+var VERSION = new Version('4.2.2-adc1b12');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
