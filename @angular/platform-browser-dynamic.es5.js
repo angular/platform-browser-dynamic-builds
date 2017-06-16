d17c59f8bb13efe00a577323b329dc2c561b6b0f
Hans Larsen <hans@hansl.ca>
2017-06-16 17:02:04 +0000

    b8c39cd fix(forms): temp roll back breaking change with min/max directives

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-9c7a84d
+ * @license Angular v4.2.2-b8c39cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-9c7a84d');
+var VERSION = new Version('4.2.2-b8c39cd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-9c7a84d
+ * @license Angular v4.2.2-b8c39cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('4.2.2-9c7a84d');
+const VERSION = new Version('4.2.2-b8c39cd');
 
 /**
  * @license
