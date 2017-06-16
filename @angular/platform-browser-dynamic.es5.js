6eb18d90b310781c97d1505ad5d7ccd384538427
Hans <hans@hansl.ca>
2017-06-16 19:35:14 +0000

    fe126cb fix(aio): remove unused icon reference and file

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-adc1b12
+ * @license Angular v4.2.2-fe126cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-adc1b12');
+var VERSION = new Version('4.2.2-fe126cb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
