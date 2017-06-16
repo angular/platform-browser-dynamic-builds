58a92cea87b5f3c6586cf084adb4cb00b78c8924
Hans <hans@hansl.ca>
2017-06-16 19:34:43 +0000

    f58211d fix(aio): improve no-javascript screen

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-f58211d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-9315ab8');
+var VERSION = new Version('4.2.2-f58211d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
