55c1fee8419a083e48eed83db19bdeb9da45691c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:21:40 +0000

    2cc931e fix(aio): add missing redirect rule for `/styleguide`

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-ce00fa3
+ * @license Angular v4.2.2-2cc931e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-ce00fa3');
+var VERSION = new Version('4.2.2-2cc931e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
