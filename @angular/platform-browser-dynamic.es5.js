683cb75c6fc451b6f6e0eafb17e9989b88bdc7d3
Hans <hans@hansl.ca>
2017-06-20 20:19:53 +0000

    c59c390 fix: argument destructuring sometimes breaks strictNullChecks

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-f194f18
+ * @license Angular v4.2.2-c59c390
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.2-f194f18');
+var VERSION = new Version('4.2.2-c59c390');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
