4f48f3d96f500b3488e27c1a917a845518afe16c
Hans Larsen <hans@hansl.ca>
2017-06-21 00:15:46 +0000

    572885d docs(aio): fix numbered list in testing.md

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-3390648
+ * @license Angular v4.2.3-572885d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.3-3390648');
+var VERSION = new Version('4.2.3-572885d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
