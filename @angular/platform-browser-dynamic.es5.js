ecfd81868aaaa6a5f109c0f511a311497aa45d39
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 17:49:20 +0000

    9344c86 docs: fix “under to hood” typo in changelog.md

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.4-353de51');
+var VERSION = new Version('4.2.4-9344c86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
