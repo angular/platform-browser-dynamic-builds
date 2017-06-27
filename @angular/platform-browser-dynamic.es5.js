c3876351aa43f2bdbd8ff1cac4a850b4ea73e907
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 20:54:37 +0000

    7205d7a fix(aio): build `scripts-js` before creating a new docker image for the preview server

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-ffbdf74
+ * @license Angular v4.2.4-7205d7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.4-ffbdf74');
+var VERSION = new Version('4.2.4-7205d7a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
