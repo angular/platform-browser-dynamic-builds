ead0a19358282bb5e183975ebccba4976783bc8a
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:07:08 +0000

    10bfa8a ci(aio): avoid printing too large PWA report (and restore `print-logs.sh`)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-c460757
+ * @license Angular v4.2.5-10bfa8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-c460757');
+var VERSION = new Version('4.2.5-10bfa8a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
