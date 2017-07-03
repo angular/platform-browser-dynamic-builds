539a17e12077d4c634090f61a55f0e21d07058a5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:43:12 +0000

    f54a901 docs(aio): fix visual-studio-2015 typo

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8a1a989
+ * @license Angular v4.3.0-beta.1-f54a901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-8a1a989
+ * @license Angular v4.3.0-beta.1-f54a901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -165,7 +165,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-8a1a989');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-f54a901');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
