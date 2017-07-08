365157e279ba52d10cf706fd1b0d98dddef5746f
Jason Aden <jasonaden@google.com>
2017-07-08 04:26:01 +0000

    3e61bf7 Revert "fix(upgrade): fix transclusion on upgraded components (#17971)"

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-670f2e5
+ * @license Angular v4.2.5-3e61bf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.5-670f2e5');
+var VERSION = new Version('4.2.5-3e61bf7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
