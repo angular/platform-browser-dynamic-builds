ff137b11c4f5efafb882552c4a950312560622ea
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:53:57 +0000

    ab13f1f build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-b69f0fa');
+var VERSION = new Version('4.2.6-ab13f1f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
