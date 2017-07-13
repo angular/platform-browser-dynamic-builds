85eb52776dfe787de36b6ed814d8e33816979d83
Igor Minar <igor@angularjs.org>
2017-07-13 20:09:39 +0000

    e8b98a7 docs(aio): add Dart as a language option (#18080)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-afd5d72
+ * @license Angular v4.2.6-e8b98a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.2.6-afd5d72');
+var VERSION = new Version('4.2.6-e8b98a7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
