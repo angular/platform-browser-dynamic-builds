4daa73cdace6b4d39579200d0508410c811e09e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:34:58 +0000

    256bc8a docs(http): Make name of injected HttpTestingController consistent

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.3.0-59c23c7');
+var VERSION = new Version('4.3.0-256bc8a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
