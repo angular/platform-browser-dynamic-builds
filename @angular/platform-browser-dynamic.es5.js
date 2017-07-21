487e0be403b2d6e0f9a67b313b6c83f2eeaa7818
Miško Hevery <misko@hevery.com>
2017-07-21 22:56:36 +0000

    b7a6f52 perf: latest tsickle to tree shake: abstract class methods & interfaces (#18236)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-abee785
+ * @license Angular v5.0.0-beta.0-b7a6f52
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-abee785');
+var VERSION = new Version('5.0.0-beta.0-b7a6f52');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
