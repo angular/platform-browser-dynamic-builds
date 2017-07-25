1194213b18646e668cc87fe0ce81ed2ddbb63813
Miško Hevery <misko@hevery.com>
2017-07-25 23:11:41 +0000

    b582e2b docs(aio): update examples to 4.3

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.0-91ab39c');
+var VERSION = new Version('5.0.0-beta.0-b582e2b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
