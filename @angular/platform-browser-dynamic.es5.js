451f43fb18da1426b32b33b104a1047f18e3ff0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 22:26:20 +0000

    0fb7484 refactor(aio): move content-specific images to `content/images/`

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-5808153
+ * @license Angular v5.0.0-beta.1-0fb7484
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-5808153');
+var VERSION = new Version('5.0.0-beta.1-0fb7484');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
