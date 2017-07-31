92f4d967ef9d07fc2293fdafabe408df92a31c4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 23:14:59 +0000

    9479a10 build: enable TSLint on the packages folder

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-e64b54b
+ * @license Angular v5.0.0-beta.1-9479a10
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-e64b54b');
+var VERSION = new Version('5.0.0-beta.1-9479a10');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
