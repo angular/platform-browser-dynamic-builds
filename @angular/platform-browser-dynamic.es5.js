59506337d5229359d6b4c50eedf4fa2712827445
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:47:12 +0000

    381471d fix(compiler): fix for element needing implicit parent placed in top-level ng-container

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-ebef5e6');
+var VERSION = new Version('5.0.0-beta.1-381471d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
