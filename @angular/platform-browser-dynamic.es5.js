47f977b57fc47f21e58de51fcb7d329aaa3878da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-01 18:05:38 +0000

    49cd851 feat(router): add events tracking activation of individual routes

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-82b067f
+ * @license Angular v5.0.0-beta.1-49cd851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.1-82b067f');
+var VERSION = new Version('5.0.0-beta.1-49cd851');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-82b067f
+ * @license Angular v5.0.0-beta.1-49cd851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.1-82b067f');
+const VERSION = new Version('5.0.0-beta.1-49cd851');
 
 /**
  * @license
