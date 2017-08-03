46ac699ea7e90250eadb4fb1b6e06a5a29c1cd37
Victor Berchet <victor@suumit.com>
2017-08-03 00:44:18 +0000

    1640d2a refactor(platform-browser): compiler platform-browser packages cleanly (#18464)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-5f501c7');
+var VERSION = new Version('5.0.0-beta.2-1640d2a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -165,7 +165,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-5f501c7');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-1640d2a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
