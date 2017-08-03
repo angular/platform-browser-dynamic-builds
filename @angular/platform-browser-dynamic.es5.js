93aaacd6b97f5a4cd27140d71061077339708f06
Victor Berchet <victor@suumit.com>
2017-08-03 00:19:59 +0000

    6a88659 test(upgrade): fix an IE9 timer issue in downgrade module tests (#18482)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-44ae6e9
+ * @license Angular v5.0.0-beta.2-6a88659
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-44ae6e9');
+var VERSION = new Version('5.0.0-beta.2-6a88659');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
