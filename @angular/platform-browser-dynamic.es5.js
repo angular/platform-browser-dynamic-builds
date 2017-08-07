f084bdc306dd9f46538654da0df04d48d5879423
Victor Berchet <victor@suumit.com>
2017-08-07 19:28:02 +0000

    05472cb fix(animations): support persisting dynamic styles within animation states (#18468)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-c0c03dc
+ * @license Angular v5.0.0-beta.2-05472cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-c0c03dc');
+var VERSION = new Version('5.0.0-beta.2-05472cb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
