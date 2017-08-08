f1a4727f10741482be496fa22efa99be5e0e1eb2
Victor Berchet <victor@suumit.com>
2017-08-08 20:03:35 +0000

    6f2038c fix(compiler-cli): fix and re-enble expression lowering (#18570)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-f0a5501
+ * @license Angular v5.0.0-beta.2-6f2038c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-f0a5501');
+var VERSION = new Version('5.0.0-beta.2-6f2038c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
