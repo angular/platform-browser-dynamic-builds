b10b6f5580359615456d47e33bcf300572b6479e
Victor Berchet <victor@suumit.com>
2017-08-09 21:49:44 +0000

    b14250b test(aio): fix the `deploy-to-firebase` tests

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-2f9d8ff
+ * @license Angular v5.0.0-beta.2-b14250b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-2f9d8ff');
+var VERSION = new Version('5.0.0-beta.2-b14250b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
