67501062c33b65796ad833cd5f25991ca4830b74
Victor Berchet <victor@suumit.com>
2017-08-09 21:45:04 +0000

    2f9d8ff test(animations): disable buggy test in Chrome 39 (#18483)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-e54bd59
+ * @license Angular v5.0.0-beta.2-2f9d8ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-e54bd59');
+var VERSION = new Version('5.0.0-beta.2-2f9d8ff');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
