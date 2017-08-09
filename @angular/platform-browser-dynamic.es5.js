c5281236047e8840f7f152c0fe2c1ffafac551e5
Victor Berchet <victor@suumit.com>
2017-08-09 21:40:53 +0000

    e54bd59 fix(core): forbid destroyed views to be inserted or moved in VC (#18568)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-1e18331
+ * @license Angular v5.0.0-beta.2-e54bd59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.2-1e18331');
+var VERSION = new Version('5.0.0-beta.2-e54bd59');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
