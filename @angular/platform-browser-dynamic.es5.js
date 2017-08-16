25e34fc24c3065dbaa1fde2886108914cad94fc6
Hans <hans@hansl.ca>
2017-08-16 00:29:41 +0000

    c65f18a docs(forms): fix reactive-forms guide typo

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-697c6ed
+ * @license Angular v5.0.0-beta.3-c65f18a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-697c6ed');
+var VERSION = new Version('5.0.0-beta.3-c65f18a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
