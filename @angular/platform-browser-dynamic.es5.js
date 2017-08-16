c38448b61acfc25544077da61340ae6124cb31c5
Hans <hans@hansl.ca>
2017-08-16 02:36:18 +0000

    32ff21c fix(forms): re-assigning options should not clear select

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-c65f18a
+ * @license Angular v5.0.0-beta.3-32ff21c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-c65f18a');
+var VERSION = new Version('5.0.0-beta.3-32ff21c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
