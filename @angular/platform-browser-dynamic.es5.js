d04ed114a638a5836cd95dba951d5550634549b2
Hans Larsen <hans@hansl.ca>
2017-08-16 20:57:53 +0000

    9aa0521 docs: add changelog for 5.0.0-beta.4

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-bc22ff1
+ * @license Angular v5.0.0-beta.4-9aa0521
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-bc22ff1');
+var VERSION = new Version('5.0.0-beta.4-9aa0521');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
