033db2d0b2b1802e44652eea89987aa704818211
Hans <hans@hansl.ca>
2017-08-16 18:54:01 +0000

    bc22ff1 fix(language-service): remove tsickle dependency

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-75d484e
+ * @license Angular v5.0.0-beta.3-bc22ff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.3-75d484e');
+var VERSION = new Version('5.0.0-beta.3-bc22ff1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
