68ab08ad870865869cb71c853d120ddd8909b86f
Hans <hans@hansl.ca>
2017-08-16 22:53:38 +0000

    43226cb feat(compiler): use typescript for resolving resource paths

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-9aa0521
+ * @license Angular v5.0.0-beta.4-43226cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-9aa0521');
+var VERSION = new Version('5.0.0-beta.4-43226cb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/platform-browser-dynamic.js
+++ @angular/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-9aa0521
+ * @license Angular v5.0.0-beta.4-43226cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -129,7 +129,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @stable
  */
-const VERSION = new Version('5.0.0-beta.4-9aa0521');
+const VERSION = new Version('5.0.0-beta.4-43226cb');
 
 /**
  * @license
