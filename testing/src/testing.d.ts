af026bb008127c7ee9ad304329da784b472c61b5
Miško Hevery <misko@hevery.com>
2017-08-18 00:15:42 +0000

    83713dd feat(common): add an empty DeprecatedI18NPipesModule module (#18737)

---
+++ testing/src/testing.d.ts
@@ -0,0 +1,13 @@
+import { PlatformRef, StaticProvider } from '@angular/core';
+export * from './private_export_testing';
+/**
+ * @stable
+ */
+export declare const platformBrowserDynamicTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+/**
+ * NgModule for testing.
+ *
+ * @stable
+ */
+export declare class BrowserDynamicTestingModule {
+}
