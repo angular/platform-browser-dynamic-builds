82c8a1a4f44142346ba412bc1d1261fd901c03cd
Miško Hevery <misko@hevery.com>
2017-08-21 23:26:39 +0000

    7ce9e06 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-7ce9e06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -435,7 +435,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-55d151a');
+var VERSION = new Version('5.0.0-beta.4-7ce9e06');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
