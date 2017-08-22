8697e7d11f08598d9e1f1ec548726286a0654de1
Miško Hevery <misko@hevery.com>
2017-08-22 01:52:44 +0000

    ec56760 refactor(common): remove deprecated `NgFor` (#18758)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-7ce9e06
+ * @license Angular v5.0.0-beta.4-ec56760
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.4-7ce9e06
+ * @license Angular v5.0.0-beta.4-ec56760
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -467,7 +467,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.4-7ce9e06');
+var VERSION = new _angular_core.Version('5.0.0-beta.4-ec56760');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
