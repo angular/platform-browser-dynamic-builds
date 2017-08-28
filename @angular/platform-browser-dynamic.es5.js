ef419e38b5ede0de5d5317a81d52ff1c7756f7cd
Miško Hevery <misko@hevery.com>
2017-08-28 23:49:48 +0000

    2e714f9 release: cut the 5.0.0-beta.5 release

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-655b495
+ * @license Angular v5.0.0-beta.5-2e714f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-655b495');
+var VERSION = new Version('5.0.0-beta.5-2e714f9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
