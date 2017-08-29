4a30167119b8d2f60628fdca045dcc1a8ac05232
Victor Berchet <victor@suumit.com>
2017-08-29 19:25:22 +0000

    90bd984 refactor(common): simplify closure-locale by removing imports (#18926)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-4878936
+ * @license Angular v5.0.0-beta.5-90bd984
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-4878936');
+var VERSION = new Version('5.0.0-beta.5-90bd984');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
