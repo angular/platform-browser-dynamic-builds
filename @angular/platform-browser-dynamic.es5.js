36fc365aca20b16df76f5860c4cfb525f8eecd21
Jason Aden <jasonaden@google.com>
2017-08-30 02:21:05 +0000

    4ec5e28 fix(common): use correct group separator for currency pipe (#18932)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-452a7ae
+ * @license Angular v5.0.0-beta.5-4ec5e28
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-452a7ae');
+var VERSION = new Version('5.0.0-beta.5-4ec5e28');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
