0cde2f18a99305d15d4977b63c3c4beb08dbcb75
GitHub <noreply@github.com>
2017-08-31 01:02:20 +0000

    043f104 fix(compiler): normalize the locale name (#18963)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-4ec5e28
+ * @license Angular v5.0.0-beta.5-043f104
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-4ec5e28');
+var VERSION = new Version('5.0.0-beta.5-043f104');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
