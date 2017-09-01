dd9304acc99e884e777a5a223e3b734ec94e71ce
Miško Hevery <misko@hevery.com>
2017-09-01 19:10:22 +0000

    450a13d fix(upgrade): deprecate the dynamic version of `ngUpgrade`

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-56238fe
+ * @license Angular v5.0.0-beta.5-450a13d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -596,7 +596,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-56238fe');
+var VERSION = new Version('5.0.0-beta.5-450a13d');
 
 /**
  * @fileoverview added by tsickle
