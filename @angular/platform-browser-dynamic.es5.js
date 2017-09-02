aeeaa1f55e4d8b78e37d89db9607b1e0cc5eb183
Misko Hevery <misko@hevery.com>
2017-09-02 05:48:14 +0000

    aeb98db docs: add changelog for 4.4.0-RC.0

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.0-RC.0-8036d05
+ * @license Angular v4.4.0-RC.0-aeb98db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.0-RC.0-8036d05');
+var VERSION = new Version('4.4.0-RC.0-aeb98db');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
