0ad85af55c33b71d15776b66d8704c49668ca628
Miško Hevery <misko@hevery.com>
2017-09-06 03:35:36 +0000

    66f0ab0 fix(router): adjust ChildActivation events to only fire when the child is actually changing (#19043)

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-fa6b802
+ * @license Angular v5.0.0-beta.6-66f0ab0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -596,7 +596,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-fa6b802');
+var VERSION = new Version('5.0.0-beta.6-66f0ab0');
 
 /**
  * @fileoverview added by tsickle
