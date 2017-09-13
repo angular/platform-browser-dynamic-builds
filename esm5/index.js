f72bd0f24836f75f1957a43c321ce6b8d03a6996
Matias Niemelä <matias@yearofmoo.com>
2017-09-13 17:53:16 +0000

    bf94f87 refactor(compiler): use new ngc for i18n (#19095)

--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-c8f742e
+ * @license Angular v5.0.0-beta.6-bf94f87
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -669,7 +669,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-c8f742e');
+var VERSION = new Version('5.0.0-beta.6-bf94f87');
 
 /**
  * @fileoverview added by tsickle
