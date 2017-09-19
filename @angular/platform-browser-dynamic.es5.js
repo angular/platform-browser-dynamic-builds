201d09540e7285441170884898bfc3529bbe39be
Igor Minar <igor@angularjs.org>
2017-09-19 22:59:53 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.2-0371538');
+var VERSION = new Version('4.4.3-e2e8ba6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
