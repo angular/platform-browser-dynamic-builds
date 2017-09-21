b7ddcdc0f4c6eeb208bade7c84dc1ffdeceeeddf
Igor Minar <igor@angularjs.org>
2017-09-21 18:48:32 +0000

    04bc525 docs(aio): tidy up tooling documentation (#18151)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-e7eb0b8
+ * @license Angular v4.4.3-04bc525
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.3-e7eb0b8');
+var VERSION = new Version('4.4.3-04bc525');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
