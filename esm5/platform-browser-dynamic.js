64cb9b12ac6f5d579b0d58d1ab607205a2fd6cf2
Igor Minar <igor@angularjs.org>
2017-09-21 00:13:43 +0000

    4586fcc refactor(upgrade): use correct property name (#19180)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -661,7 +661,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-9e84719');
+var VERSION = new Version('5.0.0-beta.7-4586fcc');
 
 /**
  * @fileoverview added by tsickle
