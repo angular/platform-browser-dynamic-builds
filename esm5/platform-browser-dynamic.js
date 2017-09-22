3ee2a4f899486447ea075f2addc90049823c6bbb
Igor Minar <igor@angularjs.org>
2017-09-22 01:21:03 +0000

    9bbf009 build(aio): change examples to CLI (#19248)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-6e1896b
+ * @license Angular v5.0.0-beta.7-9bbf009
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -661,7 +661,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-6e1896b');
+var VERSION = new Version('5.0.0-beta.7-9bbf009');
 
 /**
  * @fileoverview added by tsickle
