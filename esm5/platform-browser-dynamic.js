44f78d963e46994c13f603579b65c5c764df2e51
Victor Berchet <victor@suumit.com>
2017-09-25 22:00:25 +0000

    ffceae0 refactor(aio): remove unnecessary `--` from yarn commands

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-bb1665c
+ * @license Angular v5.0.0-beta.7-ffceae0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -661,7 +661,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-bb1665c');
+var VERSION = new Version('5.0.0-beta.7-ffceae0');
 
 /**
  * @fileoverview added by tsickle
