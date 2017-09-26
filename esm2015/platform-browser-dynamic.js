c70169a86accd07c84f284952c0854373ae791b0
Victor Berchet <victor@suumit.com>
2017-09-26 20:54:39 +0000

    a75040d refactor(compiler): bump metadata version to 4 (#19338)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-86ffacf
+ * @license Angular v5.0.0-beta.7-a75040d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-86ffacf');
+const VERSION = new Version('5.0.0-beta.7-a75040d');
 
 /**
  * @fileoverview added by tsickle
