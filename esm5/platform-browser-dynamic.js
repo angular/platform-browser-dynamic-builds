42bc562978f506781be90894f05df8b59e055621
Victor Berchet <victor@suumit.com>
2017-09-26 22:21:41 +0000

    b21a1d1 fix: create proper externs so that closure does not clobber e.g. `ng` for internal variables (#19423)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-dfb8d21
+ * @license Angular v5.0.0-beta.7-b21a1d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-dfb8d21');
+var VERSION = new Version('5.0.0-beta.7-b21a1d1');
 
 /**
  * @fileoverview added by tsickle
