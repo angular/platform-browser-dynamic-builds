a46beb7e7ee91da431abafe3194d4af9c9d9abdb
Victor Berchet <victor@suumit.com>
2017-09-27 21:02:06 +0000

    f7c7038 docs(aio): update metadata documentation to include expression lowering (#19203)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-627f048
+ * @license Angular v5.0.0-beta.7-f7c7038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-627f048');
+var VERSION = new Version('5.0.0-beta.7-f7c7038');
 
 /**
  * @fileoverview added by tsickle
