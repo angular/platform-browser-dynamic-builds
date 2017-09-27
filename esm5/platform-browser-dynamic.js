6c3732dbe68565e0f34813a04a88c7478b56f5f5
Victor Berchet <victor@suumit.com>
2017-09-27 17:31:48 +0000

    627f048 fix(compiler): implement i18n with new compiler

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8a0e458
+ * @license Angular v5.0.0-beta.7-627f048
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-8a0e458');
+var VERSION = new Version('5.0.0-beta.7-627f048');
 
 /**
  * @fileoverview added by tsickle
