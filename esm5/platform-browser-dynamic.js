b9804f982e1406f6cd9108bd26f8f8ea4c0005a6
Victor Berchet <victor@suumit.com>
2017-09-28 16:52:04 +0000

    c1b029a fix(language-service): do not report errors for `OpaqueToken` (#19427)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-82e4923
+ * @license Angular v5.0.0-beta.7-c1b029a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-82e4923');
+var VERSION = new Version('5.0.0-beta.7-c1b029a');
 
 /**
  * @fileoverview added by tsickle
