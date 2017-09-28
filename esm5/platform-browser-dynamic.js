27e55732931f7d7003b2399763a9d9368f707e4c
Victor Berchet <victor@suumit.com>
2017-09-28 00:23:45 +0000

    8dbe2af test: increase polyfills size upper limit in CLI integration tests

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-334caf8
+ * @license Angular v5.0.0-beta.7-8dbe2af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-334caf8');
+var VERSION = new Version('5.0.0-beta.7-8dbe2af');
 
 /**
  * @fileoverview added by tsickle
