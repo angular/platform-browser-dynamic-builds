c4ef2f57886e2aebe2c5c5773ae41c94270608a9
Victor Berchet <victor@suumit.com>
2017-09-28 18:26:55 +0000

    a9d32a3 fix(router): resolve and guards should be able to reject with null and undefined (#19418)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b3db3f8
+ * @license Angular v5.0.0-beta.7-a9d32a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-b3db3f8');
+var VERSION = new Version('5.0.0-beta.7-a9d32a3');
 
 /**
  * @fileoverview added by tsickle
