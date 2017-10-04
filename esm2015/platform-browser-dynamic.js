683328b0310e3805b2215a8a704d1f1d52c8a704
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 23:40:57 +0000

    01f7112 fix(compiler): don’t use `ng://` in AOT source maps, and never point to the original source file

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-c80e02b
+ * @license Angular v5.0.0-rc.0-01f7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-c80e02b');
+const VERSION = new Version('5.0.0-rc.0-01f7112');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-c80e02b
+ * @license Angular v5.0.0-rc.0-01f7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-c80e02b');
+var VERSION = new Version('5.0.0-rc.0-01f7112');
 
 /**
  * @fileoverview added by tsickle
