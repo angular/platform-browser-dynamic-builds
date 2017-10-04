1e9165e0ebde38f625da9bbd54ac563f2e4b299f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:19:27 +0000

    95f3b1d fix(compiler): disallow references for select and index evaluation

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-6b96b06
+ * @license Angular v4.4.4-95f3b1d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.4-6b96b06');
+var VERSION = new Version('4.4.4-95f3b1d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
