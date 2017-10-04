9de36656a43096393c111ae2f5a3e993a3e7627c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:27:41 +0000

    c80e02b docs(aio): document Custom Async validators

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-fbc9537
+ * @license Angular v5.0.0-rc.0-c80e02b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-fbc9537');
+var VERSION = new Version('5.0.0-rc.0-c80e02b');
 
 /**
  * @fileoverview added by tsickle
