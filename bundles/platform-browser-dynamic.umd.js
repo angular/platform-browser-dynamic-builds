d53ea13a0176db7d28d7576eff97f6eaf039c0f1
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 00:04:55 +0000

    a22121d perf(compiler): skip type check and emit in bazel in some cases. (#19646)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-3acf9c7
+ * @license Angular v5.0.0-rc.1-a22121d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.1-3acf9c7
+ * @license Angular v5.0.0-rc.1-a22121d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-3acf9c7');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-a22121d');
 
 /**
  * @fileoverview added by tsickle
