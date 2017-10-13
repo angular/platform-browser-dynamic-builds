116e3f2ea169e2439f5e99d0774c2e29bdab38dc
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 17:05:37 +0000

    f983a6c fix(animations): properly support boolean-based transitions and state changes (#19672)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-18f1b01
+ * @license Angular v4.4.5-f983a6c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-18f1b01');
+var VERSION = new Version('4.4.5-f983a6c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
