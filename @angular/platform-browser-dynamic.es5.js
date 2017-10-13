7c2e5c6ea02d42c63e8a0ede6503615b5e48fabf
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:59:49 +0000

    18f1b01 build(aio): consider `devDependencies` when overwriting dependencies of local Angular packages (#19687)

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.5-591dcc2');
+var VERSION = new Version('4.4.5-18f1b01');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
