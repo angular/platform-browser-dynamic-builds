dd45d970350998895c307e16c7f6d4115106769b
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 04:17:10 +0000

    5a9ed2d fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.2-01e4aa5');
+var VERSION = new _angular_core.Version('5.0.0-rc.2-5a9ed2d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-01e4aa5
+ * @license Angular v5.0.0-rc.2-5a9ed2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-01e4aa5');
+var VERSION = new Version('5.0.0-rc.2-5a9ed2d');
 
 /**
  * @fileoverview added by tsickle
