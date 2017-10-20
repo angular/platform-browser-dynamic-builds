98b00fb4dccb81d63236758d158ae5784ae5ec3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:02:51 +0000

    441e01c fix(aio): simplify GaService to avoid e2e test failures

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-fc86352
+ * @license Angular v5.0.0-rc.3-441e01c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-fc86352
+ * @license Angular v5.0.0-rc.3-441e01c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-fc86352');
+var VERSION = new _angular_core.Version('5.0.0-rc.3-441e01c');
 
 /**
  * @fileoverview added by tsickle
