1e25b1f2651b53ca21f0446928755cc3140662cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:41:08 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-820bb7b');
+const VERSION = new Version('5.0.0-rc.3-910735d');
 
 /**
  * @fileoverview added by tsickle
