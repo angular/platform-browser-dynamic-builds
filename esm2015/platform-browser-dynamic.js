d6902a7c7382e5542bc3753d099afbcb8c1924e2
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 23:58:05 +0000

    6bed189 build(service-worker): set skipTemplateCodegen for build (#19875)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-fc0b1d5
+ * @license Angular v5.0.0-rc.3-6bed189
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-fc0b1d5');
+const VERSION = new Version('5.0.0-rc.3-6bed189');
 
 /**
  * @fileoverview added by tsickle
