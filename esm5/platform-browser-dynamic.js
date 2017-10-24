2bb4e1d520fdfb99dffad6d9263cbba90edc3f93
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 23:42:42 +0000

    56b18ff fix(compiler-cli): produce correct paths for windows output (#19915)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-7bfeac7
+ * @license Angular v5.0.0-rc.5-56b18ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-7bfeac7');
+var VERSION = new Version('5.0.0-rc.5-56b18ff');
 
 /**
  * @fileoverview added by tsickle
