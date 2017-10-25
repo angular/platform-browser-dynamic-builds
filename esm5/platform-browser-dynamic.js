2087e2458cefbded2aa6d22ef8d1eb9270c55c4c
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 22:09:07 +0000

    47bc6f1 docs: add changelog for 5.0.0-rc.6

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-680bcf7
+ * @license Angular v5.0.0-rc.6-47bc6f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-680bcf7');
+var VERSION = new Version('5.0.0-rc.6-47bc6f1');
 
 /**
  * @fileoverview added by tsickle
