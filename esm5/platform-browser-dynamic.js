1523d8bbdc8940994349a896982a2aca4b75b954
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 22:42:55 +0000

    14016c7 fix(service-worker): fix improper call of Observable.merge (#19962)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-47caebf
+ * @license Angular v5.0.0-rc.6-14016c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.6-47caebf');
+var VERSION = new Version('5.0.0-rc.6-14016c7');
 
 /**
  * @fileoverview added by tsickle
