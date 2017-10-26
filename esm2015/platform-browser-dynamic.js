612903abcadd992909b93183ce073a2ebc71bb49
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 20:43:35 +0000

    47caebf fix(service-worker): don't block initialization on registration (#19936)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-47bc6f1
+ * @license Angular v5.0.0-rc.6-47caebf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.6-47bc6f1');
+const VERSION = new Version('5.0.0-rc.6-47caebf');
 
 /**
  * @fileoverview added by tsickle
