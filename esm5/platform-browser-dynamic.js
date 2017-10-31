1bf32b67584105a8b0c26bbc4c3ad43302afeb4f
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:07:38 +0000

    3bdbb18 build: update rollup lint rule from bad merge (#20047)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-13f8648
+ * @license Angular v5.0.0-rc.8-3bdbb18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-13f8648');
+var VERSION = new Version('5.0.0-rc.8-3bdbb18');
 
 /**
  * @fileoverview added by tsickle
