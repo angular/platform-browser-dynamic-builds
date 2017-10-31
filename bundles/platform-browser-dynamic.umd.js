227895842e7198b7b9e61f06b0130296a4ad6c39
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 05:42:39 +0000

    005a78b refactor: allow compilation with TypeScript 2.5 (#19966)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-7553ce9
+ * @license Angular v5.0.0-rc.9-005a78b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.9-7553ce9
+ * @license Angular v5.0.0-rc.9-005a78b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.9-7553ce9');
+var VERSION = new _angular_core.Version('5.0.0-rc.9-005a78b');
 
 /**
  * @fileoverview added by tsickle
