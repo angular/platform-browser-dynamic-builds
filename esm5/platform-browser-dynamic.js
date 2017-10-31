a4887b5d07f1fc1ca7f0ad278edfcfb77bd7acae
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:58:01 +0000

    fd37f3f fix(compiler): always use relative paths to refer to generated code

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-85e95cc
+ * @license Angular v5.0.0-rc.8-fd37f3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-85e95cc');
+var VERSION = new Version('5.0.0-rc.8-fd37f3f');
 
 /**
  * @fileoverview added by tsickle
