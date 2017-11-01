77aee3c5456a16d101a93bdc58dc3c8f0d8752b5
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:54:04 +0000

    a1ec65b docs(aio): fix typo in attribute-directives

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-851e1cf
+ * @license Angular v5.0.0-a1ec65b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-851e1cf');
+var VERSION = new Version('5.0.0-a1ec65b');
 
 /**
  * @fileoverview added by tsickle
