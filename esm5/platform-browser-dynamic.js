276ba4efee083ddef61a1bb9f51836857aba1fb2
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:06:00 +0000

    16d7258 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-a1ec65b
+ * @license Angular v5.0.0-16d7258
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-a1ec65b');
+var VERSION = new Version('5.0.0-16d7258');
 
 /**
  * @fileoverview added by tsickle
