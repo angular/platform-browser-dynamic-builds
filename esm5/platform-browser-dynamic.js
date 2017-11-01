7d9ed25673d0ad4d927113193cba91ef39496118
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:46:08 +0000

    ca129ba docs(aio): upgrade to v5 and cli 1.5

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-171ae15');
+var VERSION = new Version('5.0.0-ca129ba');
 
 /**
  * @fileoverview added by tsickle
