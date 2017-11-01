bf78858cbb2d178297e1928df01f481ae838c7db
Igor Minar <igor@angular.io>
2017-11-01 23:46:02 +0000

    2ea76cc build: change the version in package.json to 5.1.0-beta

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-27ae0f9
+ * @license Angular v5.1.0-beta.0-2ea76cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-27ae0f9
+ * @license Angular v5.1.0-beta.0-2ea76cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-27ae0f9');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-2ea76cc');
 
 /**
  * @fileoverview added by tsickle
