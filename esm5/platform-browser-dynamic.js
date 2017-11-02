3ecde01021e1f6e7f0ba90c0c8019ecdb1c26420
Victor Berchet <victor@suumit.com>
2017-11-02 21:53:43 +0000

    a41558e docs: BrowserSupport guide for CLI (#19843)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-132c071
+ * @license Angular v5.1.0-beta.0-a41558e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-132c071');
+var VERSION = new Version('5.1.0-beta.0-a41558e');
 
 /**
  * @fileoverview added by tsickle
