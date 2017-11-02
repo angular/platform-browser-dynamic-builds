1a6b7d54daef30b49993bd65131c935f5d84cd11
Victor Berchet <victor@suumit.com>
2017-11-02 22:40:12 +0000

    067e926 build(aio): do not fail the initial doc-gen when running docs-watch

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-8639995
+ * @license Angular v5.0.0-067e926
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-8639995');
+const VERSION = new Version('5.0.0-067e926');
 
 /**
  * @fileoverview added by tsickle
