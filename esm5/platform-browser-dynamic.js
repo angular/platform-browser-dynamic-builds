32bfebb80f86755790c26cc7038aaf5d1ffbc408
Victor Berchet <victor@suumit.com>
2017-11-02 22:49:05 +0000

    9975486 build: remove local yarn (#19981)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-068348e
+ * @license Angular v5.1.0-beta.0-9975486
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-068348e');
+var VERSION = new Version('5.1.0-beta.0-9975486');
 
 /**
  * @fileoverview added by tsickle
