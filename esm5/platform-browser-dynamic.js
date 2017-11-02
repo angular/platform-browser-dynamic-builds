d49ac102e0c858c98241bec60449ebae7a6b9492
Victor Berchet <victor@suumit.com>
2017-11-02 23:28:31 +0000

    25843fe build(aio): avoid building before running the local PWA tests

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-37a740b');
+var VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
