fd1f41d168398345132f60a07e336135a23154e4
Victor Berchet <victor@suumit.com>
2017-11-02 22:12:43 +0000

    e846953 docs: AOT guide for CLI #19510 (#19818)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-18197bd
+ * @license Angular v5.0.0-e846953
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-18197bd');
+var VERSION = new Version('5.0.0-e846953');
 
 /**
  * @fileoverview added by tsickle
