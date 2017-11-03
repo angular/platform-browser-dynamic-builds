42f585339613147a0a6fb174a722fb1a56d20133
Victor Berchet <victor@suumit.com>
2017-11-03 19:52:58 +0000

    a99eb16 test(aio): make e2e tests less flakey

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-faa6212
+ * @license Angular v5.1.0-beta.0-a99eb16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-faa6212');
+const VERSION = new Version('5.1.0-beta.0-a99eb16');
 
 /**
  * @fileoverview added by tsickle
