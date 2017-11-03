b450ac95b795b934506421c12fe11a8c4cbe7d4b
Victor Berchet <victor@suumit.com>
2017-11-03 18:32:56 +0000

    5079d2d docs(aio): Add v4 doc link to navigation (#20089)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcf8840
+ * @license Angular v5.1.0-beta.0-5079d2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-dcf8840
+ * @license Angular v5.1.0-beta.0-5079d2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-dcf8840');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-5079d2d');
 
 /**
  * @fileoverview added by tsickle
