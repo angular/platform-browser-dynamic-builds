72cae7b6201274fd0338661dc323e627646d9311
Victor Berchet <victor@suumit.com>
2017-11-03 22:54:51 +0000

    7083791 fix(core): should support event.stopImmediatePropagation (#19222)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e4cd3b0
+ * @license Angular v5.0.0-7083791
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-e4cd3b0');
+var VERSION = new Version('5.0.0-7083791');
 
 /**
  * @fileoverview added by tsickle
