f2c1c5d3b4741a46253635184652ade159fffb80
Victor Berchet <victor@suumit.com>
2017-11-03 19:32:43 +0000

    169cedd docs: Bootstrapping guide prose for CLI (#19777)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-1d19d61
+ * @license Angular v5.1.0-beta.0-169cedd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-1d19d61');
+const VERSION = new Version('5.1.0-beta.0-169cedd');
 
 /**
  * @fileoverview added by tsickle
