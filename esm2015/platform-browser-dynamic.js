6eb86cb3546a3aff13d93b0469339ccac61fecb8
Victor Berchet <victor@suumit.com>
2017-11-03 19:03:32 +0000

    e6f16a7 docs(aio): Fix typo in Architecture Overview. (#20073)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-678d1cf');
+const VERSION = new Version('5.0.0-e6f16a7');
 
 /**
  * @fileoverview added by tsickle
