6c1c99ab836f20238a61550b56dc60795a289c0c
Victor Berchet <victor@suumit.com>
2017-11-06 18:24:39 +0000

    8802016 fix(aio): style correctly on Safari (#20157)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-8b50ed0
+ * @license Angular v5.1.0-beta.0-8802016
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-8b50ed0');
+const VERSION = new Version('5.1.0-beta.0-8802016');
 
 /**
  * @fileoverview added by tsickle
