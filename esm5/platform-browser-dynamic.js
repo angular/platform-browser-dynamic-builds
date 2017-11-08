2ca482703244042dbbe92750654adf9cbc604650
Victor Berchet <victor@suumit.com>
2017-11-08 22:36:50 +0000

    119034c docs: add changelog for 5.0.1

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-78ba39b
+ * @license Angular v5.1.0-beta.0-119034c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-78ba39b');
+var VERSION = new Version('5.1.0-beta.0-119034c');
 
 /**
  * @fileoverview added by tsickle
