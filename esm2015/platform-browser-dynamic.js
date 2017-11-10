27ed4231cb04b4c4ab4433dc6fbb350a44b82d16
Jason Aden <jasonaden@google.com>
2017-11-10 19:33:40 +0000

    2aefac8 docs(aio): empty line between HTML tag and content (#20341) PR Close #20341

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-424a323
+ * @license Angular v5.0.1-2aefac8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-424a323');
+const VERSION = new Version('5.0.1-2aefac8');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-424a323
+ * @license Angular v5.0.1-2aefac8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-424a323');
+var VERSION = new Version('5.0.1-2aefac8');
 
 /**
  * @fileoverview added by tsickle
