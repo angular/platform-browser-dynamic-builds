755b1cf4b1b0e00bdb7e502bb578fa5b979a97cd
Jason Aden <jasonaden@google.com>
2017-11-10 00:24:29 +0000

    424a323 fix(compiler): show explanatory text in template errors (#20313)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-28985cb
+ * @license Angular v5.0.1-424a323
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-28985cb');
+const VERSION = new Version('5.0.1-424a323');
 
 /**
  * @fileoverview added by tsickle
