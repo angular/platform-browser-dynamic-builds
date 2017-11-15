0f9ccb3e57110f292455c53998f6ff7b268a2c3c
Jason Aden <jasonaden@google.com>
2017-11-15 20:21:04 +0000

    043e408 style(core): fix comment format for linter

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-d47b2a6
+ * @license Angular v5.1.0-beta.0-043e408
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-d47b2a6');
+var VERSION = new Version('5.1.0-beta.0-043e408');
 
 /**
  * @fileoverview added by tsickle
