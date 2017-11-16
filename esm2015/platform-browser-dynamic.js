6ccb6f7c848b1222dd912a51d779c2e752f78b0f
Miško Hevery <misko@hevery.com>
2017-11-16 04:50:49 +0000

    1285792 fix(aio): fix window title on Home page (#20440)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-2d6126e');
+const VERSION = new Version('5.0.1-1285792');
 
 /**
  * @fileoverview added by tsickle
