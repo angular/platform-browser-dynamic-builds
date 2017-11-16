ae11af06b9af4e3f6431193f6b0875bbf2ec114a
Miško Hevery <misko@hevery.com>
2017-11-16 06:59:26 +0000

    dcfffbf build: fix build failures (#20470)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-336041a
+ * @license Angular v5.1.0-beta.0-dcfffbf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-336041a
+ * @license Angular v5.1.0-beta.0-dcfffbf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-336041a');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-dcfffbf');
 
 /**
  * @fileoverview added by tsickle
