a464a00b98239c36fcdf60a1da28dcd3fa2f8d39
Miško Hevery <misko@hevery.com>
2017-11-21 17:08:04 +0000

    b53ead4 fix(compiler): support event bindings in `fullTemplateTypeCheck` (#20490)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-82aace6
+ * @license Angular v5.0.2-b53ead4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.2-82aace6');
+var VERSION = new Version('5.0.2-b53ead4');
 
 /**
  * @fileoverview added by tsickle
