0ff06bcc5e6043ff50aa4ab98d0f931abdd53b81
Miško Hevery <misko@hevery.com>
2017-11-29 22:47:43 +0000

    647ca64 docs(aio): Updating with Ignite UI for Angular (#20663)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.3-c7b211c');
+var VERSION = new Version('5.0.3-647ca64');
 
 /**
  * @fileoverview added by tsickle
