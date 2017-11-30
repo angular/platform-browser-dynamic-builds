7005285ec6b66eed2904ea04f7787bf227db9dd1
Miško Hevery <misko@hevery.com>
2017-11-30 03:59:41 +0000

    b450c83 docs(aio): remove services plurality (#20696)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-647ca64
+ * @license Angular v5.0.3-b450c83
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.3-647ca64');
+var VERSION = new Version('5.0.3-b450c83');
 
 /**
  * @fileoverview added by tsickle
