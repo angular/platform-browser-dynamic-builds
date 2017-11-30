1fd15136f2d66872cdc4482a687e9267bbdd0a17
Miško Hevery <misko@hevery.com>
2017-11-30 18:24:51 +0000

    47addd1 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -685,7 +685,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-ba6af2a');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-47addd1');
 
 /**
  * @fileoverview added by tsickle
