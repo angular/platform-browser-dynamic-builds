82e5b1b6b54ac8646e187ffbbd9f1b4876133ae7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:40:08 +0000

    9dc310e docs: add changelog for 5.0.5

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-445d833
+ * @license Angular v5.0.5-9dc310e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.4-445d833');
+var VERSION = new Version('5.0.5-9dc310e');
 
 /**
  * @fileoverview added by tsickle
