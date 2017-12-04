475081f8c4004355a00f32e8e7920dec2dd5c4ec
Igor Minar <igor@angularjs.org>
2017-12-04 23:15:21 +0000

    6c78cfb ci: fix the payload-size checking scripts (#20683)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9dc310e
+ * @license Angular v5.0.5-6c78cfb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.5-9dc310e
+ * @license Angular v5.0.5-6c78cfb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -681,7 +681,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.5-9dc310e');
+var VERSION = new _angular_core.Version('5.0.5-6c78cfb');
 
 /**
  * @fileoverview added by tsickle
