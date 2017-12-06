145f416829c652141ab783dbf9f334d3378513d3
Igor Minar <igor@angularjs.org>
2017-12-06 15:05:53 +0000

    be99449 docs(core): add docs for i18n tokens (#17920)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-6911a25
+ * @license Angular v5.1.0-rc.1-be99449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -654,7 +654,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.1-6911a25');
+var VERSION = new Version('5.1.0-rc.1-be99449');
 
 /**
  * @fileoverview added by tsickle
