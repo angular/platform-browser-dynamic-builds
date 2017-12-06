42784e119e255d840fe793ac93530b7042ec3efd
Igor Minar <igor@angularjs.org>
2017-12-06 15:20:10 +0000

    d824cc2 ci: upgrade to node 8 and Bazel 0.8 (#20807)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-c91c0d4
+ * @license Angular v5.0.5-d824cc2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-c91c0d4');
+const VERSION = new Version('5.0.5-d824cc2');
 
 /**
  * @fileoverview added by tsickle
