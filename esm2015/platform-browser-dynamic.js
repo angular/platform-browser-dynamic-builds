512d794939fef9465a36c515e330b183714cb7f9
Igor Minar <igor@angularjs.org>
2017-12-06 06:19:00 +0000

    860ef8c docs(aio): update myUnless references to appUnless (#20658)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-6d15dd7
+ * @license Angular v5.0.5-860ef8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-6d15dd7');
+const VERSION = new Version('5.0.5-860ef8c');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-6d15dd7
+ * @license Angular v5.0.5-860ef8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -650,7 +650,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-6d15dd7');
+var VERSION = new Version('5.0.5-860ef8c');
 
 /**
  * @fileoverview added by tsickle
