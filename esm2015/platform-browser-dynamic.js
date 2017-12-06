5ab053e9ac319496b5b1d14fc5ed801459ba5e98
Igor Minar <igor@angularjs.org>
2017-12-06 15:15:43 +0000

    c91c0d4 fix(service-worker): esm2015 points to wrong path (#20800)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -569,7 +569,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-9efb5da');
+const VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
