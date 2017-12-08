230ca6e91df38e8bd5b1262198c2d1036d685f4b
Jason Aden <jasonaden@google.com>
2017-12-08 18:31:45 +0000

    ddada6e build(aio): upgrade to latest `@angular/cli` (#18428)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -685,7 +685,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-225baf4');
+var VERSION = new _angular_core.Version('5.1.0-ddada6e');
 
 /**
  * @fileoverview added by tsickle
