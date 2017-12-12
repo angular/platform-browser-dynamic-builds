6c7bc9154cf2ec161b54933c92719192f97cae9e
Jason Aden <jasonaden@google.com>
2017-12-12 00:35:18 +0000

    1f46949 ci: use container version in cache key (#20952)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8a5f0f7
+ * @license Angular v5.1.0-1f46949
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-8a5f0f7
+ * @license Angular v5.1.0-1f46949
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -685,7 +685,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-8a5f0f7');
+var VERSION = new _angular_core.Version('5.1.0-1f46949');
 
 /**
  * @fileoverview added by tsickle
