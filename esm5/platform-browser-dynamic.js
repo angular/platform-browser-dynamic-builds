24fb0c2d3cd4aa21967a62b7e3bf7db1399bd46e
Igor Minar <igor@angularjs.org>
2017-12-21 20:26:35 +0000

    871ece6 fix(animations): renaming issue with DOMAnimation. (#21125)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-abca7c0
+ * @license Angular v5.2.0-beta.1-871ece6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -663,7 +663,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.1-abca7c0');
+var VERSION = new Version('5.2.0-beta.1-871ece6');
 
 /**
  * @fileoverview added by tsickle
