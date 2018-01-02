b2b2cba171e21a31c3887270a47b30895f743968
Miško Hevery <misko@hevery.com>
2018-01-02 21:10:29 +0000

    48a1f32 fix(language-service): ignore null metadatas (#20557)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-66043e0
+ * @license Angular v5.1.2-48a1f32
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.2-66043e0
+ * @license Angular v5.1.2-48a1f32
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -685,7 +685,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-66043e0');
+var VERSION = new _angular_core.Version('5.1.2-48a1f32');
 
 /**
  * @fileoverview added by tsickle
