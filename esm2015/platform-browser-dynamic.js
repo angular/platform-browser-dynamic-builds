34d6e17c4d7e783cc80543fc956ad196cb783bee
Kara Erickson <karakara@google.com>
2018-01-04 20:24:48 +0000

    ef78538 fix(aio): don't use Object.keys on NamedNodeMap to prevent SEO errors (#21305)

--- esm2015/platform-browser-dynamic.js
+++ esm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ccea372
+ * @license Angular v5.2.0-rc.0-ef78538
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -565,7 +565,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-ccea372');
+const VERSION = new Version('5.2.0-rc.0-ef78538');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ccea372
+ * @license Angular v5.2.0-rc.0-ef78538
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-ccea372');
+var VERSION = new Version('5.2.0-rc.0-ef78538');
 
 /**
  * @fileoverview added by tsickle
