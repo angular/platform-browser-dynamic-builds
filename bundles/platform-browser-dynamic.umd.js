923412f1d9600ae7d2c5d7b2a7b6fef082a8ea1f
Kara Erickson <karakara@google.com>
2018-01-09 00:33:12 +0000

    27196b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-6040ee3
+ * @license Angular v5.2.0-rc.0-27196b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-rc.0-6040ee3
+ * @license Angular v5.2.0-rc.0-27196b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-6040ee3');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-27196b6');
 
 /**
  * @fileoverview added by tsickle
