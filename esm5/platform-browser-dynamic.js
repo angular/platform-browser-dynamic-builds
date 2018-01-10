99cead9ae5d9d36459d0cdc32903594230d4adcf
Kara Erickson <karakara@google.com>
2018-01-10 20:17:54 +0000

    fc3e7e0 refactor(core): create consistent naming scheme across classes (#21403)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-908129e
+ * @license Angular v5.2.0-fc3e7e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-908129e');
+var VERSION = new Version('5.2.0-fc3e7e0');
 
 /**
  * @fileoverview added by tsickle
