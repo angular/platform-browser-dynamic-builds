d817445c95623bbbc13fef474297c242b0322c5b
Alex Eagle <alexeagle@google.com>
2018-01-10 20:47:49 +0000

    76c781f docs(router): remove extra brackets (#21294)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-51eb3d4
+ * @license Angular v5.2.0-76c781f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-51eb3d4');
+var VERSION = new Version('5.2.0-76c781f');
 
 /**
  * @fileoverview added by tsickle
