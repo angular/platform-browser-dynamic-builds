8b7dd01f90162167efa501f95c17d0597b17a88e
Alex Eagle <alexeagle@google.com>
2018-01-10 22:14:30 +0000

    2ad92ed build(bazel): fix merge conflict between c4f02e2 and ef956a2 (#21453)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-e3e2fc0
+ * @license Angular v5.2.0-2ad92ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-e3e2fc0');
+var VERSION = new Version('5.2.0-2ad92ed');
 
 /**
  * @fileoverview added by tsickle
