23419e8c0629a18d22492e95802e8d70891904cc
Alex Eagle <alexeagle@google.com>
2018-01-10 20:27:32 +0000

    48c1898 fix(animations): fix increment/decrement aliases example (#18323)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-167cbed
+ * @license Angular v5.2.0-48c1898
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-167cbed');
+var VERSION = new Version('5.2.0-48c1898');
 
 /**
  * @fileoverview added by tsickle
