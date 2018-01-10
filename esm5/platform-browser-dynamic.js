f1ebd7e8ebae53083b2e3fbd2a37a1a6d127a453
Alex Eagle <alexeagle@google.com>
2018-01-10 20:21:32 +0000

    167cbed build: add bazel rulse for benchmarks (#21436)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-70e8802
+ * @license Angular v5.2.0-167cbed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-70e8802');
+var VERSION = new Version('5.2.0-167cbed');
 
 /**
  * @fileoverview added by tsickle
