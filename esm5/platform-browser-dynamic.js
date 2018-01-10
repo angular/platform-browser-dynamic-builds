b3c1628a6bce97d6fa7c114f65fc10e983ba3ec7
Alex Eagle <alexeagle@google.com>
2018-01-10 21:28:36 +0000

    5ede67c build: move repeated tsconfig attributes to a macro (#20964)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-31b671a
+ * @license Angular v5.2.0-5ede67c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-31b671a');
+var VERSION = new Version('5.2.0-5ede67c');
 
 /**
  * @fileoverview added by tsickle
