e99e29a691ad4b05593cf3ad3fbcff99b1653453
Alex Eagle <alexeagle@google.com>
2018-01-10 22:49:31 +0000

    7b463df fix(aio): preserve static Observable methods (#21351)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-650f5fb
+ * @license Angular v5.2.0-7b463df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-650f5fb');
+var VERSION = new _angular_core.Version('5.2.0-7b463df');
 
 /**
  * @fileoverview added by tsickle
