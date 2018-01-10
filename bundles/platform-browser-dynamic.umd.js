363ad503e3af01dc744d740792d11d162fb00ea1
Kara Erickson <karakara@google.com>
2018-01-10 19:33:21 +0000

    908129e docs(core): provide note for unit test changes with the `ng-star-inserted` className (#21450)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-04dd971');
+var VERSION = new _angular_core.Version('5.2.0-908129e');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-04dd971');
+var VERSION = new Version('5.2.0-908129e');
 
 /**
  * @fileoverview added by tsickle
