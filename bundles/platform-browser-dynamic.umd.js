558858466be252494560addb7decca5a13b1d300
Alex Eagle <alexeagle@google.com>
2018-01-11 00:41:54 +0000

    e64b1e9 fix(compiler): make `.ngsummary.json` files idempotent (#21448)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-6be9c04');
+var VERSION = new _angular_core.Version('5.2.0-e64b1e9');
 
 /**
  * @fileoverview added by tsickle
