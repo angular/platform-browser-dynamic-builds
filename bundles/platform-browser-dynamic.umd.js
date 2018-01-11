26ca37286fe2c4873d1fb410c0c1ec2339e29ec7
Alex Eagle <alexeagle@google.com>
2018-01-11 00:45:14 +0000

    456f57d docs: fix lazy-loading example (#20306)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-e64b1e9
+ * @license Angular v5.2.0-456f57d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-e64b1e9
+ * @license Angular v5.2.0-456f57d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-e64b1e9');
+var VERSION = new _angular_core.Version('5.2.0-456f57d');
 
 /**
  * @fileoverview added by tsickle
