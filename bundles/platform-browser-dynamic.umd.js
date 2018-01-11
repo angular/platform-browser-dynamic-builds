968eb2a67d97839c529e2884e13a46e780bd7a98
Alex Eagle <alexeagle@google.com>
2018-01-11 01:25:38 +0000

    c6eb9ee build: update polyfill size (#21461)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-456f57d
+ * @license Angular v5.2.0-c6eb9ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-456f57d
+ * @license Angular v5.2.0-c6eb9ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-456f57d');
+var VERSION = new _angular_core.Version('5.2.0-c6eb9ee');
 
 /**
  * @fileoverview added by tsickle
