26f467569a3288f3b8b0fbc21064151f288e769f
Alex Eagle <alexeagle@google.com>
2018-01-12 20:24:19 +0000

    cffa0fe feat(bazel): allow ng_module rules to control whether type checking is enabled (#21460)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-9b5a485
+ * @license Angular v5.2.0-cffa0fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-9b5a485');
+var VERSION = new Version('5.2.0-cffa0fe');
 
 /**
  * @fileoverview added by tsickle
