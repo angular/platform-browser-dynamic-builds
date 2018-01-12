0984d562109e3b8db1a293a925cb5da62dd3dc95
Alex Eagle <alexeagle@google.com>
2018-01-12 23:07:39 +0000

    cc9419d fix(language-service): Clear caches when program changes (#21337)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d5393c7
+ * @license Angular v5.2.0-cc9419d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-d5393c7
+ * @license Angular v5.2.0-cc9419d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-d5393c7');
+var VERSION = new _angular_core.Version('5.2.0-cc9419d');
 
 /**
  * @fileoverview added by tsickle
