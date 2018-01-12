d9d78108be5fc0976273fa3b6a9a40811bff2da4
Alex Eagle <alexeagle@google.com>
2018-01-12 20:28:33 +0000

    bb62458 docs: fix release schedule date (#21474)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-cffa0fe
+ * @license Angular v5.2.0-bb62458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-cffa0fe');
+var VERSION = new Version('5.2.0-bb62458');
 
 /**
  * @fileoverview added by tsickle
