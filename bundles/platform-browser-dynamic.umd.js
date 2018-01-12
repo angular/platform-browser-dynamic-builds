1f32587ee1e0524ec991971351d2183046e88c4c
Alex Eagle <alexeagle@google.com>
2018-01-12 22:55:58 +0000

    c5c6d84 style(aio): enforce strict TypeScript checks (#21342)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-246de65
+ * @license Angular v5.2.0-c5c6d84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-246de65
+ * @license Angular v5.2.0-c5c6d84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-246de65');
+var VERSION = new _angular_core.Version('5.2.0-c5c6d84');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-246de65
+ * @license Angular v5.2.0-c5c6d84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-246de65');
+var VERSION = new Version('5.2.0-c5c6d84');
 
 /**
  * @fileoverview added by tsickle
