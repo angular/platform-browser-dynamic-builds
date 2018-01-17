f8f64a94649f362c62a877f40e6221a5af2467c0
GitHub <noreply@github.com>
2018-01-17 23:41:50 +0000

    0eabd07 fix: avoid triggering a cli bug (#21611)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.0-8503cc1');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-0eabd07');
 
 /**
  * @fileoverview added by tsickle
