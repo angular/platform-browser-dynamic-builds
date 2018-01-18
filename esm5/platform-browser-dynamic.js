07dc355f991077c3d852ca68fccb2647af1ab541
Chuck Jazdzewski <chuckj@google.com>
2018-01-18 01:23:53 +0000

    e79e98a docs: fix lazy loading example dir name (#21475)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-47b7898
+ * @license Angular v5.9.9-6-beta.0-e79e98a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-47b7898');
+var VERSION = new Version('5.9.9-6-beta.0-e79e98a');
 
 /**
  * @fileoverview added by tsickle
