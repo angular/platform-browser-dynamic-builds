8522ce66d7c70eda6cd86a03660c9888a3bf769f
Miško Hevery <misko@hevery.com>
2018-01-24 05:52:35 +0000

    8b3fbb5 fix(router): don't use ParamsInheritanceStrategy in declarations (#21574)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-ed670a3
+ * @license Angular v5.2.1-8b3fbb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.1-ed670a3');
+var VERSION = new Version('5.2.1-8b3fbb5');
 
 /**
  * @fileoverview added by tsickle
