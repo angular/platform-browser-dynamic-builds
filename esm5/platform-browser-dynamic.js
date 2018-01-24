05f7a2e053ee763756bfd8817e916b2f7f6a3adf
Miško Hevery <misko@hevery.com>
2018-01-24 01:05:12 +0000

    1e9cd95 test(ivy): content projection canonical example (#21674)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-acf381b
+ * @license Angular v5.9.9-6-beta.0-1e9cd95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-acf381b');
+var VERSION = new Version('5.9.9-6-beta.0-1e9cd95');
 
 /**
  * @fileoverview added by tsickle
