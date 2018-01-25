89d2f88fec7b36d79bd8db54cbc19144ef8171a1
Misko Hevery <misko@hevery.com>
2018-01-25 06:38:19 +0000

    3e03dbe refactor(ivy): flatten hooks and collapse LView hook properties (#21650)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-d08785d
+ * @license Angular v6.0.0-beta.1-3e03dbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-d08785d
+ * @license Angular v6.0.0-beta.1-3e03dbe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-d08785d');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-3e03dbe');
 
 /**
  * @fileoverview added by tsickle
