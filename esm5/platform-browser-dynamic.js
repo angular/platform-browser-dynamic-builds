6c6ef15785c5cd5308dad9ec1605aa1198ed9319
Misko Hevery <misko@hevery.com>
2018-01-25 05:58:48 +0000

    3aaf43f docs: add changelog for 5.2.2

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-da9e57b
+ * @license Angular v5.2.2-3aaf43f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.1-da9e57b');
+var VERSION = new Version('5.2.2-3aaf43f');
 
 /**
  * @fileoverview added by tsickle
