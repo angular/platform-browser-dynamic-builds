43a147b8696c16cc5e3fabc9eee1db106a8acfd1
Jason Aden <jasonaden@google.com>
2018-01-26 23:33:42 +0000

    dcca799 fix(ivy): call onChanges before onInit (#21793)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-e0b31db
+ * @license Angular v6.0.0-beta.1-dcca799
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-e0b31db');
+var VERSION = new Version('6.0.0-beta.1-dcca799');
 
 /**
  * @fileoverview added by tsickle
