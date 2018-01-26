2bfc22bd63f2528e6ad7d56d44d24756e7d7f368
Misko Hevery <misko@hevery.com>
2018-01-26 06:49:29 +0000

    fac4d8d build(common): specify explicit locales dir in package.json (#21016)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-170885c
+ * @license Angular v6.0.0-beta.1-fac4d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-170885c
+ * @license Angular v6.0.0-beta.1-fac4d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-170885c');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-fac4d8d');
 
 /**
  * @fileoverview added by tsickle
