ba7e882e8cdcc2ef20208d9986a50d8b3384adb3
Jason Aden <jasonaden@google.com>
2018-01-26 23:57:05 +0000

    81d64d6 fix(core): fix retrieving the binding name when an expression changes (#21814)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-7410941
+ * @license Angular v5.2.2-81d64d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-7410941');
+var VERSION = new Version('5.2.2-81d64d6');
 
 /**
  * @fileoverview added by tsickle
