c2729d09a82942275165f8899c744e082636743f
Misko Hevery <misko@hevery.com>
2018-01-26 06:37:00 +0000

    5713faa build: autosquashes SHAs as part of merge-pr script (#21791)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-23596b3
+ * @license Angular v6.0.0-beta.1-5713faa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-23596b3
+ * @license Angular v6.0.0-beta.1-5713faa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-23596b3');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-5713faa');
 
 /**
  * @fileoverview added by tsickle
