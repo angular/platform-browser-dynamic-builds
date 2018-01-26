d5406b8c4322735601efaf58cc3ce130a4c8479f
Jason Aden <jasonaden@google.com>
2018-01-26 23:14:24 +0000

    250c8da fix(language-service): ensure correct paths are passed to TypeScript (#21812)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-d159ad8
+ * @license Angular v5.2.2-250c8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-d159ad8');
+var VERSION = new Version('5.2.2-250c8da');
 
 /**
  * @fileoverview added by tsickle
