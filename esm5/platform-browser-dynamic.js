c7af6d1757cd544d47684a7deeebe72568c0e7f3
Jason Aden <jasonaden@google.com>
2018-01-29 19:57:12 +0000

    a89e709 docs: change ”it's" to "its" as needed in several docs. (#21867)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-6a7689d
+ * @license Angular v5.2.2-a89e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-6a7689d');
+var VERSION = new Version('5.2.2-a89e709');
 
 /**
  * @fileoverview added by tsickle
