ad24d9c6fe63c2e8ca8a9d45f3f37bf274d0a369
Jason Aden <jasonaden@google.com>
2018-01-29 20:01:05 +0000

    6e8bc31 docs: change ”it's" to "its" as needed in several docs. (#21867)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-c40ae7f
+ * @license Angular v6.0.0-beta.1-6e8bc31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-c40ae7f
+ * @license Angular v6.0.0-beta.1-6e8bc31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-c40ae7f');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-6e8bc31');
 
 /**
  * @fileoverview added by tsickle
