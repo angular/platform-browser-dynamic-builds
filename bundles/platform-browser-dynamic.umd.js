0657b8475ffc353be9ac80e8559d6d0f2b50d896
Jason Aden <jasonaden@google.com>
2018-01-30 21:42:53 +0000

    407b5cf refactor(ivy): re-introduce QueryPredicate with more focused role (#21857) (#21857)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-285dd6b
+ * @license Angular v6.0.0-beta.1-407b5cf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-285dd6b
+ * @license Angular v6.0.0-beta.1-407b5cf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-285dd6b');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-407b5cf');
 
 /**
  * @fileoverview added by tsickle
