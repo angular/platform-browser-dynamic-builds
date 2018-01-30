5a2de71180bedf94236442913192800efda74a70
Jason Aden <jasonaden@google.com>
2018-01-30 00:33:34 +0000

    11b1267 docs: document debugging a Node test in VSCode/Bazel (#21868)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dd48df1
+ * @license Angular v6.0.0-beta.1-11b1267
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-dd48df1');
+var VERSION = new Version('6.0.0-beta.1-11b1267');
 
 /**
  * @fileoverview added by tsickle
