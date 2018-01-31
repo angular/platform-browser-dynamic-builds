d1d0dde5780a1151ca196cab1fdf0136850f020d
Jason Aden <jasonaden@google.com>
2018-01-31 19:01:03 +0000

    e0022ae docs: Fix platform-detection example for Universal (#21796)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-f2e923e
+ * @license Angular v5.2.2-e0022ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.2-f2e923e
+ * @license Angular v5.2.2-e0022ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.2-f2e923e');
+var VERSION = new _angular_core.Version('5.2.2-e0022ae');
 
 /**
  * @fileoverview added by tsickle
