b9fe767f1edbf4b35f2e11dc22038c021d4bfe86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:56:40 +0000

    102d06b docs: consistency fix in describing a custom tag (#21747)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-aa9ba7f');
+var VERSION = new _angular_core.Version('5.2.3-102d06b');
 
 /**
  * @fileoverview added by tsickle
