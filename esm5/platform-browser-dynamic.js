d074d6b46ff8c64db864f1a00b409a69029f95d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:11:25 +0000

    965eecc build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-c4fb696');
+var VERSION = new Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
