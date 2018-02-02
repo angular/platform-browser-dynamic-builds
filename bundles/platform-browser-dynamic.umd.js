aef123bebf7fee59ca06c1772bcf509a81e9210c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:14:59 +0000

    75eecdc docs: add missing underline (#21892) PR Close #21892

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-965eecc');
+var VERSION = new _angular_core.Version('5.2.3-75eecdc');
 
 /**
  * @fileoverview added by tsickle
