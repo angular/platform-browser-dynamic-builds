af36ea456a496d7611a53ed3d296d5e4f23a5711
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:17:41 +0000

    f0396f1 docs(aio): fix swap value (#20905)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-adb1d62
+ * @license Angular v5.2.3-f0396f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-adb1d62');
+var VERSION = new Version('5.2.3-f0396f1');
 
 /**
  * @fileoverview added by tsickle
