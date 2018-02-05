4da45099c7c66f3688f4b9c2b32410966aba5f24
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:04:54 +0000

    adb1d62 docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-664f7fa
+ * @license Angular v5.2.3-adb1d62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-664f7fa');
+var VERSION = new Version('5.2.3-adb1d62');
 
 /**
  * @fileoverview added by tsickle
