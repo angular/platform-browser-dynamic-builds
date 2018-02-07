92da9462797095d5d1bcbb117bac544ecb43d34b
Miško Hevery <misko@hevery.com>
2018-02-07 21:00:10 +0000

    9744a1c fix(forms): set state before emitting a value from ngModelChange (#21514)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-0bcfae7
+ * @license Angular v6.0.0-beta.3-9744a1c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-0bcfae7');
+var VERSION = new Version('6.0.0-beta.3-9744a1c');
 
 /**
  * @fileoverview added by tsickle
