24c1c7201c8a3c4d63206b6264ff90005c079a54
Miško Hevery <misko@hevery.com>
2018-02-07 21:09:01 +0000

    9d02db3 fix(aio): ignore `.header-link` when selecting the heading text (#21695)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-56b9591');
+var VERSION = new _angular_core.Version('5.2.4-9d02db3');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-56b9591');
+var VERSION = new Version('5.2.4-9d02db3');
 
 /**
  * @fileoverview added by tsickle
