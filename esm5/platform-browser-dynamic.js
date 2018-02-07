aef7cd4789b3763686b28d2a737eddcc43e7a657
Miško Hevery <misko@hevery.com>
2018-02-07 21:25:53 +0000

    07769e5 test(common): disable deprecated date pipe tests on chrome mobile (#21933)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-9fdb804
+ * @license Angular v5.2.4-07769e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-9fdb804');
+var VERSION = new Version('5.2.4-07769e5');
 
 /**
  * @fileoverview added by tsickle
