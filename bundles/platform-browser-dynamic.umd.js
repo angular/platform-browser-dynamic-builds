bb1d28543a07839d30582e4cbe74aa2def9f8f8b
Miško Hevery <misko@hevery.com>
2018-02-07 22:06:10 +0000

    b58c352 test(ivy): add canonical spec for object literals (#22045)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-efc67ee
+ * @license Angular v6.0.0-beta.3-b58c352
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-efc67ee
+ * @license Angular v6.0.0-beta.3-b58c352
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-efc67ee');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-b58c352');
 
 /**
  * @fileoverview added by tsickle
