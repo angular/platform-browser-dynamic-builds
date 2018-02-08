44cd4d6e721f22e82bb36581962aa3237d6a1378
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:46:21 +0000

    799edd1 docs(aio): remove lifecycle hooks img (#21425)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ffcb5dd
+ * @license Angular v5.2.4-799edd1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-ffcb5dd');
+var VERSION = new Version('5.2.4-799edd1');
 
 /**
  * @fileoverview added by tsickle
