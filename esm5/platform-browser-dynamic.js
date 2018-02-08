8822a1732f5788dd37aab8cc4117c7fa33900c95
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:33:14 +0000

    16e5b86 test(ivy): also track the size of the compressed hello world bundle (#22056)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-cd25939
+ * @license Angular v6.0.0-beta.3-16e5b86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-cd25939');
+var VERSION = new Version('6.0.0-beta.3-16e5b86');
 
 /**
  * @fileoverview added by tsickle
