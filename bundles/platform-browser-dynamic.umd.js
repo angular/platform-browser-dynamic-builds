d2b699f69d1e130025719dac1be0d4aa3c754f50
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 17:20:34 +0000

    d9ae70c test(ivy): normalize template names in canonical spec (#21815)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-a751649
+ * @license Angular v6.0.0-beta.3-d9ae70c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-a751649
+ * @license Angular v6.0.0-beta.3-d9ae70c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-a751649');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-d9ae70c');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-a751649
+ * @license Angular v6.0.0-beta.3-d9ae70c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-a751649');
+var VERSION = new Version('6.0.0-beta.3-d9ae70c');
 
 /**
  * @fileoverview added by tsickle
