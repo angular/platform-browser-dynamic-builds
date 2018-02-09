b6e9e159641074b1b5a18f7f7228a70de015806e
Miško Hevery <misko@hevery.com>
2018-02-09 21:46:51 +0000

    5fc77c9 fix(aio): do not rewrite /styleguide URL in Service Worker (#22085)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-c348445
+ * @license Angular v6.0.0-beta.3-5fc77c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-c348445
+ * @license Angular v6.0.0-beta.3-5fc77c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-c348445');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-5fc77c9');
 
 /**
  * @fileoverview added by tsickle
