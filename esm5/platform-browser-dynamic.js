9f4a06c6213a489d2c17dd8bd985211186450c70
Miško Hevery <misko@hevery.com>
2018-02-10 01:48:55 +0000

    7e9b120 build: update to latest bazel rules (#22127)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b081dfe
+ * @license Angular v6.0.0-beta.3-7e9b120
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-b081dfe');
+var VERSION = new Version('6.0.0-beta.3-7e9b120');
 
 /**
  * @fileoverview added by tsickle
