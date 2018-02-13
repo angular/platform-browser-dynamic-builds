289fb69530c8ce03ec834c104048463f1236c28f
Miško Hevery <misko@hevery.com>
2018-02-13 21:59:28 +0000

    a589ca0 test(ivy): clean up canonical spec (#22188)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-72f8abd
+ * @license Angular v6.0.0-beta.3-a589ca0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-72f8abd');
+var VERSION = new Version('6.0.0-beta.3-a589ca0');
 
 /**
  * @fileoverview added by tsickle
