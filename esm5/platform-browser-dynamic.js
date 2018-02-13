09b9728458b336ce14ac8bb81194465f946a0873
Miško Hevery <misko@hevery.com>
2018-02-13 19:06:50 +0000

    b75cf3f ci: update ngbot config file (#22173)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-4f19491
+ * @license Angular v6.0.0-beta.3-b75cf3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-4f19491');
+var VERSION = new Version('6.0.0-beta.3-b75cf3f');
 
 /**
  * @fileoverview added by tsickle
