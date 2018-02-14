33c508e6df9c9af21b258a718f6fbc5e5ab1fd55
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:49:24 +0000

    5bb9fca build: comment-out chromium version checking code temporarily (#22232)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-fa7d890
+ * @license Angular v6.0.0-beta.4-5bb9fca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-fa7d890');
+var VERSION = new Version('6.0.0-beta.4-5bb9fca');
 
 /**
  * @fileoverview added by tsickle
