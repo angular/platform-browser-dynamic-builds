60abcb4054d9a4b9ffed4a2e367990de0399c4d1
Victor Berchet <victor@suumit.com>
2018-02-15 21:36:58 +0000

    03d93c9 Revert: "build: merge-pr new checks that all requested changes have been addressed (#21817)"

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-0aa9b46');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-03d93c9');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-0aa9b46');
+var VERSION = new Version('6.0.0-beta.4-03d93c9');
 
 /**
  * @fileoverview added by tsickle
