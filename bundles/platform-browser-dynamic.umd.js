374d724f912eb81cefcde70c11119c44ebfe0545
Victor Berchet <victor@suumit.com>
2018-02-16 23:11:30 +0000

    6b45784 test(aio): increase `docs-test` timeouts to prevent flakes on Travis (#22261)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5f52ea3
+ * @license Angular v5.2.5-6b45784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-5f52ea3
+ * @license Angular v5.2.5-6b45784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-5f52ea3');
+var VERSION = new _angular_core.Version('5.2.5-6b45784');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5f52ea3
+ * @license Angular v5.2.5-6b45784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-5f52ea3');
+var VERSION = new Version('5.2.5-6b45784');
 
 /**
  * @fileoverview added by tsickle
