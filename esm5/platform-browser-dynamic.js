ead0a5d59fa0a3e20def82b0d512ffed6b50bb02
Victor Berchet <victor@suumit.com>
2018-02-17 02:26:59 +0000

    f693be3 feat(ivy): add pureFunction0 instruction (#22214)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-e1bf067
+ * @license Angular v6.0.0-beta.4-f693be3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-e1bf067');
+var VERSION = new Version('6.0.0-beta.4-f693be3');
 
 /**
  * @fileoverview added by tsickle
