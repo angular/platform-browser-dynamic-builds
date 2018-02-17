1f0248fce623145ed66edea8cee2ddcdca44d0d5
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:52 +0000

    861250b docs: fix changelog errors (#22228)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-81c1e0a');
+var VERSION = new _angular_core.Version('5.2.5-861250b');
 
 /**
  * @fileoverview added by tsickle
