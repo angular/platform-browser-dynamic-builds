d476b43c07da77da988e450d782f964694a9fbe8
Victor Berchet <victor@suumit.com>
2018-02-20 20:03:42 +0000

    ee60bb5 fix(ivy): pureFunction8 should update the right bindings (#22313)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f6120c0
+ * @license Angular v6.0.0-beta.4-ee60bb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-f6120c0');
+var VERSION = new Version('6.0.0-beta.4-ee60bb5');
 
 /**
  * @fileoverview added by tsickle
