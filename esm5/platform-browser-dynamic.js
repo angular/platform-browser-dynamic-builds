52b716e174919b2ba3202976e0b9a76c11c9b7ee
Victor Berchet <victor@suumit.com>
2018-02-21 19:32:25 +0000

    435f6ee build: make git revert messages valid (#22339)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-9e6268b
+ * @license Angular v6.0.0-beta.4-435f6ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-9e6268b');
+var VERSION = new Version('6.0.0-beta.4-435f6ee');
 
 /**
  * @fileoverview added by tsickle
