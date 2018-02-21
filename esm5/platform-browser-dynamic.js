83c6d5d2748877be38655fccf3ac3dc53c822c09
Victor Berchet <victor@suumit.com>
2018-02-21 23:13:26 +0000

    b42921b docs(aio): updates directive event hooks real capabilities (#16654)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-722dec1
+ * @license Angular v6.0.0-beta.4-b42921b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-722dec1');
+var VERSION = new Version('6.0.0-beta.4-b42921b');
 
 /**
  * @fileoverview added by tsickle
