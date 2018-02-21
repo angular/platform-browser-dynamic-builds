1668acd2faf5c6c431884fd1fde2450f774d31b1
Victor Berchet <victor@suumit.com>
2018-02-21 00:05:15 +0000

    519f022 docs(aio): update installed mobile tool list (#22331) PR Close #22331

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-236a932
+ * @license Angular v6.0.0-beta.4-519f022
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-236a932
+ * @license Angular v6.0.0-beta.4-519f022
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-236a932');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-519f022');
 
 /**
  * @fileoverview added by tsickle
