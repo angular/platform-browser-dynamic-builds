94cd9ba536caa49437a670e5e2997cf3ef3e1585
Victor Berchet <victor@suumit.com>
2018-02-21 23:22:43 +0000

    a7ebf5a fix(core): properly handle function without prototype in reflector (#22284)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b42921b
+ * @license Angular v6.0.0-beta.4-a7ebf5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-b42921b');
+var VERSION = new Version('6.0.0-beta.4-a7ebf5a');
 
 /**
  * @fileoverview added by tsickle
