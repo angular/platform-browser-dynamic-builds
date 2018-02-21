663c715573c0bf33131860a13326c6450d1090f5
Victor Berchet <victor@suumit.com>
2018-02-21 19:41:07 +0000

    722dec1 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-435f6ee
+ * @license Angular v6.0.0-beta.4-722dec1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-435f6ee');
+var VERSION = new Version('6.0.0-beta.4-722dec1');
 
 /**
  * @fileoverview added by tsickle
