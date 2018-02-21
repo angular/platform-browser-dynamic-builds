c678b79f870328f1c2d3271cb78f61b79d3ad8d6
Victor Berchet <victor@suumit.com>
2018-02-21 00:26:06 +0000

    23b0707 docs(aio): fix the css of the heroes component's buttons (#22333)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-a2cb010
+ * @license Angular v5.2.5-23b0707
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-a2cb010');
+var VERSION = new Version('5.2.5-23b0707');
 
 /**
  * @fileoverview added by tsickle
