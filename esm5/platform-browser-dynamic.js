5801bcebbd575fb2d2f6ac6a1c8dde629fd8be2b
Victor Berchet <victor@suumit.com>
2018-02-22 18:45:52 +0000

    ca06af4 build: allow passing node options to ngc. (#22245)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-6091a95
+ * @license Angular v6.0.0-beta.5-ca06af4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-6091a95');
+var VERSION = new Version('6.0.0-beta.5-ca06af4');
 
 /**
  * @fileoverview added by tsickle
