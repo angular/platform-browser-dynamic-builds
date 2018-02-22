b7ae65301df2a1500ed5bfa16cc6d50f229e53db
Victor Berchet <victor@suumit.com>
2018-02-22 19:54:18 +0000

    894b098 test(ivy): add missing tests on directive lifecycle hooks (#22368)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-022ad4a
+ * @license Angular v6.0.0-beta.5-894b098
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-022ad4a');
+var VERSION = new Version('6.0.0-beta.5-894b098');
 
 /**
  * @fileoverview added by tsickle
