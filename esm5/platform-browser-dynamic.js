b0e1122bd01e358d1242737ced7d23d6f08d91d5
Victor Berchet <victor@suumit.com>
2018-02-23 18:08:40 +0000

    a81d599 ci: don't use bazel git_repository rule (#22406)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-7effb00
+ * @license Angular v6.0.0-beta.5-a81d599
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-7effb00');
+var VERSION = new Version('6.0.0-beta.5-a81d599');
 
 /**
  * @fileoverview added by tsickle
