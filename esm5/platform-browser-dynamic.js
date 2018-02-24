f23b8b460ee0671b76f526fe1e6e8f34efec3f48
Victor Berchet <victor@suumit.com>
2018-02-24 02:35:07 +0000

    a403229 Revert "fix(router): fix URL serialization so special characters are only encoded where needed (#22337)"

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-4180912
+ * @license Angular v6.0.0-beta.5-a403229
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-4180912');
+var VERSION = new Version('6.0.0-beta.5-a403229');
 
 /**
  * @fileoverview added by tsickle
