735a82b920c6835fa208be40bf9dbac921d66cf1
Victor Berchet <victor@suumit.com>
2018-02-25 18:27:09 +0000

    13ab91e fix(upgrade): fix empty transclusion content with AngularJS@>=1.5.8 (#22167)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a403229
+ * @license Angular v6.0.0-beta.5-13ab91e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-a403229
+ * @license Angular v6.0.0-beta.5-13ab91e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-a403229');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-13ab91e');
 
 /**
  * @fileoverview added by tsickle
