c90f7ac1bd2a97961cf013085f0126e3f7056469
Alex Eagle <alexeagle@google.com>
2018-02-26 22:00:30 +0000

    c593d69 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-5a32d7e
+ * @license Angular v6.0.0-beta.5-c593d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-5a32d7e
+ * @license Angular v6.0.0-beta.5-c593d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-5a32d7e');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-c593d69');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-5a32d7e
+ * @license Angular v6.0.0-beta.5-c593d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-5a32d7e');
+var VERSION = new Version('6.0.0-beta.5-c593d69');
 
 /**
  * @fileoverview added by tsickle
