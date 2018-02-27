64ec66202106ab71dd7a014cf7eb7d7469605440
Alex Eagle <alexeagle@google.com>
2018-02-27 02:16:37 +0000

    82a7912 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-ef99126
+ * @license Angular v6.0.0-beta.5-82a7912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-ef99126');
+var VERSION = new Version('6.0.0-beta.5-82a7912');
 
 /**
  * @fileoverview added by tsickle
