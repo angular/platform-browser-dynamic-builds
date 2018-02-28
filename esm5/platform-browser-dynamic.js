9d1d59851742203110d1f7567c24a976bc6b3350
Alex Eagle <alexeagle@google.com>
2018-02-28 18:08:07 +0000

    089769d build: update ts-api-guardian version (#22402)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-eccce17
+ * @license Angular v5.2.6-089769d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-eccce17');
+var VERSION = new Version('5.2.6-089769d');
 
 /**
  * @fileoverview added by tsickle
