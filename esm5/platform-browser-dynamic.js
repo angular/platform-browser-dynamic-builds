8c15afab35d94f51c104d29b84050080cc189837
Alex Eagle <alexeagle@google.com>
2018-02-28 22:11:14 +0000

    9eaf1bb feat(ivy): support injecting ChangeDetectorRef (#22469)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-aabe16c
+ * @license Angular v6.0.0-beta.5-9eaf1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-aabe16c');
+var VERSION = new Version('6.0.0-beta.5-9eaf1bb');
 
 /**
  * @fileoverview added by tsickle
