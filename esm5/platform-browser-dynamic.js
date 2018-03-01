386139c80ea02a025a8cd450087f87c8a5b693f2
Alex Eagle <alexeagle@google.com>
2018-03-01 21:26:27 +0000

    49f074f feat(ivy): support array and object literals in binding expressions (#22336)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ec445b5
+ * @license Angular v6.0.0-beta.6-49f074f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-ec445b5');
+var VERSION = new Version('6.0.0-beta.6-49f074f');
 
 /**
  * @fileoverview added by tsickle
