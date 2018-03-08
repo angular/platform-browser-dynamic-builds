bbfd19462d6d3c0eab490a91f55c8753bdd46cc0
Kara Erickson <karakara@google.com>
2018-03-08 05:09:55 +0000

    8fb34bc refactor(forms): deprecate ngModel usage on same field as formControl (#22633)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6d1367d
+ * @license Angular v6.0.0-beta.7-8fb34bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-6d1367d');
+var VERSION = new Version('6.0.0-beta.7-8fb34bc');
 
 /**
  * @fileoverview added by tsickle
