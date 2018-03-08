4b4a337172cb59e5e6a18316e96748ecf4b9b0e8
Kara Erickson <karakara@google.com>
2018-03-08 02:42:14 +0000

    065bcc5 docs: add HeroService to code tabs and fix headers (#22373)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8ad4ae0
+ * @license Angular v6.0.0-beta.7-065bcc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-8ad4ae0');
+var VERSION = new Version('6.0.0-beta.7-065bcc5');
 
 /**
  * @fileoverview added by tsickle
