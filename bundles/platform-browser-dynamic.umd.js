b7fb4a8482ba92ea75ddcbbb24f5dd6bf6bc3f4b
Kara Erickson <karakara@google.com>
2018-03-08 05:29:21 +0000

    8407fcc ci: double our cores on CircleCI (#22641)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f64ee15
+ * @license Angular v6.0.0-beta.7-8407fcc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-f64ee15
+ * @license Angular v6.0.0-beta.7-8407fcc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -691,7 +691,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-f64ee15');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-8407fcc');
 
 /**
  * @fileoverview added by tsickle
