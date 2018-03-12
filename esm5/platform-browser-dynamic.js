86f439129a6c239d1f0a1f27d1b48bce78b34506
Kara Erickson <karakara@google.com>
2018-03-12 05:41:09 +0000

    aa7dba2 feat(ivy): support checkNoChanges (#22710)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-40315be
+ * @license Angular v6.0.0-beta.7-aa7dba2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-40315be');
+var VERSION = new Version('6.0.0-beta.7-aa7dba2');
 
 /**
  * @fileoverview added by tsickle
