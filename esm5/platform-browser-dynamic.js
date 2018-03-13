45095ce29611aaff0f83578908348f0974bec0d7
Kara Erickson <karakara@google.com>
2018-03-13 20:50:08 +0000

    bd9d4df refactor(ivy): remove inputsPropertyName (#22716)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-34e355a
+ * @license Angular v6.0.0-beta.7-bd9d4df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-34e355a');
+var VERSION = new Version('6.0.0-beta.7-bd9d4df');
 
 /**
  * @fileoverview added by tsickle
