e19ca35432304a5cffe5dc8beeedd869ebc71e42
Kara Erickson <karakara@google.com>
2018-03-14 16:16:53 +0000

    37fedd0 feat(core): add task tracking to Testability (#16863)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b1365d1
+ * @license Angular v6.0.0-beta.7-37fedd0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-b1365d1');
+var VERSION = new Version('6.0.0-beta.7-37fedd0');
 
 /**
  * @fileoverview added by tsickle
