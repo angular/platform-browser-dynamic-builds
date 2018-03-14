7242bb4c37ddd4b06ff21339566139dd90de9990
Kara Erickson <karakara@google.com>
2018-03-14 22:52:53 +0000

    5298b2b docs: add changelog for 5.2.9

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-fc6dfc2
+ * @license Angular v5.2.9-5298b2b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.8-fc6dfc2');
+var VERSION = new Version('5.2.9-5298b2b');
 
 /**
  * @fileoverview added by tsickle
