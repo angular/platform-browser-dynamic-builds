d4edeef07f74e0c772b24b62553eff249c7a1bad
Miško Hevery <misko@hevery.com>
2018-03-15 19:11:48 +0000

    7d9de17 build: add release helper scripts (#22378) (#22781)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-db0afa9
+ * @license Angular v5.2.9-7d9de17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-db0afa9
+ * @license Angular v5.2.9-7d9de17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -682,7 +682,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-db0afa9');
+var VERSION = new _angular_core.Version('5.2.9-7d9de17');
 
 /**
  * @fileoverview added by tsickle
