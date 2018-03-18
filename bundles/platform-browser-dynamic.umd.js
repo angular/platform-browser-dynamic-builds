92c1e440ee852e4ec19bf0627d85b09717e9e11c
Misko Hevery <misko@hevery.com>
2018-03-18 19:04:42 +0000

    4f21d37 refactor(ivy): move hostBindings calls out of template (#22833)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-ce63dc6
+ * @license Angular v6.0.0-beta.7-4f21d37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-ce63dc6
+ * @license Angular v6.0.0-beta.7-4f21d37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-ce63dc6');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-4f21d37');
 
 /**
  * @fileoverview added by tsickle
