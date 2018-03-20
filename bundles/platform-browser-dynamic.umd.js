6f7cd82538b8c9493aed19a2f397c818415c980b
Igor Minar <igor@angular.io>
2018-03-20 20:55:31 +0000

    689f351 build: expose flatModuleOutFile option on ng_module (#22814)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-67f570c
+ * @license Angular v6.0.0-beta.7-689f351
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-67f570c
+ * @license Angular v6.0.0-beta.7-689f351
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-67f570c');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-689f351');
 
 /**
  * @fileoverview added by tsickle
