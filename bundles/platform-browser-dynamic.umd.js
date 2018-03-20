0c0e4e889d816736c42d7df3eeb8640156fae8c2
Igor Minar <igor@angular.io>
2018-03-20 05:14:22 +0000

    4648597 build(aio): temporarily pin elements to 6.0.0-beta.8 (#22573)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-2b3de63
+ * @license Angular v6.0.0-beta.7-4648597
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-2b3de63
+ * @license Angular v6.0.0-beta.7-4648597
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-2b3de63');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-4648597');
 
 /**
  * @fileoverview added by tsickle
