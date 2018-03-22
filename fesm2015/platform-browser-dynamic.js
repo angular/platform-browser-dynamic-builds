375800aa2aed221a8cedaa673370139414ee79bb
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:38:58 +0000

    bb58664 build: rm --noimplicit_deps from bazel query (#22912)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-bfe35da
+ * @license Angular v6.0.0-rc.0-bb58664
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-bfe35da');
+const VERSION = new Version('6.0.0-rc.0-bb58664');
 
 /**
  * @fileoverview added by tsickle
