6315f0c02a3c5f1917ac7306d2da4b8ce452ef47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:23:34 +0000

    0d4fe38 fix(service-worker): ignore invalid `only-if-cached` requests (#22883)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-776bb82
+ * @license Angular v5.2.9-0d4fe38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-776bb82');
+var VERSION = new Version('5.2.9-0d4fe38');
 
 /**
  * @fileoverview added by tsickle
