eb6a8dbe76ebabd1f7076192390ac722280a4947
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:07:52 +0000

    6cb1adf ci(aio): add monitoring for angular.io (#22483)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-0c4e371
+ * @license Angular v6.0.0-rc.1-6cb1adf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-0c4e371
+ * @license Angular v6.0.0-rc.1-6cb1adf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-0c4e371');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-6cb1adf');
 
 /**
  * @fileoverview added by tsickle
