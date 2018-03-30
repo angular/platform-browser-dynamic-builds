42aa80eeb5c9601d99dc3316e3d81e8cee7f668c
GitHub <noreply@github.com>
2018-03-30 23:23:38 +0000

    d6bf71a docs(compiler): initial outline of Ivy Compiler Architecture design doc (#22916)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-9cd4465
+ * @license Angular v6.0.0-rc.1-d6bf71a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-9cd4465
+ * @license Angular v6.0.0-rc.1-d6bf71a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-9cd4465');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-d6bf71a');
 
 /**
  * @fileoverview added by tsickle
