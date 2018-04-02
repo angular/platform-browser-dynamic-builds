ad464a1ad40ff60a5f0f62a2561cfd8cb6921400
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:34:55 +0000

    f43fba6 fix(upgrade): correctly handle downgraded `OnPush` components (#22209)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-4473da7
+ * @license Angular v5.2.9-f43fba6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-4473da7');
+var VERSION = new Version('5.2.9-f43fba6');
 
 /**
  * @fileoverview added by tsickle
