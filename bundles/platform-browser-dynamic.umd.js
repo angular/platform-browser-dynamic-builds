2148658a3167e1fe821b91f6ac803fee672eeb14
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 23:11:55 +0000

    55c9fb2 test(ivy): create todo app in ivy (#22921)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-a2330ff
+ * @license Angular v6.0.0-rc.1-55c9fb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-a2330ff
+ * @license Angular v6.0.0-rc.1-55c9fb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-a2330ff');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-55c9fb2');
 
 /**
  * @fileoverview added by tsickle
