fb9c9601960f822103203173f1ac735c266c15e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:13:23 +0000

    9703079 test(router): fix typo in expectation (#23137)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-dd61595
+ * @license Angular v5.2.9-9703079
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-dd61595');
+var VERSION = new Version('5.2.9-9703079');
 
 /**
  * @fileoverview added by tsickle
