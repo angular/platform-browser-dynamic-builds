34cabe84abebe2ab8f45c1243edacdd744e0b7db
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:44:54 +0000

    550433a feat(compiler-cli): lower loadChildren fields to allow dynamic module paths (#23088)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-4506230
+ * @license Angular v6.0.0-rc.1-550433a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-4506230
+ * @license Angular v6.0.0-rc.1-550433a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-4506230');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-550433a');
 
 /**
  * @fileoverview added by tsickle
