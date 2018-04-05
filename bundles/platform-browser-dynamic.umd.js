6142caa6e90926f5b2936055b676552f947705ab
Igor Minar <igor@angular.io>
2018-04-05 23:54:36 +0000

    eb9968a docs(elements): use Angular Elements naming in nav (#23213)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-b5f41f2
+ * @license Angular v6.0.0-rc.2-eb9968a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.2-b5f41f2
+ * @license Angular v6.0.0-rc.2-eb9968a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.2-b5f41f2');
+var VERSION = new _angular_core.Version('6.0.0-rc.2-eb9968a');
 
 /**
  * @fileoverview added by tsickle
