32b8453f98c906f3f4a2cf2e7fae31ec9fbed5ca
Igor Minar <igor@angular.io>
2018-04-05 19:24:21 +0000

    f3d2623 docs: add changelog for 6.0.0-rc.2

---
+++ esm2015/src/resource_loader/resource_loader_cache.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { ResourceLoader } from '@angular/compiler';
+import { ɵglobal as global } from '@angular/core';
+/**
+ * An implementation of ResourceLoader that uses a template cache to avoid doing an actual
+ * ResourceLoader.
+ *
+ * The template cache needs to be built and loaded into window.$templateCache
+ * via a separate mechanism.
+ */
+export class CachedResourceLoader extends ResourceLoader {
+    constructor() {
+        super();
+        this._cache = (/** @type {?} */ (global)).$templateCache;
+        if (this._cache == null) {
+            throw new Error('CachedResourceLoader: Template cache was not found in $templateCache.');
+        }
+    }
+    /**
+     * @param {?} url
+     * @return {?}
+     */
+    get(url) {
+        if (this._cache.hasOwnProperty(url)) {
+            return Promise.resolve(this._cache[url]);
+        }
+        else {
+            return /** @type {?} */ (Promise.reject('CachedResourceLoader: Did not find cached template for ' + url));
+        }
+    }
+}
+function CachedResourceLoader_tsickle_Closure_declarations() {
+    /** @type {?} */
+    CachedResourceLoader.prototype._cache;
+}
+//# sourceMappingURL=resource_loader_cache.js.map
---
+++ esm5/testing/src/platform_core_dynamic_testing.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { COMPILER_OPTIONS, CompilerFactory, Injector, createPlatformFactory } from '@angular/core';
+import { ɵTestingCompilerFactory as TestingCompilerFactory } from '@angular/core/testing';
+import { ɵplatformCoreDynamic as platformCoreDynamic } from '@angular/platform-browser-dynamic';
+import { COMPILER_PROVIDERS, TestingCompilerFactoryImpl } from './compiler_factory';
+/**
+ * Platform for dynamic tests
+ *
+ * \@experimental
+ */
+export var /** @type {?} */ platformCoreDynamicTesting = createPlatformFactory(platformCoreDynamic, 'coreDynamicTesting', [
+    { provide: COMPILER_OPTIONS, useValue: { providers: COMPILER_PROVIDERS }, multi: true }, {
+        provide: TestingCompilerFactory,
+        useClass: TestingCompilerFactoryImpl,
+        deps: [Injector, CompilerFactory]
+    }
+]);
+//# sourceMappingURL=platform_core_dynamic_testing.js.map
