e5fbac8b5aabb925ae3bfa38fca979638baaa31d
Igor Minar <igor@angular.io>
2018-04-05 17:46:45 +0000

    524e5d8 refactor(ivy): adjust types (#23167)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-79cecf9
+ * @license Angular v6.0.0-rc.1-524e5d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-79cecf9');
+var VERSION = new Version('6.0.0-rc.1-524e5d8');
 
 /**
  * @fileoverview added by tsickle
