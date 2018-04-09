5e6c6b10a2911d7aac0d21fef8ec4c2f1d0dc6a3
Igor Minar <igor@angular.io>
2018-04-09 22:40:42 +0000

    3e8eef6 fix(bazel): set rollup output.name and amd.id (#23274)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-1fac5f4
+ * @license Angular v6.0.0-rc.3-3e8eef6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-1fac5f4');
+var VERSION = new Version('6.0.0-rc.3-3e8eef6');
 
 /**
  * @fileoverview added by tsickle
