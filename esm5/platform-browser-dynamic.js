59b593759fb8b164fd16a52509be0f87b5e87bc6
Igor Minar <igor@angular.io>
2018-04-09 22:39:57 +0000

    bda6908 docs(core): update directives documentation (#23255)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a1231be
+ * @license Angular v5.2.9-bda6908
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-a1231be');
+var VERSION = new Version('5.2.9-bda6908');
 
 /**
  * @fileoverview added by tsickle
