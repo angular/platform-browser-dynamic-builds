5e556f41d66cc6a994d8f664cfe770ff4bedc59e
Igor Minar <igor@angular.io>
2018-04-12 06:40:16 +0000

    7e8cee6 build: fix aio size tracking, we need to use node_modules local to aio (#23328)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-37d2cb4
+ * @license Angular v6.0.0-rc.3-7e8cee6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-37d2cb4');
+var VERSION = new Version('6.0.0-rc.3-7e8cee6');
 
 /**
  * @fileoverview added by tsickle
