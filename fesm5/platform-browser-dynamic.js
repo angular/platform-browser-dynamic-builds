8702470111bd64fcfc74618452ea3b4499c7f567
Igor Minar <iminar@google.com>
2018-04-13 07:30:37 +0000

    a025f7e refactor(language-service): fix typo on type.ts language-service

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-b551f84
+ * @license Angular v6.0.0-rc.4-a025f7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-b551f84');
+var VERSION = new Version('6.0.0-rc.4-a025f7e');
 
 /**
  * @fileoverview added by tsickle
