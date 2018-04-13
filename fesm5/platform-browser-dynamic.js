97abc26c8fb547ac2fc774401fb5ff170a40eb17
Igor Minar <igor@angular.io>
2018-04-13 21:53:48 +0000

    f4017ce fix(ivy): Update Todo app to take advantage of optional injector (#23345)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-490772e
+ * @license Angular v6.0.0-rc.4-f4017ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-490772e');
+var VERSION = new Version('6.0.0-rc.4-f4017ce');
 
 /**
  * @fileoverview added by tsickle
