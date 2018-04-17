f549fc531dfcc9a09cd4d52a9abdef520648abdc
Jason Aden <jasonaden@google.com>
2018-04-17 21:45:26 +0000

    402f452 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- esm5/platform-browser-dynamic.js
+++ esm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-d2e7c99
+ * @license Angular v5.2.10-402f452
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -651,7 +651,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.10-d2e7c99');
+var VERSION = new Version('5.2.10-402f452');
 
 /**
  * @fileoverview added by tsickle
