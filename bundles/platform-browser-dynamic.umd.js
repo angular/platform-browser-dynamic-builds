3c9ad22bc2ecf15118a2430edf437103dfd55634
Jason Aden <jasonaden@google.com>
2018-04-17 21:28:48 +0000

    7be7abd refactor(animations): use a const enum to avoid compilation side effects (#23402)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-5a1ddee
+ * @license Angular v6.0.0-rc.5-7be7abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-5a1ddee
+ * @license Angular v6.0.0-rc.5-7be7abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -699,7 +699,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-5a1ddee');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-7be7abd');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-5a1ddee
+ * @license Angular v6.0.0-rc.5-7be7abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-5a1ddee');
+var VERSION = new Version('6.0.0-rc.5-7be7abd');
 
 /**
  * @fileoverview added by tsickle
