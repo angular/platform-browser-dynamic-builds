e170209093c0cf2c0bf5d203c730978bef626185
Victor Berchet <victor@suumit.com>
2018-04-19 23:56:11 +0000

    9757347 feat(ivy): add an IVY local the compiler which avoids analyzeModule (#23441)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-1d1e75e
+ * @license Angular v6.0.0-rc.5-9757347
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -574,7 +574,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-1d1e75e');
+const VERSION = new Version('6.0.0-rc.5-9757347');
 
 /**
  * @fileoverview added by tsickle
