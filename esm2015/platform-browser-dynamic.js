ee62a776134056751a4f471cdafd358baa1a20fa
GitHub <noreply@github.com>
2018-04-20 13:18:57 +0000

    ab41920 fix(compiler): avoid a crash in ngc-wrapped.

---
+++ esm2015/platform-browser-dynamic.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { VERSION, JitCompilerFactory, RESOURCE_CACHE_PROVIDER, platformBrowserDynamic, ɵCompilerImpl, ɵplatformCoreDynamic, ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, ɵResourceLoaderImpl } from './public_api';
+export { CachedResourceLoader as ɵa } from './src/resource_loader/resource_loader_cache';
+//# sourceMappingURL=platform-browser-dynamic.js.map
---
+++ esm5/testing/testing.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { platformBrowserDynamicTesting, BrowserDynamicTestingModule, ɵDOMTestComponentRenderer, ɵplatformCoreDynamicTesting } from './public_api';
+export { COMPILER_PROVIDERS as ɵa, TestingCompilerFactoryImpl as ɵb } from './src/compiler_factory';
+//# sourceMappingURL=testing.js.map
