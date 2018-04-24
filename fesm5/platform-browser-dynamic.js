150ee091ff4fb2f5773e53e2e9dc87151f562d69
Victor Berchet <victor@suumit.com>
2018-04-24 18:37:32 +0000

    540626a build(common): mark locales files as side-effect-full (#23509)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-391bfce
+ * @license Angular v6.0.0-rc.5-540626a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-391bfce');
+var VERSION = new Version('6.0.0-rc.5-540626a');
 
 /**
  * @fileoverview added by tsickle
