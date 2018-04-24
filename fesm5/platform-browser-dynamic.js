b477f3a3990d2843f91ed371151266f731f1bb06
Victor Berchet <victor@suumit.com>
2018-04-24 18:38:06 +0000

    b1d03fe fix(ivy): properly destroy view trees where root is an embedded view without children (#23482)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+77.sha-dab5df9
+ * @license Angular v6.0.0-rc.5+76.sha-b1d03fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -431,7 +431,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+77.sha-dab5df9');
+var VERSION = new Version('6.0.0-rc.5+76.sha-b1d03fe');
 
 /**
  * @license
