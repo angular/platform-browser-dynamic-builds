a1d4e5df2cf8001c93f550172a88d04931bef38c
Igor Minar <igor@angular.io>
2018-04-25 20:46:28 +0000

    64bf6ed docs: update glossary architectural terms (#23045)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-adf6235
+ * @license Angular v6.0.0-rc.5-64bf6ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-adf6235');
+var VERSION = new Version('6.0.0-rc.5-64bf6ed');
 
 /**
  * @fileoverview added by tsickle
