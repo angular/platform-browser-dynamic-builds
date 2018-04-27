5961113e6c613b93c76afb42545305a67eefbbab
Igor Minar <igor@angular.io>
2018-04-27 18:46:26 +0000

    4008e36 release: cut the v6.0.0-rc.6 release

---
+++ esm5/src/platform-browser-dynamic.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { ResourceLoader } from '@angular/compiler';
+import { createPlatformFactory } from '@angular/core';
+import { platformCoreDynamic } from './platform_core_dynamic';
+import { INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from './platform_providers';
+import { CachedResourceLoader } from './resource_loader/resource_loader_cache';
+export { ɵCompilerImpl, ɵplatformCoreDynamic, ɵINTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS, ɵResourceLoaderImpl } from './private_export';
+export { VERSION } from './version';
+export { JitCompilerFactory } from './compiler_factory';
+/**
+ * \@experimental
+ */
+export var /** @type {?} */ RESOURCE_CACHE_PROVIDER = [{ provide: ResourceLoader, useClass: CachedResourceLoader, deps: [] }];
+/**
+ *
+ */
+export var /** @type {?} */ platformBrowserDynamic = createPlatformFactory(platformCoreDynamic, 'browserDynamic', INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS);
+//# sourceMappingURL=platform-browser-dynamic.js.map
