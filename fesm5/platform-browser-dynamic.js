85be0132c1cbf810e453bee46dbbffa404f93264
Igor Minar <igor@angular.io>
2018-05-02 23:16:42 +0000

    91d4da0 docs: add missing link to bootstrapping section (#23214)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-22eb8e2
+ * @license Angular v6.0.0-rc.6-91d4da0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.6-22eb8e2');
+var VERSION = new Version('6.0.0-rc.6-91d4da0');
 
 /**
  * @fileoverview added by tsickle
