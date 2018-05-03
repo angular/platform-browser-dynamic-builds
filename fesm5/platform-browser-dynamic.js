7ccb206e6e9a9cdc74f758db2ad3dc44c41f66c8
Igor Minar <igor@angular.io>
2018-05-03 21:49:46 +0000

    9027818 docs(aio): Upgrade server-side rendering example to Angular V6 (#23649)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-5bd8c68
+ * @license Angular v6.0.0-9027818
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -668,7 +668,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-5bd8c68');
+var VERSION = new Version('6.0.0-9027818');
 
 /**
  * @fileoverview added by tsickle
