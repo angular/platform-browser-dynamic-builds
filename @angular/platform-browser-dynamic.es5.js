181ed2da22532fdd1cac663d043901ecd80e9c52
Igor Minar <igor@angular.io>
2018-05-03 22:30:32 +0000

    b19216d fix(aio): correct project id for deployment of archive sites

--- @angular/platform-browser-dynamic.es5.js
+++ @angular/platform-browser-dynamic.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.7-84fc1a3
+ * @license Angular v4.4.7-b19216d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -133,7 +133,7 @@ var CachedResourceLoader = (function (_super) {
 /**
  * @stable
  */
-var VERSION = new Version('4.4.7-84fc1a3');
+var VERSION = new Version('4.4.7-b19216d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
