3899e1a8f4efd35098bb5f70a6c2ee11f88b8b26
Igor Minar <igor@angular.io>
2018-05-05 15:39:05 +0000

    e351896 style(animations): fix short param names (#23668)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+136.sha-7e9649b
+ * @license Angular v6.0.0-rc.5+138.sha-e351896
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -431,7 +431,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+136.sha-7e9649b');
+var VERSION = new Version('6.0.0-rc.5+138.sha-e351896');
 
 /**
  * @license
