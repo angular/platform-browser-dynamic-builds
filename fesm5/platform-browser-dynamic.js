e038098d927f0ff5074a0d05ce49230117de2f75
Igor Minar <igor@angular.io>
2018-05-05 16:01:39 +0000

    19262d9 Revert "style(animations): fix short param names (#23668)"

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+25.sha-3807599
+ * @license Angular v6.0.0+27.sha-19262d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -431,7 +431,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0+25.sha-3807599');
+var VERSION = new Version('6.0.0+27.sha-19262d9');
 
 /**
  * @license
