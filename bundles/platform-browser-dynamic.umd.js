8cddd521320754baab2f49bd5f669ad1e6891c7b
Igor Minar <igor@angular.io>
2018-05-08 21:16:27 +0000

    fc03427 fix(core): call ngOnDestroy on all services that have it (#23755)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+140.sha-77ff72f
+ * @license Angular v6.0.0-rc.5+141.sha-fc03427
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -498,7 +498,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+140.sha-77ff72f');
+var VERSION = new core.Version('6.0.0-rc.5+141.sha-fc03427');
 
 /**
  * @license
