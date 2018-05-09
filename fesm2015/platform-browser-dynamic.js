88e38bb82e0391ece63183bd5d47b57dd2fc492e
Igor Minar <igor@angular.io>
2018-05-09 19:32:20 +0000

    2254ac2 fix(service-worker): correctly handle requests with empty `clientId` (#23625)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+38.sha-2b31b6d
+ * @license Angular v6.0.0+37.sha-2254ac2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  *
  */
-const VERSION = new Version('6.0.0+38.sha-2b31b6d');
+const VERSION = new Version('6.0.0+37.sha-2254ac2');
 
 /**
  * @fileoverview added by tsickle
