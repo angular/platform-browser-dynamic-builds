5f95eec666b91d16c4340aa9b43df2ac0410177a
Igor Minar <igor@angular.io>
2018-05-10 15:17:55 +0000

    f109791 ci: add config for size plugin of the github rebot (#23665)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+37.sha-2254ac2
+ * @license Angular v6.0.0+40.sha-f109791
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -431,7 +431,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new Version('6.0.0+37.sha-2254ac2');
+var VERSION = new Version('6.0.0+40.sha-f109791');
 
 /**
  * @license
