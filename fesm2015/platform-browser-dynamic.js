c5427182b5dff2044190046a39935d5a1e42584b
Igor Minar <igor@angular.io>
2018-05-10 19:28:04 +0000

    484233f docs(aio): add Alain Chautard in GDE list (#23783)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+42.sha-3d8799b
+ * @license Angular v6.0.0+43.sha-484233f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -575,7 +575,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  *
  */
-const VERSION = new Version('6.0.0+42.sha-3d8799b');
+const VERSION = new Version('6.0.0+43.sha-484233f');
 
 /**
  * @fileoverview added by tsickle
