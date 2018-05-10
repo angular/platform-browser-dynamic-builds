e02103459bdd511b333b2772640f773293f03540
Igor Minar <igor@angular.io>
2018-05-10 15:19:59 +0000

    553a680 fix(router): correct the segment parsing so it won't break on ampersand (#23684)

--- bundles/platform-browser-dynamic.umd.js
+++ bundles/platform-browser-dynamic.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+154.sha-f1e4a15
+ * @license Angular v6.0.0-rc.5+157.sha-553a680
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -498,7 +498,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+154.sha-f1e4a15');
+var VERSION = new core.Version('6.0.0-rc.5+157.sha-553a680');
 
 /**
  * @license
