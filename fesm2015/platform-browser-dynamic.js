835a5fdc2908af0aac851e406b7d5022fc0ec433
Miško Hevery <misko@hevery.com>
2018-06-14 00:27:38 +0000

    39af314 build(aio): add github links to API doc members (#24000)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+1.sha-03f93b3
+ * @license Angular v6.1.0-beta.1+22.sha-39af314
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -428,7 +428,7 @@ class CachedResourceLoader extends ResourceLoader {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+1.sha-03f93b3');
+const VERSION = new Version('6.1.0-beta.1+22.sha-39af314');
 
 /**
  * @license
