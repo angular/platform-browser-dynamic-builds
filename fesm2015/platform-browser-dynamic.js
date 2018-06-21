996f1c79f1f119db5146fc9c70d73c9c6c9c04c7
Miško Hevery <misko@hevery.com>
2018-06-21 20:48:56 +0000

    10da6a4 refactor(ivy): first pass at extracting ReflectionHost for abstract reflection (#24541)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+7.sha-84272e2
+ * @license Angular v6.1.0-beta.2+8.sha-10da6a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -428,7 +428,7 @@ class CachedResourceLoader extends ResourceLoader {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
+const VERSION = new Version('6.1.0-beta.2+8.sha-10da6a4');
 
 /**
  * @license
