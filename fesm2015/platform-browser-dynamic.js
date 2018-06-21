0f94106f1f8a24c3d3c80667b22bb62e80466e15
Miško Hevery <misko@hevery.com>
2018-06-21 00:26:42 +0000

    a269658 feat(docs-infra): Add GitHub and Twitter external icon links to topnav toolbar (#24542)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+2.sha-8b81682
+ * @license Angular v6.1.0-beta.2+1.sha-a269658
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -428,7 +428,7 @@ class CachedResourceLoader extends ResourceLoader {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+2.sha-8b81682');
+const VERSION = new Version('6.1.0-beta.2+1.sha-a269658');
 
 /**
  * @license
