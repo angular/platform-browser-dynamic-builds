44bc70f34270345cbcb5363836ecfcbe57794c9b
Miško Hevery <misko@hevery.com>
2018-07-06 19:08:47 +0000

    f62876b fix(ivy): pipes are pure by default (#24750)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+46.sha-f62876b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -597,7 +597,7 @@ class CachedResourceLoader extends ResourceLoader {
  * found in the LICENSE file at https://angular.io/license
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+const VERSION = new Version('6.1.0-beta.3+46.sha-f62876b');
 
 /**
  * @fileoverview added by tsickle
