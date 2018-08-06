c16e00291b85c6ba4276c9ea65106bf10ab21048
Kara Erickson <karakara@google.com>
2018-08-06 17:47:54 +0000

    97b5cb2 docs(aio): add Made with Angular (#21297)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+18.sha-26a15cc
+ * @license Angular v7.0.0-beta.0+22.sha-97b5cb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -19,7 +19,6 @@ const MODULE_SUFFIX = '';
 const builtinExternalReferences = createBuiltinExternalReferencesMap();
 class JitReflector {
     constructor() {
-        this.builtinExternalReferences = new Map();
         this.reflectionCapabilities = new ɵReflectionCapabilities();
     }
     /**
@@ -548,7 +547,7 @@ class CachedResourceLoader extends ResourceLoader {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+18.sha-26a15cc');
+const VERSION = new Version('7.0.0-beta.0+22.sha-97b5cb2');
 
 /**
  * @fileoverview added by tsickle
