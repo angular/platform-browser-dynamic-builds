e1aaa7612d27a79cf0c2d28244c5a7a8fa670578
Ben Lesh <ben@benlesh.com>
2018-08-16 00:21:50 +0000

    1f11039 refactor(ivy): cleanup the public API for core/testing (#25492)

--- bundles/platform-browser-dynamic-testing.umd.js
+++ bundles/platform-browser-dynamic-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+10.sha-b64fed1
+ * @license Angular v7.0.0-beta.1+52.sha-1f11039
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -51,7 +51,7 @@
      */
     var DOMTestComponentRenderer = /** @class */ (function (_super) {
         __extends(DOMTestComponentRenderer, _super);
-        function DOMTestComponentRenderer(_doc /** TODO #9100 */) {
+        function DOMTestComponentRenderer(_doc) {
             var _this = _super.call(this) || this;
             _this._doc = _doc;
             return _this;
@@ -82,119 +82,6 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var _nextReferenceId = 0;
-    var MetadataOverrider = /** @class */ (function () {
-        function MetadataOverrider() {
-            this._references = new Map();
-        }
-        /**
-         * Creates a new instance for the given metadata class
-         * based on an old instance and overrides.
-         */
-        MetadataOverrider.prototype.overrideMetadata = function (metadataClass, oldMetadata, override) {
-            var props = {};
-            if (oldMetadata) {
-                _valueProps(oldMetadata).forEach(function (prop) { return props[prop] = oldMetadata[prop]; });
-            }
-            if (override.set) {
-                if (override.remove || override.add) {
-                    throw new Error("Cannot set and add/remove " + core.ɵstringify(metadataClass) + " at the same time!");
-                }
-                setMetadata(props, override.set);
-            }
-            if (override.remove) {
-                removeMetadata(props, override.remove, this._references);
-            }
-            if (override.add) {
-                addMetadata(props, override.add);
-            }
-            return new metadataClass(props);
-        };
-        return MetadataOverrider;
-    }());
-    function removeMetadata(metadata, remove, references) {
-        var removeObjects = new Set();
-        var _loop_1 = function (prop) {
-            var removeValue = remove[prop];
-            if (removeValue instanceof Array) {
-                removeValue.forEach(function (value) { removeObjects.add(_propHashKey(prop, value, references)); });
-            }
-            else {
-                removeObjects.add(_propHashKey(prop, removeValue, references));
-            }
-        };
-        for (var prop in remove) {
-            _loop_1(prop);
-        }
-        var _loop_2 = function (prop) {
-            var propValue = metadata[prop];
-            if (propValue instanceof Array) {
-                metadata[prop] = propValue.filter(function (value) { return !removeObjects.has(_propHashKey(prop, value, references)); });
-            }
-            else {
-                if (removeObjects.has(_propHashKey(prop, propValue, references))) {
-                    metadata[prop] = undefined;
-                }
-            }
-        };
-        for (var prop in metadata) {
-            _loop_2(prop);
-        }
-    }
-    function addMetadata(metadata, add) {
-        for (var prop in add) {
-            var addValue = add[prop];
-            var propValue = metadata[prop];
-            if (propValue != null && propValue instanceof Array) {
-                metadata[prop] = propValue.concat(addValue);
-            }
-            else {
-                metadata[prop] = addValue;
-            }
-        }
-    }
-    function setMetadata(metadata, set) {
-        for (var prop in set) {
-            metadata[prop] = set[prop];
-        }
-    }
-    function _propHashKey(propName, propValue, references) {
-        var replacer = function (key, value) {
-            if (typeof value === 'function') {
-                value = _serializeReference(value, references);
-            }
-            return value;
-        };
-        return propName + ":" + JSON.stringify(propValue, replacer);
-    }
-    function _serializeReference(ref, references) {
-        var id = references.get(ref);
-        if (!id) {
-            id = "" + core.ɵstringify(ref) + _nextReferenceId++;
-            references.set(ref, id);
-        }
-        return id;
-    }
-    function _valueProps(obj) {
-        var props = [];
-        // regular public props
-        Object.keys(obj).forEach(function (prop) {
-            if (!prop.startsWith('_')) {
-                props.push(prop);
-            }
-        });
-        // getters
-        var proto = obj;
-        while (proto = Object.getPrototypeOf(proto)) {
-            Object.keys(proto).forEach(function (protoProp) {
-                var desc = Object.getOwnPropertyDescriptor(proto, protoProp);
-                if (!protoProp.startsWith('_') && desc && 'get' in desc) {
-                    props.push(protoProp);
-                }
-            });
-        }
-        return props;
-    }
 
     /**
      * @license
@@ -228,7 +115,7 @@
             this._directiveResolver = _directiveResolver;
             this._pipeResolver = _pipeResolver;
             this._moduleResolver = _moduleResolver;
-            this._overrider = new MetadataOverrider();
+            this._overrider = new testing.ɵMetadataOverrider();
         }
         Object.defineProperty(TestingCompilerImpl.prototype, "injector", {
             get: function () { return this._compiler.injector; },
