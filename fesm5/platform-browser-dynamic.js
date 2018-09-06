e33c0d221d72745d9fd1a57272ec6adc8b3b1e38
Igor Minar <igor@angular.io>
2018-09-06 23:41:12 +0000

    e244b51 build(bazel): update bazel integration to build angular from source (#25774)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+7.sha-f67229e
+ * @license Angular v6.1.7+3.sha-e244b51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -428,7 +428,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.7+7.sha-f67229e');
+var VERSION = new Version('6.1.7+3.sha-e244b51');
 
 /**
  * @license
