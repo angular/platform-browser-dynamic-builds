36a8fe64bb01c47a35383adb83a10bfb4adc3ad5
Jason Aden <jasonaden@google.com>
2018-10-05 21:23:41 +0000

    b8a081a docs(service-worker): improve API docs for `SwPush`/`SwUpdate` (#23138)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+57.sha-1c1fd98
+ * @license Angular v6.1.9+60.sha-b8a081a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -548,7 +548,7 @@ class CachedResourceLoader extends ResourceLoader {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+57.sha-1c1fd98');
+const VERSION = new Version('6.1.9+60.sha-b8a081a');
 
 /**
  * @fileoverview added by tsickle
