5e84cdbb30bad6e2dbf31d49741f37038fe6e1f8
Igor Minar <igor@angular.io>
2018-10-23 21:37:16 +0000

    ee0b857 build: rename the ivy compile mode 'local' to 'aot' (#26686)

---
+++ testing/src/dom_test_component_renderer.d.ts
@@ -0,0 +1,18 @@
+import * as i0 from '@angular/core';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { TestComponentRenderer } from '@angular/core/testing';
+/**
+ * A DOM based implementation of the TestComponentRenderer.
+ */
+export declare class DOMTestComponentRenderer extends TestComponentRenderer {
+    private _doc;
+    constructor(_doc: any);
+    insertRootElement(rootElId: string): void;
+    static ngInjectableDef: i0.ɵInjectableDef<DOMTestComponentRenderer>;
+}
