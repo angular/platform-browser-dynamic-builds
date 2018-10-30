0d094ea13af785ac5af96ddf4cc8a43c583f9cc3
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 22:58:48 +0000

    332394d build(bazel): generalize fix for AMD name for flat module out file (#26837)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+73.sha-62b4ff52
+ * @license Angular v7.1.0-beta.0+74.sha-332394d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -554,7 +554,7 @@ class CachedResourceLoader extends ResourceLoader {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+73.sha-62b4ff52');
+const VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
 
 /**
  * @fileoverview added by tsickle
