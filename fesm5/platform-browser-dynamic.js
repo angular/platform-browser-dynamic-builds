ea53c03e72ddbf67f57dfe78f4b1c7078f0f6088
Kara Erickson <karakara@google.com>
2018-11-05 23:37:23 +0000

    099d1a6 docs(forms): update reactive form directives API reference (#26823)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+73.sha-6744b19
+ * @license Angular v7.1.0-beta.1+74.sha-099d1a6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+73.sha-6744b19');
+var VERSION = new Version('7.1.0-beta.1+74.sha-099d1a6');
 
 /**
  * @license
