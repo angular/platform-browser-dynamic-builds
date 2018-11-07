1642ad1f9834111f001d02ec15bee3e3a80f22e9
Kara Erickson <karakara@google.com>
2018-11-07 01:32:19 +0000

    931a363 fix(ivy): use `window.ng` instead of `window.ngDev` for debug tooling (#26951)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+80.sha-3ca1a57
+ * @license Angular v7.1.0-beta.1+81.sha-931a363
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+80.sha-3ca1a57');
+var VERSION = new Version('7.1.0-beta.1+81.sha-931a363');
 
 /**
  * @license
