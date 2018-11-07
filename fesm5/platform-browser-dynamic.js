5ea55201e618d26f3aeecb4db4f41fe7092b9dea
Kara Erickson <karakara@google.com>
2018-11-07 19:03:20 +0000

    a4398aa docs(forms): update description for FormBuilder.group (#26970)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+83.sha-53e4e0c
+ * @license Angular v7.1.0-beta.1+84.sha-a4398aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+83.sha-53e4e0c');
+var VERSION = new Version('7.1.0-beta.1+84.sha-a4398aa');
 
 /**
  * @license
