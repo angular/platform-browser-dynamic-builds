bddd89396f08d2781b5c44fbb79ab4f2e792d887
Misko Hevery <misko@hevery.com>
2018-11-21 23:55:50 +0000

    dc300c5 feat(ivy): render flags support in host bindings function (FW-649) (#27204)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0
+ * @license Angular v7.1.0+1.sha-dc300c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0');
+var VERSION = new Version('7.1.0+1.sha-dc300c5');
 
 /**
  * @license
