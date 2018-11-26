e83dba008786686c8ed4513dd8cca843e41966b2
Jason Aden <jasonaden@google.com>
2018-11-26 20:45:23 +0000

    f45aedc build(docs-infra): upgrade `npm-run-all` to latest version for security (#27274)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+1.sha-dc300c5
+ * @license Angular v7.1.0+11.sha-f45aedc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+1.sha-dc300c5');
+var VERSION = new Version('7.1.0+11.sha-f45aedc');
 
 /**
  * @license
