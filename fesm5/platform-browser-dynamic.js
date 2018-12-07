547ac4805a2d07207eba857e9cba48bbf6bc6260
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:03:13 +0000

    15ca1bb style: run latest buildifier binary (#27489)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+7.sha-f6a67c9
+ * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+7.sha-f6a67c9');
+var VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
 
 /**
  * @license
