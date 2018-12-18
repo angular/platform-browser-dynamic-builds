d16bea18920ffdf014c8d74209cd58497e4c2bbc
Miško Hevery <misko@hevery.com>
2018-12-18 05:17:50 +0000

    e98c57a refactor(core): rename spec file to match case of src file

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+74.sha-4bf8d64
+ * @license Angular v7.2.0-beta.2+75.sha-e98c57a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+74.sha-4bf8d64');
+var VERSION = new Version('7.2.0-beta.2+75.sha-e98c57a');
 
 /**
  * @license
