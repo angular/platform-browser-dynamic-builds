dcbc188d2aadca25cae2a3ccfdc77e444ad80cfe
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 22:06:54 +0000

    4b67b0a fix(ivy): adding TestBed.overrideProvider support (#27693)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+22.sha-509aa61
+ * @license Angular v7.2.0-rc.0+25.sha-4b67b0a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+22.sha-509aa61');
+var VERSION = new Version('7.2.0-rc.0+25.sha-4b67b0a');
 
 /**
  * @license
