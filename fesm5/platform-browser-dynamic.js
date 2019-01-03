93f729627fc6efb09cc8f1837f42ff9916464676
Ben Lesh <ben@benlesh.com>
2019-01-03 18:23:19 +0000

    1e6c9be fix(bazel): unable to launch protractor test on windows (#27850)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+48.sha-880b4aa
+ * @license Angular v7.2.0-rc.0+46.sha-1e6c9be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+48.sha-880b4aa');
+var VERSION = new Version('7.2.0-rc.0+46.sha-1e6c9be');
 
 /**
  * @license
