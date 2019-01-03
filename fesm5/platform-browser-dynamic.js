9d5ed8f726827efdc0f85cbcddb87e0ac8ba0aff
Ben Lesh <ben@benlesh.com>
2019-01-03 18:21:18 +0000

    a979001 fix(ivy): resolve `forwardRefs` correctly in TestBed (JIT) (#27737)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+42.sha-7df9040
+ * @license Angular v7.2.0-rc.0+45.sha-a979001
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+42.sha-7df9040');
+var VERSION = new Version('7.2.0-rc.0+45.sha-a979001');
 
 /**
  * @license
