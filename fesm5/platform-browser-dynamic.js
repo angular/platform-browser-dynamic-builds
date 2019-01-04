950d73b4a33af186fdc20418992c28f4d9fc7e9c
Kara Erickson <karakara@google.com>
2019-01-04 22:38:44 +0000

    5d3dcfc fix(ivy): ensure @animation host bindings/listeners work properly (#27896)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+61.sha-0bd9deb
+ * @license Angular v7.2.0-rc.0+62.sha-5d3dcfc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+61.sha-0bd9deb');
+var VERSION = new Version('7.2.0-rc.0+62.sha-5d3dcfc');
 
 /**
  * @license
