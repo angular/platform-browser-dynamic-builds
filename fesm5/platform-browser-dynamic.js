311b285a31293eee30ffce6ce7d05c224111f5ec
Kara Erickson <karakara@google.com>
2019-01-06 02:07:23 +0000

    224d787 ci: replace our pullapprove setup with GitHub CODEOWNERS (#27690)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+68.sha-7adcbb3
+ * @license Angular v7.2.0-rc.0+69.sha-224d787
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+68.sha-7adcbb3');
+var VERSION = new Version('7.2.0-rc.0+69.sha-224d787');
 
 /**
  * @license
