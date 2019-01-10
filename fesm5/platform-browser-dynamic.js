48e3a17e31b0919505c34831e44100f72ad86d07
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:30:39 +0000

    c5ab3e8 fix(ivy): proper resolution of Enums in Component decorator (#27971)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+88.sha-142553a
+ * @license Angular v7.2.0+89.sha-c5ab3e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+88.sha-142553a');
+var VERSION = new Version('7.2.0+89.sha-c5ab3e8');
 
 /**
  * @license
