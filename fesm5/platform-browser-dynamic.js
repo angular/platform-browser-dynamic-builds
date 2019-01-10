7420b7663eaa914d4574cc841346e69ed0329cd7
Andrew Kushnir <akushnir@google.com>
2019-01-10 00:17:57 +0000

    8934b73 test(ivy): re-enable passing upgrade tests (#28030)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+80.sha-11325ba
+ * @license Angular v7.2.0+81.sha-8934b73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+80.sha-11325ba');
+var VERSION = new Version('7.2.0+81.sha-8934b73');
 
 /**
  * @license
