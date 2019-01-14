d4e837bc0c6cd60d5eead51445aa3636f2e8b8cb
Andrew Kushnir <akushnir@google.com>
2019-01-14 22:30:59 +0000

    d12db4e fix(bazel): incorrectly always uses ngc-wrapped from "npm" workspace (#28137)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+161.sha-e8495b4
+ * @license Angular v7.2.0+169.sha-d12db4e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+161.sha-e8495b4');
+var VERSION = new Version('7.2.0+169.sha-d12db4e');
 
 /**
  * @license
