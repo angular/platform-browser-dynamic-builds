58b324c9a392197f2c5560f1b54c988fa0feda2a
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:32:12 +0000

    78bc21c docs: fix scripts section and some minor issues in universal documentation (#26444)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+147.sha-48a03fc
+ * @license Angular v7.2.0+148.sha-78bc21c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+147.sha-48a03fc');
+var VERSION = new Version('7.2.0+148.sha-78bc21c');
 
 /**
  * @license
