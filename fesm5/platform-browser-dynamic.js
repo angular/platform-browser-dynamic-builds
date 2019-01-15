a52c0fd98316f064e8354755750190a8bc01f81c
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:35:32 +0000

    60fecc1 fixup! refactor(ivy): simplify context discover based on feedback (#28101)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+174.sha-fa53150
+ * @license Angular v7.2.0+176.sha-60fecc1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+174.sha-fa53150');
+var VERSION = new Version('7.2.0+176.sha-60fecc1');
 
 /**
  * @license
