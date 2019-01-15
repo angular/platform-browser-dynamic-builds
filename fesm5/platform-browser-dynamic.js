36d4571ab41b2d175491560bc79b80124cdf80d1
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:43:31 +0000

    ce3a746 perf(platform-server): use shared `DomElementSchemaRegistry` instance (#28150) (#28151)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+187.sha-bc02e31
+ * @license Angular v7.2.0+188.sha-ce3a746
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+187.sha-bc02e31');
+var VERSION = new Version('7.2.0+188.sha-ce3a746');
 
 /**
  * @license
