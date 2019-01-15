4454bf8a557c7b14d914d5930afdcc707d2d894a
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:11:46 +0000

    850b867 docs: add api doc for switch directives (#27378)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+183.sha-a241200
+ * @license Angular v7.2.0+184.sha-850b867
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+183.sha-a241200');
+var VERSION = new Version('7.2.0+184.sha-850b867');
 
 /**
  * @license
