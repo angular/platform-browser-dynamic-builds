cae02a561ab8a49954a5c49691c97679fdf74c40
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:01:11 +0000

    d336bff fix(ivy): add missging getDirectives export (#28259)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+41.sha-f99082f
+ * @license Angular v8.0.0-beta.0+42.sha-d336bff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+41.sha-f99082f');
+var VERSION = new Version('8.0.0-beta.0+42.sha-d336bff');
 
 /**
  * @license
