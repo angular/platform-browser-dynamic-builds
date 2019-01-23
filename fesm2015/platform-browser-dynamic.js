4633b64cbb38142745b4ec12ec69f457948af7e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:18:32 +0000

    5877b3f refactor: remove unused parameter in _main method invocation (#28203)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2
+ * @license Angular v7.2.2+3.sha-5877b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -565,7 +565,7 @@ class CachedResourceLoader extends ResourceLoader {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.2');
+const VERSION = new Version('7.2.2+3.sha-5877b3f');
 
 /**
  * @fileoverview added by tsickle
