4a1c7e4204f7a47fd9431b5133f8c4563651de65
Jason Aden <jasonaden@google.com>
2019-01-23 23:59:34 +0000

    d4ecffe refactor: remove obsolete font-smoothing property (#28174)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+18.sha-cbd6264
+ * @license Angular v8.0.0-beta.1+20.sha-d4ecffe
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+18.sha-cbd6264');
+var VERSION = new Version('8.0.0-beta.1+20.sha-d4ecffe');
 
 /**
  * @license
