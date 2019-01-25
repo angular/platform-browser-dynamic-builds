da89da3c317d2702c1e013344273a4059da8de35
Jason Aden <jasonaden@google.com>
2019-01-25 21:27:39 +0000

    3d5a919 refactor(ivy): clean up TNode not depending on LView (#28354)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+41.sha-2da82db
+ * @license Angular v8.0.0-beta.1+43.sha-3d5a919
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+41.sha-2da82db');
+var VERSION = new Version('8.0.0-beta.1+43.sha-3d5a919');
 
 /**
  * @license
