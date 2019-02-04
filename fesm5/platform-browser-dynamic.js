e5cc96fbf8758a487103d5780808379f75198998
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:58:08 +0000

    7219639 fix(compiler-cli): base synthetic filepaths on input filepath (#28453)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+35.sha-f2621db
+ * @license Angular v8.0.0-beta.2+36.sha-7219639
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+35.sha-f2621db');
+var VERSION = new Version('8.0.0-beta.2+36.sha-7219639');
 
 /**
  * @license
