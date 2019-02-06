8d43d8cdac63f61ba5ac7ddbb93b50ad69113f14
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 01:26:09 +0000

    63b744b test: enter notes on ivy-related material test errors (#28527)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+58.sha-fe4d811
+ * @license Angular v8.0.0-beta.2+59.sha-63b744b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+58.sha-fe4d811');
+var VERSION = new Version('8.0.0-beta.2+59.sha-63b744b');
 
 /**
  * @license
