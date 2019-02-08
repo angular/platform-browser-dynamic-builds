d2e4d8be02c100fb061322aa6d3ac92fe0de4d6a
Miško Hevery <misko@hevery.com>
2019-02-08 02:12:28 +0000

    94f042b refactor(ivy): remove unneeded detach property (#28595) PR Close #28595

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+50.sha-4b7264f
+ * @license Angular v8.0.0-beta.3+53.sha-94f042b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -438,7 +438,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+50.sha-4b7264f');
+var VERSION = new Version('8.0.0-beta.3+53.sha-94f042b');
 
 /**
  * @license
