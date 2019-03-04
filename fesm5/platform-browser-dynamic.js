b16297ef723fab1219857af3a42a3e9a26d8c863
Andrew Kushnir <akushnir@google.com>
2019-03-04 20:05:30 +0000

    b598e88 fix(service-worker): ignore passive mixed content requests (#25994)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+18.sha-a0119b1.with-local-changes
+ * @license Angular v7.2.7+24.sha-b598e88.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.7+18.sha-a0119b1.with-local-changes');
+var VERSION = new Version('7.2.7+24.sha-b598e88.with-local-changes');
 
 /**
  * @license
