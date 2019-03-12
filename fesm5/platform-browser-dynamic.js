dde7a9f213b33910bbf8fa89d4ef2222aaef0dd6
Kara Erickson <karakara@google.com>
2019-03-12 17:54:27 +0000

    09e55ea docs: add Sam Julien to GDE group (#29190)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+12.sha-67444e7.with-local-changes
+ * @license Angular v7.2.8+11.sha-09e55ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.8+12.sha-67444e7.with-local-changes');
+var VERSION = new Version('7.2.8+11.sha-09e55ea.with-local-changes');
 
 /**
  * @license
