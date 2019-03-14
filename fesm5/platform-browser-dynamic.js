fffc2e87bb170d7fd6e90a43e0980b69245cd732
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:40:24 +0000

    da78b02 docs: clarify commands to integrate routing (#26530)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+17.sha-d787925.with-local-changes
+ * @license Angular v7.2.9+18.sha-da78b02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.9+17.sha-d787925.with-local-changes');
+var VERSION = new Version('7.2.9+18.sha-da78b02.with-local-changes');
 
 /**
  * @license
