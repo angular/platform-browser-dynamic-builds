d754a2a4251d57c6283a8aa2d8008a9dba26845e
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 19:02:43 +0000

    64e5628 feat(ivy): ngcc - support creating a new copy of the entry-point format (#29092)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+20.sha-0d04450.with-local-changes
+ * @license Angular v8.0.0-beta.9+40.sha-64e5628.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+20.sha-0d04450.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+40.sha-64e5628.with-local-changes');
 
 /**
  * @license
