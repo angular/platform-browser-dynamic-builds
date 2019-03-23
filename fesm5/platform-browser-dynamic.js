a9c568e781bfc3cc7b224bc6398a1b8c3d48cf66
Miško Hevery <misko@hevery.com>
2019-03-23 00:01:25 +0000

    cfe6581 test(ivy): remove passing test from blocklist (#29484)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+83.sha-bef5043.with-local-changes
+ * @license Angular v8.0.0-beta.9+84.sha-cfe6581.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+83.sha-bef5043.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+84.sha-cfe6581.with-local-changes');
 
 /**
  * @license
