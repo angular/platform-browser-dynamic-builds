a0bb0b993113a59867a75b811d61b1bc7f8f9f29
Jason Aden <jasonaden@google.com>
2019-04-01 18:27:10 +0000

    e4eed73 docs(http): add information about body content type to flush method for testing (#29214)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+37.sha-a52123f.with-local-changes
+ * @license Angular v7.2.11+38.sha-e4eed73.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+37.sha-a52123f.with-local-changes');
+var VERSION = new Version('7.2.11+38.sha-e4eed73.with-local-changes');
 
 /**
  * @license
