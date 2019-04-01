b65952e178d2b2c86a2e250b26133034340602c8
Jason Aden <jasonaden@google.com>
2019-04-01 18:38:13 +0000

    157b948 test(docs-infra): add e2e test for the contributors page (#29601)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+38.sha-e4eed73.with-local-changes
+ * @license Angular v7.2.11+40.sha-157b948.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,7 +436,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+38.sha-e4eed73.with-local-changes');
+var VERSION = new Version('7.2.11+40.sha-157b948.with-local-changes');
 
 /**
  * @license
