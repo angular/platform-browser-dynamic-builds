92fb5e98132f88218e9b707193c13633016d7fbc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:05:05 +0000

    5d824c4 fix(ivy): DebugNode.properties not preserving type of values (#29914)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
+ * @license Angular v8.0.0-beta.12+20.sha-5d824c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -622,7 +622,7 @@ class CachedResourceLoader extends ResourceLoader {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.12+18.sha-2deac0a.with-local-changes');
+const VERSION = new Version('8.0.0-beta.12+20.sha-5d824c4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
