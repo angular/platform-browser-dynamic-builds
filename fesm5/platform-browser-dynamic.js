10aee0f7bcf8fecc259f2f0910e452a5acc94b6c
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:39:29 +0000

    6c86ae7 fix(ivy): output should not be subscribe twice when 2 listeners (#30144)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes
+ * @license Angular v8.0.0-rc.0+11.sha-6c86ae7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+11.sha-6c86ae7.with-local-changes');
 
 /**
  * @license
