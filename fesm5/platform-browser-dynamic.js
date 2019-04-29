834b625eba1f3b992d84e4beb5fe59611cae1200
Andrew Kushnir <akushnir@google.com>
2019-04-29 22:40:54 +0000

    68ff2cc fix(ivy): host bindings and listeners not being inherited from undecorated classes (#30158)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+11.sha-6c86ae7.with-local-changes
+ * @license Angular v8.0.0-rc.0+66.sha-68ff2cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+11.sha-6c86ae7.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+66.sha-68ff2cc.with-local-changes');
 
 /**
  * @license
