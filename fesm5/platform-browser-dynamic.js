7ba486ca86251f101ed05e7b9b320cba8327b735
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 01:31:49 +0000

    8ced321 Revert "fix(router): fix a problem with router not responding to back button (#30160)" (#30320)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+102.sha-392473e.with-local-changes
+ * @license Angular v8.0.0-rc.0+104.sha-8ced321.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+102.sha-392473e.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+104.sha-8ced321.with-local-changes');
 
 /**
  * @license
