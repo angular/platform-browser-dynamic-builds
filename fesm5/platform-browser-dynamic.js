b86443c6c63a03ff0ef38353d0758d7b1c239ccf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 20:12:30 +0000

    8cec8f5 test(core): update core_all size-tracking golden file (#30257)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+123.sha-3aff79c.with-local-changes
+ * @license Angular v8.0.0-rc.0+134.sha-8cec8f5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+123.sha-3aff79c.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+134.sha-8cec8f5.with-local-changes');
 
 /**
  * @license
