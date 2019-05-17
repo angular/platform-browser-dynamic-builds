131eb2bd6563494ec814cd714202fde898ef4145
Jason Aden <jasonaden@google.com>
2019-05-17 20:52:49 +0000

    fd7dd4d refactor: clean up language service from unused code and imports (#30534)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+246.sha-01919fb.with-local-changes
+ * @license Angular v8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+246.sha-01919fb.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes');
 
 /**
  * @license
