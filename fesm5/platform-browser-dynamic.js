fa90c705ab589711e3e340e7b1a32fffa1c8b555
Jason Aden <jasonaden@google.com>
2019-05-20 17:34:48 +0000

    6454f76 refactor(ivy): remove ngPrivateData megamorphic prop access (#30548)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+257.sha-c1135ee.with-local-changes
+ * @license Angular v8.0.0-rc.0+259.sha-6454f76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+257.sha-c1135ee.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+259.sha-6454f76.with-local-changes');
 
 /**
  * @license
