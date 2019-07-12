ab8ecfbf7c3acb956a3124f17c3d1f5a6f4953e9
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:17:26 +0000

    6737a8d7a build(docs-infra): display CLI positional option enum values (#31529)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+41.sha-fa69e99.with-local-changes
+ * @license Angular v8.1.0+43.sha-6737a8d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+41.sha-fa69e99.with-local-changes');
+var VERSION = new Version('8.1.0+43.sha-6737a8d.with-local-changes');
 
 /**
  * @license
