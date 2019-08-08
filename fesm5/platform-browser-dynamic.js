454ab65fa5ab91784be1d035a56f2392dff71b55
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:31:45 +0000

    9106271f2 refactor(ivy): remove dependency on global state from isCreationMode (#31959)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+63.sha-a8e2ee1.with-local-changes
+ * @license Angular v9.0.0-next.0+67.sha-9106271.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+63.sha-a8e2ee1.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+67.sha-9106271.with-local-changes');
 
 /**
  * @license
