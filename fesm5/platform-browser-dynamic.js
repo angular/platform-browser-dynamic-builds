25efe7b243d8f42ea5b119123e34ff0e41470739
Kara Erickson <karakara@google.com>
2019-08-09 22:32:56 +0000

    c67f490d2 build: ensure fixup commits match an earlier, unmerged commit (#32023)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+6.sha-eccb60c.with-local-changes
+ * @license Angular v8.2.1+10.sha-c67f490.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.1+6.sha-eccb60c.with-local-changes');
+var VERSION = new Version('8.2.1+10.sha-c67f490.with-local-changes');
 
 /**
  * @license
