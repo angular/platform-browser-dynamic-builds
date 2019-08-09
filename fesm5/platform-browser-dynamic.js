794de56811b14212ab1dd27f40aaba46e50b18fd
Kara Erickson <karakara@google.com>
2019-08-09 18:13:08 +0000

    eb5412d76 fix(ivy): reuse compilation scope for incremental template changes. (#31932)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+10.sha-fd6ed17.with-local-changes
+ * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.1+10.sha-fd6ed17.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
 
 /**
  * @license
