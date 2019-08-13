08c96db4e6fa3c0786268f7a96142bd956a2878c
Kara Erickson <karakara@google.com>
2019-08-13 18:43:39 +0000

    a5f39aeda refactor(language-service): Return ts.Diagnostic[] for getDiagnostics (#32115)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+8.sha-a91ab15.with-local-changes
+ * @license Angular v9.0.0-next.2+9.sha-a5f39ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+8.sha-a91ab15.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+9.sha-a5f39ae.with-local-changes');
 
 /**
  * @license
