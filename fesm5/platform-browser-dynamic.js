1723550b9d5ed9c92f51fc4bc04ea0cd97d63fd7
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:21:08 +0000

    984d23f68 fix(docs-infra): fix broken toc ul styles (#32124)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+23.sha-9e6c677.with-local-changes
+ * @license Angular v9.0.0-next.2+26.sha-984d23f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+23.sha-9e6c677.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+26.sha-984d23f.with-local-changes');
 
 /**
  * @license
