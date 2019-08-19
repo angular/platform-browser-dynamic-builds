d9016fd6d6a250fcf10e6f326718e68e44d9b444
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:34:29 +0000

    994264c0b refactor(ivy): simplify `walkTNodeTree` method for readability (#31065)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+55.sha-e85ec23.with-local-changes
+ * @license Angular v9.0.0-next.2+58.sha-994264c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+55.sha-e85ec23.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+58.sha-994264c.with-local-changes');
 
 /**
  * @license
