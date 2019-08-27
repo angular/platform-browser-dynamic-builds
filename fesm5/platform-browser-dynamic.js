b1232ee1d886b5d6c10364a5eb162413f7d92f5a
atscott <atscott@google.com>
2019-08-27 16:25:18 +0000

    c7c7f9fbe style: align i18n angular config (#32334)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+46.sha-0874bf4.with-local-changes
+ * @license Angular v9.0.0-next.3+49.sha-c7c7f9f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.3+46.sha-0874bf4.with-local-changes');
+var VERSION = new Version('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
 
 /**
  * @license
