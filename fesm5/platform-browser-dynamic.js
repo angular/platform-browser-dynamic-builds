e8d7486ef24213127b8add90ccb0de9a1c0963ec
Miško Hevery <misko@hevery.com>
2019-08-29 19:49:00 +0000

    63dff9c88 ci: temporarily disable chrome mobile tests on android (#32403)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+21.sha-18ce58c.with-local-changes
+ * @license Angular v9.0.0-next.4+30.sha-63dff9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+21.sha-18ce58c.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+30.sha-63dff9c.with-local-changes');
 
 /**
  * @license
