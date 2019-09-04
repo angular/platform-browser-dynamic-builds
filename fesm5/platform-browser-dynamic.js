0aea64c7ea5d49ce228151803220e1a625ba8d3c
Miško Hevery <misko@hevery.com>
2019-09-04 21:04:06 +0000

    8be20f3a3 build: fix incorrect formating which caused a broken build

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+19.sha-5112669.with-local-changes
+ * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.4+19.sha-5112669.with-local-changes');
+var VERSION = new Version('8.2.4+20.sha-8be20f3.with-local-changes');
 
 /**
  * @license
