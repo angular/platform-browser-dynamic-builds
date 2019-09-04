48dbebd785454b9cb1b62c41c2818d0db879269c
Miško Hevery <misko@hevery.com>
2019-09-04 19:17:11 +0000

    7cc4225eb fix(ivy): ensure binding ordering doesn't mess up when a `NO_CHANGE` value is encountered (#32143)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+85.sha-01e0f58.with-local-changes
+ * @license Angular v9.0.0-next.4+88.sha-7cc4225.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+85.sha-01e0f58.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+88.sha-7cc4225.with-local-changes');
 
 /**
  * @license
