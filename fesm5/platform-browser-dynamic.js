559900b7d3d8e7c7e62c00e11274bad9471800c3
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 23:30:04 +0000

    9166baf70 refactor(core): Migrate TestBed.get to TestBed.inject (#32382)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+46.sha-a391aeb.with-local-changes
+ * @license Angular v9.0.0-next.5+48.sha-9166baf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+46.sha-a391aeb.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+48.sha-9166baf.with-local-changes');
 
 /**
  * @license
