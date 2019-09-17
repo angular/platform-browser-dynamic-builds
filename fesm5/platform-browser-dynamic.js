93fb00b735311ef389edbd40a39d794a1d215dae
Andrew Kushnir <akushnir@google.com>
2019-09-17 22:37:08 +0000

    f1b1de9a3 fix(ivy): i18n - start generated placeholder name at `PH` (#32493)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+76.sha-252966b.with-local-changes
+ * @license Angular v9.0.0-next.6+75.sha-f1b1de9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+76.sha-252966b.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+75.sha-f1b1de9.with-local-changes');
 
 /**
  * @license
