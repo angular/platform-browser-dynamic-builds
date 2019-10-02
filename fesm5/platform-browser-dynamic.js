7441d73d60fb55d5f9d338e9eb3f8584df70a008
atscott <atscott01@gmail.com>
2019-10-02 21:51:01 +0000

    8237e958a docs: fix typo in attribute-directives.md (#32943)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+9.sha-9ba898d.with-local-changes
+ * @license Angular v8.2.9+10.sha-8237e95.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.9+9.sha-9ba898d.with-local-changes');
+var VERSION = new Version('8.2.9+10.sha-8237e95.with-local-changes');
 
 /**
  * @license
