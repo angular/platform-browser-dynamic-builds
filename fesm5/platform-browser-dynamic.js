2c97d4d91c138b9a1ac244aa6c2f2a2c7f28cd7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-04 19:08:07 +0000

    3a53e2c96 fix(core): ngNoopZone should have the same signature with ngZone (#32068)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+45.sha-01677b2.with-local-changes
+ * @license Angular v9.0.0-next.9+46.sha-3a53e2c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.9+45.sha-01677b2.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+46.sha-3a53e2c.with-local-changes');
 
 /**
  * @license
