d396514628a541c3aebfda9d1e635e2124e19369
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:27:00 +0000

    c1bb88603 fix(common): expand type for "ngForOf" input to work with strict null checks (#31371)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+61.sha-2089727.with-local-changes
+ * @license Angular v9.0.0-next.9+60.sha-c1bb886.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -442,7 +442,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.9+61.sha-2089727.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+60.sha-c1bb886.with-local-changes');
 
 /**
  * @license
