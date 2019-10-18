a98fcd18869443dcba3945c03f4d418edd44c524
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 15:53:04 +0000

    f13e40db1 docs: change "patchMatch" to "pathMatch" (#32935)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+5.sha-fab6ea0.with-local-changes
+ * @license Angular v8.2.11+6.sha-f13e40d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.11+5.sha-fab6ea0.with-local-changes');
+var VERSION = new Version('8.2.11+6.sha-f13e40d.with-local-changes');
 
 /**
  * @license
