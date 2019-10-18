53af73671a2c833a7e66e0eb6e133f9c4a5eaf44
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 20:49:43 +0000

    d9581bd6a4 ci: fix dev-infra incorrectly matching all ".bzl" files as codeowner (#33249)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+8.sha-19ada81.with-local-changes
+ * @license Angular v8.2.11+9.sha-d9581bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.11+8.sha-19ada81.with-local-changes');
+var VERSION = new Version('8.2.11+9.sha-d9581bd.with-local-changes');
 
 /**
  * @license
