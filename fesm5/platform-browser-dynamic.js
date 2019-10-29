181265dfc0ccb95f7ce7d2896d712203f0366941
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:09:52 +0000

    f97c464a9e fix(compiler): i18n - ignore `alt-trans` tags in XLIFF 1.2 (#33464)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.12+3.sha-26a6db2.with-local-changes
+ * @license Angular v8.2.12+5.sha-f97c464.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.12+3.sha-26a6db2.with-local-changes');
+var VERSION = new Version('8.2.12+5.sha-f97c464.with-local-changes');
 
 /**
  * @license
