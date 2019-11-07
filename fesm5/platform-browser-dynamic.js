11be1af26eec8e3a314fe56ad52b5282280f27be
Andrew Scott <atscott01@gmail.com>
2019-11-07 22:36:22 +0000

    c77faf566e fix(common): update CLDR generated files to 36.0.0 (#33584)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+18.sha-71b400e.with-local-changes
+ * @license Angular v9.0.0-rc.1+20.sha-c77faf5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
 /**
  * @license
