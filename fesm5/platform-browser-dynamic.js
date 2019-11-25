d4519145b08649d35a69cc42e246b2cd4151fbba
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:05:09 +0000

    04b12fc0d6 refactor(ivy): remove isRootView check from getRenderParent (#33988)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+251.sha-478f882.with-local-changes
+ * @license Angular v9.0.0-rc.1+250.sha-04b12fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+251.sha-478f882.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+250.sha-04b12fc.with-local-changes');
 
 /**
  * @license
