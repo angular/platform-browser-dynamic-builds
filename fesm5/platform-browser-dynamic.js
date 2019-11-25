ee88f503c321161e4ceefd372f13b05076d5dd84
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:05:41 +0000

    31e2bdffa8 perf(ivy): fix creation time micro-benchmarks (#34031)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+253.sha-d80eeb6.with-local-changes
+ * @license Angular v9.0.0-rc.1+254.sha-31e2bdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+253.sha-d80eeb6.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+254.sha-31e2bdf.with-local-changes');
 
 /**
  * @license
