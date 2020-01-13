b38b9db97a928d2f646764de76a4a5b4a74d9572
atscott <atscott@google.com>
2020-01-13 18:08:08 +0000

    ff02ddfd9b fix(ngcc): correctly detect dependencies in CommonJS (#34528)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+85.sha-a0eb57f
+ * @license Angular v9.0.0-rc.8+88.sha-ff02ddf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.8+85.sha-a0eb57f');
+var VERSION = new Version('9.0.0-rc.8+88.sha-ff02ddf');
 
 /**
  * @license
