0874de19f7a6780a9de069ca99a889c95e1b5a80
atscott <atscott@google.com>
2020-01-14 22:01:06 +0000

    e100bbc696 docs: replace non-breaking with regular spaces in `resources.json` (#34772)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+112.sha-b1d213b
+ * @license Angular v9.0.0-rc.8+115.sha-e100bbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -669,7 +669,7 @@ if (false) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+112.sha-b1d213b');
+const VERSION = new Version('9.0.0-rc.8+115.sha-e100bbc');
 
 /**
  * @fileoverview added by tsickle
