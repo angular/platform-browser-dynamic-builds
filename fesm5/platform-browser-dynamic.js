8ef7a54e0ce92ec890c46e660a31ae0a06ee262e
GitHub <noreply@github.com>
2020-01-15 12:14:09 +0000

    50e52c8e1b fixup! docs: fix public api tool name

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-ae9c061
+ * @license Angular v9.0.0-rc.1+691.sha-50e52c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+689.sha-ae9c061');
+var VERSION = new Version('9.0.0-rc.1+691.sha-50e52c8');
 
 /**
  * @license
