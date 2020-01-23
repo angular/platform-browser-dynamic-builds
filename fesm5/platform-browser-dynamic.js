073835724b817d757ff32b6e3df80f00f1b9c484
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:43:35 +0000

    cc7fca4dc1 feat(language-service): specific suggestions for template context diags (#34751)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+37.sha-6a4186c
+ * @license Angular v9.0.0-rc.10+38.sha-cc7fca4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.10+37.sha-6a4186c');
+var VERSION = new Version('9.0.0-rc.10+38.sha-cc7fca4');
 
 /**
  * @license
