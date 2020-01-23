910b074ea4b460dd2c3f14c0b3b4af676b2a7148
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:02:31 +0000

    6a4186c017 build: vendor yarn using the .yarnrc yarn-path value (#34902)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+34.sha-e69d02c
+ * @license Angular v9.0.0-rc.10+37.sha-6a4186c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.10+34.sha-e69d02c');
+var VERSION = new Version('9.0.0-rc.10+37.sha-6a4186c');
 
 /**
  * @license
