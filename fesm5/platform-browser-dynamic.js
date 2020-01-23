df30af1b1eb419f2e7e5771b20d99fc570b7b260
Andrew Kushnir <akushnir@google.com>
2020-01-23 23:11:49 +0000

    080a8bfd43 fix(ngcc): do not attempt compilation when analysis fails (#34889)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+45.sha-c80392b
+ * @license Angular v9.0.0-rc.10+47.sha-080a8bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.10+45.sha-c80392b');
+var VERSION = new Version('9.0.0-rc.10+47.sha-080a8bf');
 
 /**
  * @license
