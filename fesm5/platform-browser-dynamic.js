0acd3bfa4b22afa29a9b872688b87218ace27927
Andrew Kushnir <akushnir@google.com>
2020-01-23 00:12:52 +0000

    32b72f39f0 fix(ivy): ensure eventListeners added outside angular context are not called... (#34514)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+760.sha-d15cf60
+ * @license Angular v9.0.0-rc.1+761.sha-32b72f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+760.sha-d15cf60');
+var VERSION = new Version('9.0.0-rc.1+761.sha-32b72f3');
 
 /**
  * @license
