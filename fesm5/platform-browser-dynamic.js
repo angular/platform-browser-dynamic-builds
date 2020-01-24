98d8ec4ec1331b218940a0930e64e65b3ef431bc
Andrew Kushnir <akushnir@google.com>
2020-01-24 19:52:57 +0000

    ed24100976 ci(docs-infra): use the correct version of yarn in `aio_monitoring_stable` (#34954)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+791.sha-24b2f1d
+ * @license Angular v9.0.0-rc.1+792.sha-ed24100
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+791.sha-24b2f1d');
+var VERSION = new Version('9.0.0-rc.1+792.sha-ed24100');
 
 /**
  * @license
