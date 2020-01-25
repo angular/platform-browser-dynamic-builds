5dfe56301751fe05d50f620f4823a82781fa83fe
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:15:50 +0000

    3414ef276e refactor(language-service): Avoid leaking host outside of LanguageService (#34941)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+807.sha-8926f76
+ * @license Angular v9.0.0-rc.1+808.sha-3414ef2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+807.sha-8926f76');
+var VERSION = new Version('9.0.0-rc.1+808.sha-3414ef2');
 
 /**
  * @license
