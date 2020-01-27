d3be4b457407bff28f435f4404a73d01e7b3cd5c
Andrew Kushnir <akushnir@google.com>
2020-01-27 18:03:35 +0000

    efb17ae0c8 build: update API Golden files after node 12 update (#34955)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+10.sha-4a6093d
+ * @license Angular v9.0.0-rc.11+15.sha-efb17ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+10.sha-4a6093d');
+var VERSION = new Version('9.0.0-rc.11+15.sha-efb17ae');
 
 /**
  * @license
