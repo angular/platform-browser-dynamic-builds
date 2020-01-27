e1447a9142bc4889d491071f502c49bb1da774e9
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:47:51 +0000

    724ce8c22b build(docs-infra): update to @angular/material 9.0.0-rc.8 (#34966)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+814.sha-3d4b143
+ * @license Angular v9.0.0-rc.1+817.sha-724ce8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+814.sha-3d4b143');
+var VERSION = new Version('9.0.0-rc.1+817.sha-724ce8c');
 
 /**
  * @license
