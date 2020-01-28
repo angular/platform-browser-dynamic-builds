068d908af6dd3285ea96096ef708ddfb56d7ae20
Andrew Kushnir <akushnir@google.com>
2020-01-28 21:24:50 +0000

    a5a598104f docs: change p to pointer for better understanding (#34927)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+836.sha-1cee9b3
+ * @license Angular v9.0.0-rc.1+837.sha-a5a5981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+836.sha-1cee9b3');
+var VERSION = new Version('9.0.0-rc.1+837.sha-a5a5981');
 
 /**
  * @license
