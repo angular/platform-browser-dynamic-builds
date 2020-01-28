3ad3b44534e993696f67217bc0776dbfc3b1be0b
Andrew Kushnir <akushnir@google.com>
2020-01-28 22:07:08 +0000

    dff4e1e19c refactor: simplify bazel saucelabs targets using karma pre-test wrapper and shared saucelabs connection between tests (#34769)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+837.sha-a5a5981
+ * @license Angular v9.0.0-rc.1+838.sha-dff4e1e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+837.sha-a5a5981');
+var VERSION = new Version('9.0.0-rc.1+838.sha-dff4e1e');
 
 /**
  * @license
