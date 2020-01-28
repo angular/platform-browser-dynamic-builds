28686176ff2fa49283eabefcb6324f6c1e9febdc
George Kalpakas <kalpakas.g@gmail.com>
2020-01-28 22:15:52 +0000

    015f833f8e ci(docs-infra): ignore node version in `aio_monitoring_stable` CI job

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.14+2.sha-d2aad91.with-local-changes
+ * @license Angular v8.2.14+3.sha-015f833.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -439,7 +439,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.14+2.sha-d2aad91.with-local-changes');
+var VERSION = new Version('8.2.14+3.sha-015f833.with-local-changes');
 
 /**
  * @license
