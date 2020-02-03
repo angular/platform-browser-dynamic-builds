698ae4b366d2877e81127fe144f764d5db332f53
Miško Hevery <misko@hevery.com>
2020-02-03 18:04:28 +0000

    6a771d9659 refactor(language-service): dedupe diagnostics using ts utility function (#35086)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.13
+ * @license Angular v9.0.0-rc.13+6.sha-6a771d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.13');
+var VERSION = new Version('9.0.0-rc.13+6.sha-6a771d9');
 
 /**
  * @license
