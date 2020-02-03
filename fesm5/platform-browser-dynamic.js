12b4829893ec9e8821fb8aa15c6e18e4ed8d9a7e
Miško Hevery <misko@hevery.com>
2020-02-03 22:26:11 +0000

    3c30474417 docs: update zone-bluebird patch document for angular (#34536)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+904.sha-9601b5d
+ * @license Angular v9.0.0-rc.1+905.sha-3c30474
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+904.sha-9601b5d');
+var VERSION = new Version('9.0.0-rc.1+905.sha-3c30474');
 
 /**
  * @license
