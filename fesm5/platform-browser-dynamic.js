aaf0d556ddcdd9c8f8984bb20345a825ebd76bc5
Miško Hevery <misko@hevery.com>
2020-02-03 17:07:32 +0000

    65dbd50594 test(ivy): correct var count in perf benchmarks. (#35071)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+898.sha-3d4067a
+ * @license Angular v9.0.0-rc.1+899.sha-65dbd50
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+898.sha-3d4067a');
+var VERSION = new Version('9.0.0-rc.1+899.sha-65dbd50');
 
 /**
  * @license
