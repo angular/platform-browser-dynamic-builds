9530deb608e35433191714c24afb53ec6f094d2a
Misko Hevery <misko@hevery.com>
2020-02-06 17:56:43 +0000

    722251da40 build: update scripts/release/post-check with the latest versions (#35151)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+934.sha-c286fbb
+ * @license Angular v9.0.0-rc.1+935.sha-722251d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
+var VERSION = new Version('9.0.0-rc.1+935.sha-722251d');
 
 /**
  * @license
