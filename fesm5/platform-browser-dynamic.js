7dad49d98ff326a509db44d6c977f47786dfa08a
Misko Hevery <misko@hevery.com>
2020-02-06 01:45:06 +0000

    24555dbb2d build: update @angular/cli to rc 14 (#35162)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+21.sha-7de8772
+ * @license Angular v9.0.0-rc.14+22.sha-24555db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.14+21.sha-7de8772');
+var VERSION = new Version('9.0.0-rc.14+22.sha-24555db');
 
 /**
  * @license
