8ff84b026ba9464de212957b4a6a1b8330b6a94e
Kara Erickson <karakara@google.com>
2020-02-06 22:51:57 +0000

    532ccb647c docs: remove the release schedule from docs (#34474)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+940.sha-ba0cdd3
+ * @license Angular v9.0.0-rc.1+941.sha-532ccb6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+940.sha-ba0cdd3');
+var VERSION = new Version('9.0.0-rc.1+941.sha-532ccb6');
 
 /**
  * @license
