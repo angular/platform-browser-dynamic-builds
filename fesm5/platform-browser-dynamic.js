57c4aadfddb540c10d5fdb9b5eed7ae95fe6c58c
Misko Hevery <misko@hevery.com>
2020-02-06 23:59:35 +0000

    6fb538ed1d fix(bazel): update ibazel to 0.11.1 (#35158)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+944.sha-3c69442
+ * @license Angular v9.0.0-rc.1+948.sha-6fb538e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+944.sha-3c69442');
+var VERSION = new Version('9.0.0-rc.1+948.sha-6fb538e');
 
 /**
  * @license
