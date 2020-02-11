b5195b2be7c3ca6117fc99d7158f6236eae4ce9f
Kara Erickson <karakara@google.com>
2020-02-11 00:54:49 +0000

    a92d97cda7 fix(compiler): report errors for missing binding names (#34595)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+981.sha-e1160f1
+ * @license Angular v9.0.0-rc.1+982.sha-a92d97c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+981.sha-e1160f1');
+var VERSION = new Version('9.0.0-rc.1+982.sha-a92d97c');
 
 /**
  * @license
