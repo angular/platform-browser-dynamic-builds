d6d8c1c65e661a4bb33e243f00caeb3ad19a1c28
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-13 21:54:44 +0000

    db693f482f docs: Fix minor typos and coding styles (#35325)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+16.sha-84c9c6e
+ * @license Angular v9.0.1+17.sha-db693f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.1+16.sha-84c9c6e');
+var VERSION = new Version('9.0.1+17.sha-db693f4');
 
 /**
  * @license
