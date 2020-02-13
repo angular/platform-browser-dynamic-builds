06b77904e630e573cc583d4058edf2b397912b28
Kara Erickson <karakara@google.com>
2020-02-13 18:38:39 +0000

    b2222e7e02 docs: minor typo fix (#35423) PR Close #35423

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+8.sha-dc63ceb
+ * @license Angular v9.1.0-next.0+16.sha-b2222e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+8.sha-dc63ceb');
+var VERSION = new Version('9.1.0-next.0+16.sha-b2222e7');
 
 /**
  * @license
