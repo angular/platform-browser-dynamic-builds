f4b2655fcce8d8a8697d2a3d8341a9d1d3ff1049
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:42:19 +0000

    eee8c7f718 docs: change `item` to `items` to match the code (#35433)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+20.sha-4f8d303
+ * @license Angular v9.0.1+24.sha-eee8c7f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.1+20.sha-4f8d303');
+var VERSION = new Version('9.0.1+24.sha-eee8c7f');
 
 /**
  * @license
