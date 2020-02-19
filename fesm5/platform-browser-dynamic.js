25b4a5d9a9ecbd057e3803c4e774d66aaa386fe1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 01:47:10 +0000

    5fbfe6996a fix(ivy): wrong context passed to ngOnDestroy when resolved multiple times (#35249)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+37.sha-183a862
+ * @license Angular v9.1.0-next.0+38.sha-5fbfe69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+37.sha-183a862');
+var VERSION = new Version('9.1.0-next.0+38.sha-5fbfe69');
 
 /**
  * @license
