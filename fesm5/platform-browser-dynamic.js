576a190a59a8c459c193bad52c609248273f7526
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 20:34:03 +0000

    5edeee69dd release: cut the v9.0.2 release

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+44.sha-ce85cbf
+ * @license Angular v9.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.1+44.sha-ce85cbf');
+var VERSION = new Version('9.0.2');
 
 /**
  * @license
