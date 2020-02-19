3fcffc04e0e072b1cd4c9ba40b984965f26fc8e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 17:32:14 +0000

    d0d36a5ebf ci: increase AIO payload size limit (#35538)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+39.sha-3f4e02b
+ * @license Angular v9.1.0-next.0+43.sha-d0d36a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+39.sha-3f4e02b');
+var VERSION = new Version('9.1.0-next.0+43.sha-d0d36a5');
 
 /**
  * @license
