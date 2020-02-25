6102c342e1051151f01812cb7d1296695239453a
Miško Hevery <misko@hevery.com>
2020-02-25 21:36:46 +0000

    0a1a989fa9 perf(core): avoid recursive scope recalculation when TestBed.overrideModule is used (#35454)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+60.sha-294e56d
+ * @license Angular v9.1.0-next.1+58.sha-0a1a989
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+60.sha-294e56d');
+var VERSION = new Version('9.1.0-next.1+58.sha-0a1a989');
 
 /**
  * @license
