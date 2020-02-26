3c6c2ba90f878deec9a59ce88c07af1ad36bd9bb
Miško Hevery <misko@hevery.com>
2020-02-26 21:30:38 +0000

    df816c9c80 feat(ngcc): implement source-map flattening (#35132)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+66.sha-a7ed74f');
+var VERSION = new Version('9.1.0-next.1+69.sha-df816c9');
 
 /**
  * @license
