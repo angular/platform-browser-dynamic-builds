93d569523283b4314dde17d6c5fb2c47790a1eec
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 00:17:47 +0000

    2cccf59415 build: create dev-infra-private npm package (#35862)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+5.sha-2a9b254
+ * @license Angular v9.0.5+6.sha-2cccf59
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.5+5.sha-2a9b254');
+var VERSION = new Version('9.0.5+6.sha-2cccf59');
 
 /**
  * @license
