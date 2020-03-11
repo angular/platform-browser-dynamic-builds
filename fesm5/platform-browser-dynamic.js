eb53cb2965f72d7b8346b22c445727e670922d40
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:27:21 +0000

    e262962b12 fix(docs-infra): add boilerplate path and project type for elements example project (#33484)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+6.sha-3112b1b
+ * @license Angular v9.0.6+17.sha-e262962
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.6+6.sha-3112b1b');
+var VERSION = new Version('9.0.6+17.sha-e262962');
 
 /**
  * @license
