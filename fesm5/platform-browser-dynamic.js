5b20a4573db9cde8ef8e5239b5fc4664031f2c00
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:31:34 +0000

    99ea5d7044 fix(zone.js): fix `zone-patch-rxjs` bundle to refer to `rxjs` (rather than include) it. (#35983)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+33.sha-8ea61a1
+ * @license Angular v9.1.0-next.4+34.sha-99ea5d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+33.sha-8ea61a1');
+var VERSION = new Version('9.1.0-next.4+34.sha-99ea5d7');
 
 /**
  * @license
