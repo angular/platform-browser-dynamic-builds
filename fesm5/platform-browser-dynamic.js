2f0702f508072de1387f067d224081387d37962d
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:13:36 +0000

    43635a6c54 build: update ibazel to `0.12.2` (#36033)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+37.sha-37a4839
+ * @license Angular v9.1.0-next.4+38.sha-43635a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+37.sha-37a4839');
+var VERSION = new Version('9.1.0-next.4+38.sha-43635a6');
 
 /**
  * @license
