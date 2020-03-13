07ed8ef82d677b686561328c05a9497340039843
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:46:30 +0000

    9e698b7cca fix(docs-infra): remove `routerLink` from `top-bar` (#35951)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+36.sha-a8cc6a2
+ * @license Angular v9.0.6+37.sha-9e698b7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.6+36.sha-a8cc6a2');
+var VERSION = new Version('9.0.6+37.sha-9e698b7');
 
 /**
  * @license
