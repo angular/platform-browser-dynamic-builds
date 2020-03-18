b4c001ac3697072a13b9328293f3eb5519eafbb1
Andrew Kushnir <akushnir@google.com>
2020-03-18 21:05:24 +0000

    a90e82db3e release: cut the v9.0.7 release

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+80.sha-647eb87
+ * @license Angular v9.0.7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.6+80.sha-647eb87');
+var VERSION = new Version('9.0.7');
 
 /**
  * @license
