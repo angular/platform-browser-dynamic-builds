6f8225f3a0006de095ed94a4fdb1934f3350880c
Andrew Kushnir <akushnir@google.com>
2020-03-19 16:26:58 +0000

    e342ffd855 fix(core): adhere to bootstrap options for JIT compiled components (#35534)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+9.sha-fb92f5d
+ * @license Angular v9.1.0-next.5+8.sha-e342ffd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.5+9.sha-fb92f5d');
+var VERSION = new Version('9.1.0-next.5+8.sha-e342ffd');
 
 /**
  * @license
