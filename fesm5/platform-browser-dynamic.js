4ad6071b5084ecfc3c6770af230aa0f66e6578a8
Misko Hevery <misko@hevery.com>
2020-03-23 19:00:59 +0000

    fced8ee40e fix(localize): allow ICU expansion case to start with any character except `}` (#36123)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
+ * @license Angular v9.1.0-rc.0+19.sha-fced8ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -434,7 +434,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+16.sha-e81ad3a');
+var VERSION = new Version('9.1.0-rc.0+19.sha-fced8ee');
 
 /**
  * @license
