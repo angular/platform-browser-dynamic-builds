7d438e22d54ca7ee4e66151ae390994c1a0c24de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:49:35 +0000

    0baab0f3db docs: update release nots with blog post (#36268)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+2.sha-294c629
+ * @license Angular v9.1.0+14.sha-0baab0f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+2.sha-294c629');
+var VERSION = new Version('9.1.0+14.sha-0baab0f');
 
 /**
  * @license
