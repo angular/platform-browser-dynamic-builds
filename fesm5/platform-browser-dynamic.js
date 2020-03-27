0c1269341d50f801de967e2fbb5b12df3dab41d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:22:47 +0000

    bfa7b1a494 docs: correct a misleading sentence (#36155) (#36158)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+38.sha-fc3e5cb');
+var VERSION = new Version('9.1.0-rc.0+46.sha-bfa7b1a');
 
 /**
  * @license
