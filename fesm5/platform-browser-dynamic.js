37cdb0dc2a3cb9b19bf98d94f9161a38387c2965
atscott <atscott@google.com>
2020-04-09 20:57:42 +0000

    58d028178f fix(docs-infra): fix `elements` example when used with ES5 (#36536)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+25.sha-e06512b
+ * @license Angular v9.1.1+26.sha-58d0281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -441,7 +441,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+25.sha-e06512b');
+var VERSION = new Version('9.1.1+26.sha-58d0281');
 
 /**
  * @license
