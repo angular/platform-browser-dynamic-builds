88980798eeac56ad502953350b87523fa4830a62
atscott <atscott@google.com>
2020-04-14 20:05:41 +0000

    93993864b1 build: update to latest version of yarn (#36464)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+40.sha-26f4915
+ * @license Angular v9.1.1+41.sha-9399386
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+40.sha-26f4915');
+var VERSION = new Version('9.1.1+41.sha-9399386');
 
 /**
  * @license
