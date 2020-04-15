282452959f69139c25406cd1b3f8a1fc38cc401f
atscott <atscott@google.com>
2020-04-15 16:48:21 +0000

    e041ac6f0d fix(ngcc): display output from the unlocker process on Windows (#36569)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+36.sha-663b768
+ * @license Angular v10.0.0-next.1+38.sha-e041ac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+36.sha-663b768');
+var VERSION = new Version('10.0.0-next.1+38.sha-e041ac6');
 
 /**
  * @license
