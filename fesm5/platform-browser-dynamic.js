f11b68dbd9665b08a8eb25634cee92db83f57637
atscott <atscott@google.com>
2020-04-16 17:04:01 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+2.sha-03ff380
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+2.sha-03ff380');
+var VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
 /**
  * @license
