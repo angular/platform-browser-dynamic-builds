4142de5690193f450e295578c71a7fef5d78be9f
atscott <atscott@google.com>
2020-04-16 00:10:00 +0000

    793a001d7c release: cut the v9.1.2 release

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+46.sha-5c3774c');
+var VERSION = new Version('9.1.2');
 
 /**
  * @license
