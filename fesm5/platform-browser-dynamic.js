f258fd5c8256e9ddcc2e7d6ea4f7984abc3e0e49
Andrew Kushnir <akushnir@google.com>
2020-04-28 22:32:09 +0000

    a6a7e1bb99 build: enable platform-browser tests on Saucelabs (#36797)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+43.sha-70dd27f
+ * @license Angular v10.0.0-next.3+44.sha-a6a7e1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+43.sha-70dd27f');
+var VERSION = new Version('10.0.0-next.3+44.sha-a6a7e1b');
 
 /**
  * @license
