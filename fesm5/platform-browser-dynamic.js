49438fe677ad4553a528b33e248a3933490f6eb9
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:20:26 +0000

    c89421bfa8 docs: release notes for the v9.1.4 release

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+47.sha-cebd4fa
+ * @license Angular v10.0.0-next.3+48.sha-c89421b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+47.sha-cebd4fa');
+var VERSION = new Version('10.0.0-next.3+48.sha-c89421b');
 
 /**
  * @license
