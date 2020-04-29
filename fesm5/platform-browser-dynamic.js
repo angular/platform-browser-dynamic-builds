20b3e8f25be941e2209aa4b4643707d97a28cc01
Andrew Kushnir <akushnir@google.com>
2020-04-29 20:27:57 +0000

    cebd4fada7 build: use sh instead of exec for release scripts (#36862)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+46.sha-3d82aa7
+ * @license Angular v10.0.0-next.3+47.sha-cebd4fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+46.sha-3d82aa7');
+var VERSION = new Version('10.0.0-next.3+47.sha-cebd4fa');
 
 /**
  * @license
