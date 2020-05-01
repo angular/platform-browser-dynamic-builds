97957851db4d4110d5a26a5a2f67f65e755f3342
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:24:46 +0000

    95a407d9dd build: Add entry point for Ivy language service (#36864)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+31.sha-12fcc7c
+ * @license Angular v10.0.0-next.4+30.sha-95a407d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+31.sha-12fcc7c');
+var VERSION = new Version('10.0.0-next.4+30.sha-95a407d');
 
 /**
  * @license
