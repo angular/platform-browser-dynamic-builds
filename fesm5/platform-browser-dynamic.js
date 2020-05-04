40dd612fe45eefc9636dc5839fe3c52a2794b77f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:05:34 +0000

    0f3831b105 build: create macro for transpiling javascript file to es5 (#36802)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+33.sha-9d9d46f
+ * @license Angular v10.0.0-next.4+34.sha-0f3831b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+33.sha-9d9d46f');
+var VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
 
 /**
  * @license
