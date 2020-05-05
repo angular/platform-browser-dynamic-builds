f42b8a382de8ab72106503db20bdc89021b86a69
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:42:29 +0000

    35d61c11fd test(core): Add benchmark for transplanted views when insertion is dirty (#36722)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+21.sha-e30e132
+ * @license Angular v10.0.0-next.5+22.sha-35d61c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+21.sha-e30e132');
+var VERSION = new Version('10.0.0-next.5+22.sha-35d61c1');
 
 /**
  * @license
