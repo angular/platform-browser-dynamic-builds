f1afa2dcc32de1368a18a4db32835fddcfe17b16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 02:00:58 +0000

    ecffc3557f perf(compiler-cli): perform template type-checking incrementally (#36211)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+25.sha-388dc93
+ * @license Angular v10.0.0-next.5+42.sha-ecffc35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+25.sha-388dc93');
+var VERSION = new Version('10.0.0-next.5+42.sha-ecffc35');
 
 /**
  * @license
