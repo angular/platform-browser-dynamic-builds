5455a395c82f8719f02283f8bd4cc2c686f37c99
Kara Erickson <karakara@google.com>
2020-05-18 22:17:19 +0000

    178a78f9ed docs(router): add docs for RouterLink inputs (#37018)

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+67.sha-ba9e63a
+ * @license Angular v9.1.6+70.sha-178a78f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.6+67.sha-ba9e63a');
+var VERSION = new Version('9.1.6+70.sha-178a78f');
 
 /**
  * @license
