2e1a8137b5ea3a8a9040cea28c149f04d8353d6b
atscott <atscott@google.com>
2020-06-08 17:47:05 +0000

    650974e1b3 test(language-service): Remove all markers from test project (#37475)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+91.sha-ed3c549
+ * @license Angular v10.0.0-rc.0+92.sha-650974e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -454,7 +454,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+91.sha-ed3c549');
+const VERSION = new Version('10.0.0-rc.0+92.sha-650974e');
 
 /**
  * @license
