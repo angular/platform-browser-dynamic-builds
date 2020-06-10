b132400a85ba3be5877e36c790826930557d14b4
atscott <atscott01@gmail.com>
2020-06-10 17:51:28 +0000

    742f3d6787 Revert "fix(elements): fire custom element output events during component initialization (#36161)" (#37524)

--- bundles/platform-browser-dynamic-testing.umd.js
+++ bundles/platform-browser-dynamic-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+14.sha-9d397eb
+ * @license Angular v10.0.0-rc.3+16.sha-742f3d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -269,7 +269,6 @@
         DOMTestComponentRenderer.decorators = [
             { type: core.Injectable }
         ];
-        /** @nocollapse */
         DOMTestComponentRenderer.ctorParameters = function () { return [
             { type: undefined, decorators: [{ type: core.Inject, args: [common.DOCUMENT,] }] }
         ]; };
