f541931fdc7720a2e7066d65e13e7c042ae8e4f8
Misko Hevery <misko@hevery.com>
2020-06-11 23:25:58 +0000

    3a43cdefe8 release: cut the v10.0.0-rc.5 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+14.sha-38c48be
+ * @license Angular v10.0.0-rc.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.4+14.sha-38c48be');
+const VERSION = new Version('10.0.0-rc.5');
 
 /**
  * @license
