4250239126fa5c8feaa38aaea350cdddda08e5c0
Andrew Kushnir <akushnir@google.com>
2020-06-25 18:58:35 +0000

    bedc0451a0 docs(ngcc): add additional next steps to an error (#37672)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+228.sha-4481beb
+ * @license Angular v10.0.0-rc.0+231.sha-bedc045
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+228.sha-4481beb');
+const VERSION = new Version('10.0.0-rc.0+231.sha-bedc045');
 
 /**
  * @license
