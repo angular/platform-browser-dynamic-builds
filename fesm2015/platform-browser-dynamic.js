01b49302c25f7ee3827dfdfe979c2aa1a0b5eff3
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:42:46 +0000

    ce879fc416 refactor(compiler-cli): more accurate reporting of complex function call (#37587)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+256.sha-5103d90
+ * @license Angular v10.0.0-rc.0+259.sha-ce879fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+256.sha-5103d90');
+const VERSION = new Version('10.0.0-rc.0+259.sha-ce879fc');
 
 /**
  * @license
