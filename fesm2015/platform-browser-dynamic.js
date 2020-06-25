af81f658f920b7b417eed80011992cc2f04f47aa
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:19:39 +0000

    80d0067048 fix(router): `RouterLinkActive` should run CD when setting `isActive` (#21411)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+238.sha-5218916
+ * @license Angular v10.0.0-rc.0+239.sha-80d0067
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+238.sha-5218916');
+const VERSION = new Version('10.0.0-rc.0+239.sha-80d0067');
 
 /**
  * @license
