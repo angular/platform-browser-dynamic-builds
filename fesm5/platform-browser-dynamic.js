e3fd442ec73cd3ec05adfae68b52cdecd6a78234
Igor Minar <igor@angular.io>
2020-06-25 21:08:00 +0000

    0feb560136 fixup! feat(docs-infra): update deploy-to-firebase.sh script to support v9 multisite setup

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+1.sha-6c1ab47
+ * @license Angular v9.1.11+3.sha-0feb560
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.11+1.sha-6c1ab47');
+var VERSION = new Version('9.1.11+3.sha-0feb560');
 
 /**
  * @license
