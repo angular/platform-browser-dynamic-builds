1f048e365802de918db071c16658b70dd1586fe8
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:06:27 +0000

    666e6d15fa docs: Replace $emdash; with an actual em dash (#37723)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+234.sha-f3ec37e
+ * @license Angular v10.0.0-rc.0+235.sha-666e6d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+234.sha-f3ec37e');
+const VERSION = new Version('10.0.0-rc.0+235.sha-666e6d1');
 
 /**
  * @license
