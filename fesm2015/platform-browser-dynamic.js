510502c89736ac6d26dfae5a20358be0ace9a9be
Misko Hevery <misko@hevery.com>
2020-06-25 03:15:29 +0000

    f5bbfd7b8c docs(changelog): fix v10 announcement url (#37722)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+226.sha-41ce524
+ * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+226.sha-41ce524');
+const VERSION = new Version('10.0.0-rc.0+227.sha-f5bbfd7');
 
 /**
  * @license
