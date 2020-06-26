a2c135391016b6014c5a765d05c4ae5c6fbaebaf
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:22:29 +0000

    dbc2364d16 fix(dev-infra): support running scripts from within a detached head (#37737)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+271.sha-dbc2364
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+const VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
 
 /**
  * @license
