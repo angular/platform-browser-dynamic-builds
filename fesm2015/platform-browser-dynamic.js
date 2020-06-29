28cbc808e2ea4c12d09855a58a247666add1d37d
Andrew Kushnir <akushnir@google.com>
2020-06-29 23:59:19 +0000

    57a518a36d perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+20.sha-29b8318
+ * @license Angular v10.0.1+21.sha-57a518a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1+20.sha-29b8318');
+const VERSION = new Version('10.0.1+21.sha-57a518a');
 
 /**
  * @license
