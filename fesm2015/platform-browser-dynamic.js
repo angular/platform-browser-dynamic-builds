0ff1b36c9c5221aa116bf3c56fa82caec1d221f0
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:04:36 +0000

    eba9dd364e fix(elements): fire custom element output events during component initialization (#37570)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+291.sha-78a4476
+ * @license Angular v10.0.0-rc.0+292.sha-eba9dd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+291.sha-78a4476');
+const VERSION = new Version('10.0.0-rc.0+292.sha-eba9dd3');
 
 /**
  * @license
