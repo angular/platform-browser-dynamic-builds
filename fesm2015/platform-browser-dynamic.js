15d0519f390e49a1e5a5591fb806e5d4613ef416
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:27:08 +0000

    e84539f809 build(docs-infra): update to latest dgeni-packages (#37793)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+302.sha-873330b
+ * @license Angular v10.0.0-rc.0+303.sha-e84539f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+302.sha-873330b');
+const VERSION = new Version('10.0.0-rc.0+303.sha-e84539f');
 
 /**
  * @license
