3f515999fca43dc71ca6060f1a0b13b3c113a45f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:18:43 +0000

    1801d0c650 fix(router): properly compare array queryParams for equality (#37709) (#37860)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+314.sha-d7dd295
+ * @license Angular v10.0.0-rc.0+316.sha-1801d0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+314.sha-d7dd295');
+const VERSION = new Version('10.0.0-rc.0+316.sha-1801d0c');
 
 /**
  * @license
