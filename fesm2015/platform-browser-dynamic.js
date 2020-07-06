76c3a1b12557b0474cc8034bef797419ca6a211c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:21:15 +0000

    ec32eba02c feat(localize): expose `canParse()` diagnostics (#37909)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+318.sha-aed6b13
+ * @license Angular v10.0.0-rc.0+320.sha-ec32eba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+318.sha-aed6b13');
+const VERSION = new Version('10.0.0-rc.0+320.sha-ec32eba');
 
 /**
  * @license
