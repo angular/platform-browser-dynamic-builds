af93c8cd8a8d68c4b7e61c54d9889b2b3e2ea85f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 16:59:10 +0000

    a5ffca0576 fix(router): fix navigation ignoring logic to compare to the browser url (#37716)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+312.sha-196bfa8
+ * @license Angular v10.0.0-rc.0+313.sha-a5ffca0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+312.sha-196bfa8');
+const VERSION = new Version('10.0.0-rc.0+313.sha-a5ffca0');
 
 /**
  * @license
