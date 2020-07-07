14db18974969ffab7160bba63b85948ba3b373e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:40:34 +0000

    cb2ab26296 feat(dev-infra): merge script should link to original commit when cherry-picking with API strategy (#37889)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+320.sha-ec32eba
+ * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+320.sha-ec32eba');
+const VERSION = new Version('10.0.0-rc.0+324.sha-cb2ab26');
 
 /**
  * @license
