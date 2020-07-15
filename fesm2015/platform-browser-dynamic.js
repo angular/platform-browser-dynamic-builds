91dc0ab5ecc91fd3cfa8ddcb0d80f52620625964
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:11:17 +0000

    17ddab98fb fix(core): incorrectly validating properties on ng-content and ng-container (#37773)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4
+ * @license Angular v10.0.4+9.sha-17ddab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4');
+const VERSION = new Version('10.0.4+9.sha-17ddab9');
 
 /**
  * @license
