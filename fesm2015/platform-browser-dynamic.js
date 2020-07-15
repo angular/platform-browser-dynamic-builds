c9b267e9646127bd29851d83adc1826c93193914
atscott <atscott01@gmail.com>
2020-07-15 18:54:34 +0000

    1d9e56eba0 docs: release notes for the v10.0.4 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1
+ * @license Angular v10.1.0-next.1+1.sha-1d9e56e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1');
+const VERSION = new Version('10.1.0-next.1+1.sha-1d9e56e');
 
 /**
  * @license
