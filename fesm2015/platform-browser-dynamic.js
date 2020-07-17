ba063caa73f293d4be4227abd3f25f4d024ff039
Andrew Kushnir <akushnir@google.com>
2020-07-17 20:32:23 +0000

    38a7021d5e fix(core): error due to integer overflow when there are too many host bindings (#38014)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+26.sha-b3b03c3
+ * @license Angular v10.1.0-next.1+27.sha-38a7021
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+26.sha-b3b03c3');
+const VERSION = new Version('10.1.0-next.1+27.sha-38a7021');
 
 /**
  * @license
