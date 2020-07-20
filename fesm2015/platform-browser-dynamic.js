7d92b11b4e5f8ff3a08d43b303c95a51753d9675
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:40:23 +0000

    5b31a0a294 docs: separate template syntax into multiple docs (#36954)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
+ * @license Angular v10.1.0-next.1+36.sha-5b31a0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+35.sha-5d3d8b8');
+const VERSION = new Version('10.1.0-next.1+36.sha-5b31a0a');
 
 /**
  * @license
