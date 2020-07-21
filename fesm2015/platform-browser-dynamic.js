cf50ed495fad915ce5e2dc00f7a1c5a59ecb9035
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:32:50 +0000

    4664acce50 docs: add routing terms to glossary (#38053)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+34.sha-3797861
+ * @license Angular v10.0.4+35.sha-4664acc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+34.sha-3797861');
+const VERSION = new Version('10.0.4+35.sha-4664acc');
 
 /**
  * @license
