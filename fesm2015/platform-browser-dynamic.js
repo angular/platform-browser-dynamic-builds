cbdf6ab20d7d58d065bac21c9b1b5917d0f50e8a
Misko Hevery <misko@hevery.com>
2020-07-25 01:23:24 +0000

    a123ef58b1 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+20.sha-c4e6f58
+ * @license Angular v10.0.5+23.sha-a123ef5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+20.sha-c4e6f58');
+const VERSION = new Version('10.0.5+23.sha-a123ef5');
 
 /**
  * @license
