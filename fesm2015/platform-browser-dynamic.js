de37b44599c2bf475306cdb8c5b44a1a13c5e9dd
Misko Hevery <misko@hevery.com>
2020-07-27 22:50:58 +0000

    03a6252123 docs: update api reference doc for router link directive (#38181)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+43.sha-174aac6
+ * @license Angular v10.1.0-next.2+44.sha-03a6252
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+43.sha-174aac6');
+const VERSION = new Version('10.1.0-next.2+44.sha-03a6252');
 
 /**
  * @license
