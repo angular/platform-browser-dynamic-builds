d78154cd4f744fe034ab29f954c5aa36f3fd6e61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:44:57 +0000

    879ff08f0e fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+20.sha-a15d7ac
+ * @license Angular v10.0.6+21.sha-879ff08f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+20.sha-a15d7ac');
+const VERSION = new Version('10.0.6+21.sha-879ff08f');
 
 /**
  * @license
