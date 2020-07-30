d80b3b945c08cdd8d39e0c97613648c60668d761
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:26:40 +0000

    4a6abbdfc6 fix(compiler-cli): correct type of makeDiagnostic()

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+1.sha-0469d92
+ * @license Angular v10.0.6+19.sha-4a6abbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+1.sha-0469d92');
+const VERSION = new Version('10.0.6+19.sha-4a6abbd');
 
 /**
  * @license
