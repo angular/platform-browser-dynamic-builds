cfc71760da3894733c4500ae4c71ea0c3b6536cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:57:17 +0000

    ff9f4de4f1 fix(compiler): update unparsable character reference entity error messages (#38319)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+38.sha-3a46c2d
+ * @license Angular v10.1.0-next.3+39.sha-ff9f4de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+38.sha-3a46c2d');
+const VERSION = new Version('10.1.0-next.3+39.sha-ff9f4de');
 
 /**
  * @license
