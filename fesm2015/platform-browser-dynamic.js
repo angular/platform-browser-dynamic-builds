5fc877ceeca65a431d581d3d124f2ef0e9126785
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:20:28 +0000

    415131413d fix(router): prevent calling unsubscribe on undefined subscription in RouterPreloader (#38344)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8
+ * @license Angular v10.0.8+6.sha-4151314
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8');
+const VERSION = new Version('10.0.8+6.sha-4151314');
 
 /**
  * @license
