53d9c92776691c177c77fc96bd34b40f0fca20bc
Andrew Kushnir <akushnir@google.com>
2020-08-06 23:51:32 +0000

    deb290bc2c refactor(core): extract `icuSwitchCase`, `icuUpdateCase`, `removeNestedIcu` (#38154) (#38370)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+17.sha-f5d5bac
+ * @license Angular v10.0.8+19.sha-deb290b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+17.sha-f5d5bac');
+const VERSION = new Version('10.0.8+19.sha-deb290b');
 
 /**
  * @license
