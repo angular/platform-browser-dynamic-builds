ade309c00279db97573a4ef3b7bf6d7c2a6ee50c
Andrew Kushnir <akushnir@google.com>
2020-08-06 01:14:46 +0000

    58f4b3ad80 fix(common): ensure scrollRestoration is writable (#30630) (#38357)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+8.sha-db897f4
+ * @license Angular v10.0.8+10.sha-58f4b3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+8.sha-db897f4');
+const VERSION = new Version('10.0.8+10.sha-58f4b3a');
 
 /**
  * @license
