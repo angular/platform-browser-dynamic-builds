600ada905ab47ab956b4e469800d5081f7c479eb
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:22:07 +0000

    ef7b70a375 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+19.sha-deb290b
+ * @license Angular v10.0.8+20.sha-ef7b70a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+19.sha-deb290b');
+const VERSION = new Version('10.0.8+20.sha-ef7b70a');
 
 /**
  * @license
