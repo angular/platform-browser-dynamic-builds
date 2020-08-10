a48c44f370d22d204f5fda925ce78e40618021a8
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:34:43 +0000

    7f111491a8 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+27.sha-ee5123f
+ * @license Angular v10.0.8+28.sha-7f11149
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+27.sha-ee5123f');
+const VERSION = new Version('10.0.8+28.sha-7f11149');
 
 /**
  * @license
