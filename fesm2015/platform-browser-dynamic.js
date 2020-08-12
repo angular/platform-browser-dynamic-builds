f6066744b172699721e855c1cb772d55261b024b
Andrew Scott <atscott01@gmail.com>
2020-08-12 23:35:15 +0000

    8366effeec refactor(dev-infra): extract the commit message parsing function into its own file (#38429)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+3.sha-5f2e475
+ * @license Angular v10.1.0-next.5+4.sha-8366eff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+3.sha-5f2e475');
+const VERSION = new Version('10.1.0-next.5+4.sha-8366eff');
 
 /**
  * @license
