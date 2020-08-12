dea34d3c32325ff38542d3d5709b2776ecc1a13e
Andrew Kushnir <akushnir@google.com>
2020-08-12 17:14:33 +0000

    25f79ba73e release: cut the v10.0.9 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+40.sha-2590a9b
+ * @license Angular v10.0.9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+40.sha-2590a9b');
+const VERSION = new Version('10.0.9');
 
 /**
  * @license
