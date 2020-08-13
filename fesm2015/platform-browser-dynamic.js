93e7de075709df9ab74c41350afc789f782830b5
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:04:28 +0000

    aa816d3887 ci: disable closure size tracking test (#38449)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+9.sha-cb83b8a
+ * @license Angular v10.0.9+11.sha-aa816d3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+9.sha-cb83b8a');
+const VERSION = new Version('10.0.9+11.sha-aa816d3');
 
 /**
  * @license
