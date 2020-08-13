7b4ec5d45d640615f2dcc50dce1a4eed87b7413e
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:59:31 +0000

    d5f819ebc1 style(compiler-cli): remove unused constant (#38441)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+16.sha-1388c17
+ * @license Angular v10.1.0-next.5+17.sha-d5f819e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+16.sha-1388c17');
+const VERSION = new Version('10.1.0-next.5+17.sha-d5f819e');
 
 /**
  * @license
