46a3affb5387d2f102d30e407f116be716c720dc
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:55:00 +0000

    1388c1761f perf(compiler-cli): don't emit template guards when child scope is empty (#38418)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+13.sha-175c79d
+ * @license Angular v10.1.0-next.5+16.sha-1388c17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+13.sha-175c79d');
+const VERSION = new Version('10.1.0-next.5+16.sha-1388c17');
 
 /**
  * @license
