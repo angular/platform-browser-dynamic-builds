14a36f6af53cb98f56fb34c673627f6bf911c9dc
Andrew Scott <atscott01@gmail.com>
2020-08-13 18:00:56 +0000

    a80f654af9 fix(core): error if CSS custom property in host binding has number in name (#38432)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+9.sha-aa847cb
+ * @license Angular v10.1.0-next.5+10.sha-a80f654
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+9.sha-aa847cb');
+const VERSION = new Version('10.1.0-next.5+10.sha-a80f654');
 
 /**
  * @license
