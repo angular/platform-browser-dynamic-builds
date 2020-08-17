b9eeb496c80d3463aef1e79e877c9ce4bbc56713
Andrew Scott <atscott@google.com>
2020-08-17 18:54:22 +0000

    cfe424e875 refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+26.sha-3b9c802
+ * @license Angular v10.1.0-next.5+27.sha-cfe424e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+26.sha-3b9c802');
+const VERSION = new Version('10.1.0-next.5+27.sha-cfe424e');
 
 /**
  * @license
