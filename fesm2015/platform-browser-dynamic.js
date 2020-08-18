cc5a95ea4f2f13f799a54ad692512519ea3be560
Andrew Scott <atscott@google.com>
2020-08-18 19:19:03 +0000

    aaa1d8e2fe release: cut the zone.js-0.11.0 release (#38473)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+6.sha-dbfb50e
+ * @license Angular v10.1.0-next.6+7.sha-aaa1d8e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+6.sha-dbfb50e');
+const VERSION = new Version('10.1.0-next.6+7.sha-aaa1d8e');
 
 /**
  * @license
