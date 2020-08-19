f45e6a9e43c400c1e75d0e39825186c7c858f79b
Andrew Scott <atscott@google.com>
2020-08-19 00:25:13 +0000

    f77fd5e02a feat(dev-infra): create a wizard for building commit messages (#38457)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+8.sha-63ba74f
+ * @license Angular v10.1.0-next.6+9.sha-f77fd5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+8.sha-63ba74f');
+const VERSION = new Version('10.1.0-next.6+9.sha-f77fd5e');
 
 /**
  * @license
