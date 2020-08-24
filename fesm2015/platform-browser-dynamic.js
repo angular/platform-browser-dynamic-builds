63fedc1f255dc0004280281d31d67221f1c50e8d
Misko Hevery <misko@hevery.com>
2020-08-24 16:49:40 +0000

    8224e65e70 docs(localize): fix angular.json syntax (#38553)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+13.sha-814b436
+ * @license Angular v10.0.11+16.sha-8224e65
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11+13.sha-814b436');
+const VERSION = new Version('10.0.11+16.sha-8224e65');
 
 /**
  * @license
