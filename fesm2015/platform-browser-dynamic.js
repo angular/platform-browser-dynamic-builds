9393242eb918f54a765bdf289f769cd1845f098a
Andrew Scott <atscott@google.com>
2020-09-02 22:15:06 +0000

    5da1934115 fix(localize): render context of translation file parse errors (#38673)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+5.sha-1d8c5d8
+ * @license Angular v11.0.0-next.0+2.sha-5da1934
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+5.sha-1d8c5d8');
+const VERSION = new Version('11.0.0-next.0+2.sha-5da1934');
 
 /**
  * @license
