e834c1169759ad352932d6ace37cdb187022ea5d
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:21:32 +0000

    c01bd0fe8e release: cut the v10.1.0 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+19.sha-5588324');
+const VERSION = new Version('10.1.0');
 
 /**
  * @license
