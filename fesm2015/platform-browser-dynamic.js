4a85a887a7ce683e411c28d603a76adc4c54049b
atscott <atscott01@gmail.com>
2020-09-03 19:23:12 +0000

    25afbcc459 docs: add dayjs date adapter to resources page (#38031)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+6.sha-29c89c9
+ * @license Angular v10.1.0+7.sha-25afbcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+6.sha-29c89c9');
+const VERSION = new Version('10.1.0+7.sha-25afbcc');
 
 /**
  * @license
