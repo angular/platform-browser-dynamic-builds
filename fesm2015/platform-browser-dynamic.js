1af7db8005e68fc5a41bc24054f0d4b255033734
atscott <atscott01@gmail.com>
2020-09-09 16:36:48 +0000

    b1398d1771 docs(docs-infra): add The Deep Dive podcast, update Angular inDepth URL (#37621)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+36.sha-e156e29
+ * @license Angular v10.1.0+43.sha-b1398d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+36.sha-e156e29');
+const VERSION = new Version('10.1.0+43.sha-b1398d1');
 
 /**
  * @license
