8b9b27ea7b3b2af47db62791f44b43879bae48c3
atscott <atscott01@gmail.com>
2020-09-09 16:32:14 +0000

    d896c33b0e fix(dev-infra): set build commit message type to allow an optional scope (#38745)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+52.sha-f645d26
+ * @license Angular v11.0.0-next.0+48.sha-d896c33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+52.sha-f645d26');
+const VERSION = new Version('11.0.0-next.0+48.sha-d896c33');
 
 /**
  * @license
