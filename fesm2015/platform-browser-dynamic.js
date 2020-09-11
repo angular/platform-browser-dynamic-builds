f845f35a47cab9815da12bfe2b60ae16f168cdc0
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:25 +0000

    aa43cbf8c5 fix(dev-infra): correct build setup for dev-infra (#38815)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+18.sha-b05d79d
+ * @license Angular v10.1.1+19.sha-aa43cbf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+18.sha-b05d79d');
+const VERSION = new Version('10.1.1+19.sha-aa43cbf');
 
 /**
  * @license
