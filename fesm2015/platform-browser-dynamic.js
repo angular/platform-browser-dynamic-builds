9db35d31c01daad16e8159298fd026c95ed8513a
Misko Hevery <misko@hevery.com>
2020-09-17 17:00:26 +0000

    c4b8964424 docs: drop `newEvent()` compatibility function (#37251)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+15.sha-a33d630
+ * @license Angular v11.0.0-next.2+14.sha-c4b8964
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+15.sha-a33d630');
+const VERSION = new Version('11.0.0-next.2+14.sha-c4b8964');
 
 /**
  * @license
