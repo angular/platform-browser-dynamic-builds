c5fee4e1d6f9e210fc8ea1df6e11e92b45d1fed0
Misko Hevery <misko@hevery.com>
2020-09-29 15:05:10 +0000

    69302adc02 refactor(core): Create `NodeInjectorOffset` type which better describes NodeInjector (#38707)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+49.sha-875fcfe
+ * @license Angular v10.1.3+60.sha-69302ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+49.sha-875fcfe');
+const VERSION = new Version('10.1.3+60.sha-69302ad');
 
 /**
  * @license
