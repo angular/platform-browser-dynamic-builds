a6fb72d78151116b257029377d291d16f6b2c2c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 17:31:08 +0000

    a8134dcfd4 release: cut the v10.1.4 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+72.sha-a1bf0de
+ * @license Angular v10.1.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+72.sha-a1bf0de');
+const VERSION = new Version('10.1.4');
 
 /**
  * @license
