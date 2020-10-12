82090edeb8b97f7865ddbdd5e4b73088b8ef3dbb
atscott <atscott01@gmail.com>
2020-10-12 20:04:12 +0000

    3487d5a13a docs: move Inputs and Outputs topic to Components section (#38984)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+17.sha-1efc75c
+ * @license Angular v10.1.5+18.sha-3487d5a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+17.sha-1efc75c');
+const VERSION = new Version('10.1.5+18.sha-3487d5a');
 
 /**
  * @license
