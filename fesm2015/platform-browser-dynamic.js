f087a86e85d5ee3d92a666a00b9f7f4ec12d20f3
atscott <atscott01@gmail.com>
2020-10-14 17:48:43 +0000

    df0941554b docs: add Discord as an official communication channel (#39149)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+22.sha-5221df8
+ * @license Angular v10.1.5+23.sha-df09415
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+22.sha-5221df8');
+const VERSION = new Version('10.1.5+23.sha-df09415');
 
 /**
  * @license
