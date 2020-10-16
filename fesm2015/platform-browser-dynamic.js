19232ae322f2e7e0a01740aae056912d05d1ef86
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:44:46 +0000

    a38293d213 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+35.sha-492e236
+ * @license Angular v11.0.0-next.6+37.sha-a38293d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+35.sha-492e236');
+const VERSION = new Version('11.0.0-next.6+37.sha-a38293d');
 
 /**
  * @license
