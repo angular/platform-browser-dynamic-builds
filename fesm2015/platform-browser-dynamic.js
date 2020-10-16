24cc6e211e78d46c6997cfbd7d5a7fbbaf2b5ede
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:23:06 +0000

    94d7ef3909 refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+14.sha-af17038
+ * @license Angular v10.1.6+16.sha-94d7ef3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+14.sha-af17038');
+const VERSION = new Version('10.1.6+16.sha-94d7ef3');
 
 /**
  * @license
