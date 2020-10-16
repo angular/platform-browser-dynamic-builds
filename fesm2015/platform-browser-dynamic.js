2dafde011b856adfeb6be4616ceb4f7516c4c1ae
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:28:12 +0000

    d8e313bb6d docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+18.sha-4a5f17c
+ * @license Angular v10.1.6+20.sha-d8e313b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+18.sha-4a5f17c');
+const VERSION = new Version('10.1.6+20.sha-d8e313b');
 
 /**
  * @license
