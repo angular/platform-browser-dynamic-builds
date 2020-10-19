af996c4cfaf3b674add7c428748f01755c1bfdc2
Andrew Kushnir <akushnir@google.com>
2020-10-19 16:18:20 +0000

    38db976617 docs: add Johannes Hoppe to GDE resources (#34694)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+22.sha-10db995
+ * @license Angular v10.1.6+24.sha-38db976
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+22.sha-10db995');
+const VERSION = new Version('10.1.6+24.sha-38db976');
 
 /**
  * @license
