c04a53fda63b432eac25b691599e255ae378f0c4
Andrew Kushnir <akushnir@google.com>
2020-10-20 23:53:52 +0000

    ac53101fc6 build: upgrade karma to version 4.4.0 (#39180)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+49.sha-161b278
+ * @license Angular v11.0.0-next.6+50.sha-ac53101
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+49.sha-161b278');
+const VERSION = new Version('11.0.0-next.6+50.sha-ac53101');
 
 /**
  * @license
