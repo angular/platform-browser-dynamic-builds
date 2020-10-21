7724cd0bd9dff96fc5a805547935460c1488b6d2
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:26:12 +0000

    7768aeb62f fix(platform-server): Resolve absolute URL from baseUrl (#39334)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+65.sha-d33eaa6
+ * @license Angular v11.0.0-next.6+66.sha-7768aeb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+65.sha-d33eaa6');
+const VERSION = new Version('11.0.0-next.6+66.sha-7768aeb');
 
 /**
  * @license
