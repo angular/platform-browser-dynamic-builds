050e810ef679093598d3a344b6ab29dd432fcc65
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-27 22:21:26 +0000

    e649f1dda6 ci: bust cache of stored node_modules on CircleCI (#39461)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+102.sha-2b09f5b
+ * @license Angular v11.0.0-next.6+130.sha-e649f1d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+102.sha-2b09f5b');
+const VERSION = new Version('11.0.0-next.6+130.sha-e649f1d');
 
 /**
  * @license
