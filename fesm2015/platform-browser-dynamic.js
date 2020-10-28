2962eb0adfd1a42aecff9e0c2b9c033e62f6af3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:46:27 +0000

    bdec818b3a docs: release notes for the v11.0.0-rc.1 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+146.sha-e42d3ec
+ * @license Angular v11.0.0-next.6+147.sha-bdec818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+146.sha-e42d3ec');
+const VERSION = new Version('11.0.0-next.6+147.sha-bdec818');
 
 /**
  * @license
