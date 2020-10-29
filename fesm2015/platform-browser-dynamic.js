e0751ee627a32d5999b0e42759e8819608ec182f
Joey Perrott <josephperrott@gmail.com>
2020-10-29 18:41:14 +0000

    e7f262d691 docs(forms): update ngModel documentation (#39481)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+8.sha-8ea0ee9
+ * @license Angular v11.0.0-rc.1+9.sha-e7f262d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+8.sha-8ea0ee9');
+const VERSION = new Version('11.0.0-rc.1+9.sha-e7f262d');
 
 /**
  * @license
