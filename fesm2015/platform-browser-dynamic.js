15485b3c7b9f3237b2e5cbfbcfc8ce15b9ca9a94
Joey Perrott <josephperrott@gmail.com>
2020-10-30 20:04:44 +0000

    eb625b993e docs: update CLI workspace config documentation (#39427)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
+ * @license Angular v11.0.0-rc.1+19.sha-eb625b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+18.sha-a8e0db7');
+const VERSION = new Version('11.0.0-rc.1+19.sha-eb625b9');
 
 /**
  * @license
