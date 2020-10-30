0f6f68a1e9ef194b5680b67f193132c7f4e877aa
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:29:50 +0000

    beb935613e refactor(compiler-cli): Store inline templates and styles in the resource registry (#39482)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+166.sha-3241d92
+ * @license Angular v11.0.0-next.6+167.sha-beb9356
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+166.sha-3241d92');
+const VERSION = new Version('11.0.0-next.6+167.sha-beb9356');
 
 /**
  * @license
