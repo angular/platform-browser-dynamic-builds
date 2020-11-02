b16e24940d351720d97cd0ed46e5f775768a7a39
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:59:16 +0000

    5f1e9758f5 build(docs-infra): ensure that deployment works on CI (#39535)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+187.sha-ad62eda
+ * @license Angular v11.0.0-next.6+188.sha-5f1e975
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+187.sha-ad62eda');
+const VERSION = new Version('11.0.0-next.6+188.sha-5f1e975');
 
 /**
  * @license
