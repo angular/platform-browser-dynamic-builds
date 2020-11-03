77a9558159e778c4eb873ac50dd49c68d0feebd6
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:14:32 +0000

    7e33cb9626 fix(compiler-cli): generating invalid setClassMetadata call in ES5 for class with custom decorator (#39527)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+190.sha-db1c48f
+ * @license Angular v11.0.0-next.6+192.sha-7e33cb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+190.sha-db1c48f');
+const VERSION = new Version('11.0.0-next.6+192.sha-7e33cb9');
 
 /**
  * @license
