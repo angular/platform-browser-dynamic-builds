263f161c8a224dbb4ab1529b25b5c1d9a58b3889
atscott <atscott01@gmail.com>
2020-11-12 23:35:17 +0000

    c33326c538 refactor(compiler-cli): add keySpan to parsed events (#39609)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+248.sha-21651d3
+ * @license Angular v11.0.0-next.6+249.sha-c33326c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+248.sha-21651d3');
+const VERSION = new Version('11.0.0-next.6+249.sha-c33326c');
 
 /**
  * @license
