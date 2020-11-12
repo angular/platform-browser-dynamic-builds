41f42d419232223a0fe0c20a60d35446bbc39a6e
Misko Hevery <misko@hevery.com>
2020-11-12 00:09:13 +0000

    6d1347363e docs: update the roadmap with our priorities at Q4 (#39635)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+233.sha-8a03e3e
+ * @license Angular v11.0.0-next.6+234.sha-6d13473
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+233.sha-8a03e3e');
+const VERSION = new Version('11.0.0-next.6+234.sha-6d13473');
 
 /**
  * @license
