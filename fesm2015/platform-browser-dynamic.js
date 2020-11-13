27407544483be3247383efc0fbb4b168780ba5ee
atscott <atscott01@gmail.com>
2020-11-13 01:03:37 +0000

    06de803963 ci: fix payload size (#39666)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+7.sha-20db90a
+ * @license Angular v11.0.0+14.sha-06de803
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+7.sha-20db90a');
+const VERSION = new Version('11.0.0+14.sha-06de803');
 
 /**
  * @license
