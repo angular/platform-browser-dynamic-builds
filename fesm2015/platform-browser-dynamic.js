a186f3d85156b734fb51aba177dc3cf710810347
atscott <atscott01@gmail.com>
2020-11-18 20:33:18 +0000

    3273856d33 docs: fixes on paragraph (#39664)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+3.sha-cc509d6
+ * @license Angular v11.1.0-next.0+4.sha-3273856
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+3.sha-cc509d6');
+const VERSION = new Version('11.1.0-next.0+4.sha-3273856');
 
 /**
  * @license
