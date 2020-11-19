408a28756b54ee741537a8430ee45a2b919a8449
Andrew Kushnir <akushnir@google.com>
2020-11-19 19:19:02 +0000

    2fe2104738 ci: decrease payload size limit for AIO tests (#39766)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+3.sha-26533a0
+ * @license Angular v11.0.1+17.sha-2fe2104
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.1+3.sha-26533a0');
+const VERSION = new Version('11.0.1+17.sha-2fe2104');
 
 /**
  * @license
