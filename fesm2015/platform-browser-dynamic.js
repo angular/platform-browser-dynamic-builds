f1347bbc72b8671d1392e60531083b35eb4d634f
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:01:37 +0000

    518fbeac9b ci: enable the Selenium Promise Manager in AIO e2e tests to avoid flakiness on CI (#39905)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+74.sha-425e329
+ * @license Angular v11.0.2+76.sha-518fbea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+74.sha-425e329');
+const VERSION = new Version('11.0.2+76.sha-518fbea');
 
 /**
  * @license
