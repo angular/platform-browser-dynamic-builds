5955f6d7b0043443f4ae5b4aef214e5ce91b274e
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 23:19:36 +0000

    40b127c7a3 test(compiler-cli): migrate listener compliance tests (#39867)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+121.sha-7a5bc95
+ * @license Angular v11.1.0-next.0+123.sha-40b127c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+121.sha-7a5bc95');
+const VERSION = new Version('11.1.0-next.0+123.sha-40b127c');
 
 /**
  * @license
