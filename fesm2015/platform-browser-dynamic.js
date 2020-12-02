bcaf4739ca8224e64aa33c650b5c9e6e65acb675
Joey Perrott <josephperrott@gmail.com>
2020-12-02 19:04:03 +0000

    784667ca12 release: cut the v11.1.0-next.1 release (#39932)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+126.sha-eac85b4
+ * @license Angular v11.1.0-next.0+127.sha-784667c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+126.sha-eac85b4');
+const VERSION = new Version('11.1.0-next.0+127.sha-784667c');
 
 /**
  * @license
