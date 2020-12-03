527f0936140951cffdbfbd9989b89d07e5294f94
Misko Hevery <misko@hevery.com>
2020-12-03 22:11:40 +0000

    f01c713ee2 ci: fix AIO deployment to multiple Firebase sites (#39948)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+37.sha-3b70098
+ * @license Angular v11.1.0-next.1+34.sha-f01c713
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+37.sha-3b70098');
+const VERSION = new Version('11.1.0-next.1+34.sha-f01c713');
 
 /**
  * @license
