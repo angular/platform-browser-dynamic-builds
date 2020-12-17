7b107b83f41c3156e60a04b2aa3d76f9d710e931
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:50:24 +0000

    31448197e4 release: cut the v9.1.13 release

--- fesm5/platform-browser-dynamic.js
+++ fesm5/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -457,7 +457,7 @@ var CachedResourceLoader = /** @class */ (function (_super) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+13.sha-9ae216c');
+var VERSION = new Version('9.1.13');
 
 /**
  * @license
