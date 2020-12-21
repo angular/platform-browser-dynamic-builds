3199e7100efa6cdba7ef7b33fff07281e3a70049
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:45:58 +0000

    df7ed4160f docs: remove -webkit-transition-timing-function CSS property (#40153)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+10.sha-eae07e1
+ * @license Angular v11.1.0-next.3+11.sha-df7ed41
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+10.sha-eae07e1');
+const VERSION = new Version('11.1.0-next.3+11.sha-df7ed41');
 
 /**
  * @license
