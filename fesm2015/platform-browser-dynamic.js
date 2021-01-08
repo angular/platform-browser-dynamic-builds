0145e77f95c1147bec8ded07d62384002cffdc91
Andrew Scott <atscott@google.com>
2021-01-08 18:22:45 +0000

    6ccdb2ddba docs: add @christianliebel to list of GDEs (#39788)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.7+4.sha-40a03f0
+ * @license Angular v11.0.7+11.sha-6ccdb2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.7+4.sha-40a03f0');
+const VERSION = new Version('11.0.7+11.sha-6ccdb2d');
 
 /**
  * @license
