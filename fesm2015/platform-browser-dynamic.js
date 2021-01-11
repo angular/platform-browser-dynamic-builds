715ae533dd9002ba0df5346dcc9561102e29b8d3
atscott <atscott01@gmail.com>
2021-01-11 18:14:15 +0000

    1e46b73463 ci: add new group to approve contributor list changes (#40368)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+49.sha-94ec027
+ * @license Angular v11.1.0-next.4+50.sha-1e46b73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+49.sha-94ec027');
+const VERSION = new Version('11.1.0-next.4+50.sha-1e46b73');
 
 /**
  * @license
