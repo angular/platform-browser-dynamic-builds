3c036fea70f98c94cdf23870bc1733938e572fc8
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:43:48 +0000

    2731a4b633 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+35.sha-2c5ad5c
+ * @license Angular v11.1.0-rc.0+37.sha-2731a4b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-rc.0+35.sha-2c5ad5c');
+const VERSION = new Version('11.1.0-rc.0+37.sha-2731a4b');
 
 /**
  * @license
