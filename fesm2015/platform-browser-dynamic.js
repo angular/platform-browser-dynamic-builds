2cf1f89c2fc0fba0f9ac4c9591cd6fbe7dcd1150
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 20:02:58 +0000

    2fab148cb5 fix(forms): allow `patchValue()` method of `FormGroup` and `FormArray` classes to skip `null` values (#40534)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+36.sha-5661298
+ * @license Angular v11.1.0+39.sha-2fab148
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+36.sha-5661298');
+const VERSION = new Version('11.1.0+39.sha-2fab148');
 
 /**
  * @license
