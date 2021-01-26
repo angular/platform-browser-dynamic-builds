f5b4f25d653b19eed8f255a217b37b94055b0756
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:57:30 +0000

    97ec6e4849 fix(core): fix possible XSS attack in development through SSR (#40525)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+43.sha-a87e900
+ * @license Angular v11.1.0+45.sha-97ec6e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+43.sha-a87e900');
+const VERSION = new Version('11.1.0+45.sha-97ec6e4');
 
 /**
  * @license
