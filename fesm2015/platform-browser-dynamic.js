d4ec4e7cfd6c00d74ac16e52f887e6941a38d920
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:19:29 +0000

    9482a4df4c docs: copy paste error (#40812)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+16.sha-4c6d557
+ * @license Angular v11.2.0+18.sha-9482a4d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+16.sha-4c6d557');
+const VERSION = new Version('11.2.0+18.sha-9482a4d');
 
 /**
  * @license
