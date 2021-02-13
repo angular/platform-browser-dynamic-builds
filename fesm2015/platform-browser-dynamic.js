b8abac2e0ba4905815eb4e66c61b6fffbdadfc5e
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:10:56 +0000

    5554a3f174 docs: add NgRx book to resources (#39841)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+34.sha-9661873
+ * @license Angular v12.0.0-next.0+33.sha-5554a3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+34.sha-9661873');
+const VERSION = new Version('12.0.0-next.0+33.sha-5554a3f');
 
 /**
  * @license
