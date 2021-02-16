18b89981fdfd662166dcedd50cd88e229c5be2ea
Joey Perrott <josephperrott@gmail.com>
2021-02-16 20:01:01 +0000

    95ad452e54 fix(router): fix load interaction of navigation and preload strategies (#40389)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+44.sha-7fa8819
+ * @license Angular v12.0.0-next.0+66.sha-95ad452
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+44.sha-7fa8819');
+const VERSION = new Version('12.0.0-next.0+66.sha-95ad452');
 
 /**
  * @license
