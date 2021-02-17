609cb616f01597f141ce0b70035d934a622ad0bc
atscott <atscott01@gmail.com>
2021-02-17 20:26:29 +0000

    cdf1ea1951 refactor(compiler): retrieve variables from context inside nested template listener (#40833)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+1.sha-8658cd5
+ * @license Angular v12.0.0-next.1+3.sha-cdf1ea1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+1.sha-8658cd5');
+const VERSION = new Version('12.0.0-next.1+3.sha-cdf1ea1');
 
 /**
  * @license
