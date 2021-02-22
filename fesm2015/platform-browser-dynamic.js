414aa7eaed9ddceafff2a66691b632d6893960bd
Andrew Scott <atscott@google.com>
2021-02-22 20:56:37 +0000

    8beead1db2 docs: update year (#40925)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v11.2.1+36.sha-0d5fbea
- * (c) 2010-2020 Google LLC. https://angular.io/
+ * @license Angular v11.2.1+37.sha-8beead1
+ * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+36.sha-0d5fbea');
+const VERSION = new Version('11.2.1+37.sha-8beead1');
 
 /**
  * @license
