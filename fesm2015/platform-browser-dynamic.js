27b2e5cb6552698df56541bf945bbbbea24eeb54
Zach Arend <zachzach@google.com>
2021-02-26 20:59:03 +0000

    91cdc11aa0 fix(common): allow number or boolean as http params (#40663)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+13.sha-383d226
+ * @license Angular v12.0.0-next.2+14.sha-91cdc11
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+13.sha-383d226');
+const VERSION = new Version('12.0.0-next.2+14.sha-91cdc11');
 
 /**
  * @license
