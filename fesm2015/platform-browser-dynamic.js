bf8f4ef3753b4e95e68265b12c57bc27a2abe473
Zach Arend <zachzach@google.com>
2021-03-03 21:34:52 +0000

    6dd54972d4 fix(language-service): Always attempt HTML AST to template AST conversion for LS (#41068)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+46.sha-e4774da
+ * @license Angular v11.2.3+48.sha-6dd5497
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+46.sha-e4774da');
+const VERSION = new Version('11.2.3+48.sha-6dd5497');
 
 /**
  * @license
