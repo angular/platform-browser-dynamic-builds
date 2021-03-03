d9262bce20ec11fc25002ddf68eb27dbde80095a
Zach Arend <zachzach@google.com>
2021-03-03 23:18:00 +0000

    4606a6097b docs: release notes for the v11.2.4 release

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+58.sha-bb6e5e2
+ * @license Angular v12.0.0-next.2+59.sha-4606a60
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+58.sha-bb6e5e2');
+const VERSION = new Version('12.0.0-next.2+59.sha-4606a60');
 
 /**
  * @license
