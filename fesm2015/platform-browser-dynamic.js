d454bc3abcd29ed66a5688a6573ffbec7b64d8e3
Zach Arend <zachzach@google.com>
2021-03-03 18:23:48 +0000

    e4774da6c3 docs: update Angular Global Summit on event pages (#41050)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+38.sha-78bf697
+ * @license Angular v11.2.3+46.sha-e4774da
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+38.sha-78bf697');
+const VERSION = new Version('11.2.3+46.sha-e4774da');
 
 /**
  * @license
