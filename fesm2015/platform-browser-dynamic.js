6913cc6ecea8e1defd881b17e805b259483ae502
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:50:40 +0000

    c4ea0ab4e0 fix(docs-infra): print info to help debugging SW cache issue (#41106) (#41139)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+51.sha-16f90ca
+ * @license Angular v11.2.4+52.sha-c4ea0ab
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+51.sha-16f90ca');
+const VERSION = new Version('11.2.4+52.sha-c4ea0ab');
 
 /**
  * @license
