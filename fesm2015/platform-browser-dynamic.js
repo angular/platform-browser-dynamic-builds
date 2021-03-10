2704bf544cf6d3e25fd3d02ffd23231213d4034d
GitHub <noreply@github.com>
2021-03-10 18:23:36 +0000

    1fb0dbb300 release: cut the v12.0.0-next.4 release (#41156)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+63.sha-ab15e61
+ * @license Angular v12.0.0-next.3+64.sha-1fb0dbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+63.sha-ab15e61');
+const VERSION = new Version('12.0.0-next.3+64.sha-1fb0dbb');
 
 /**
  * @license
