38677a2e0db282f788ac42a4bb42e48bcd39a762
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:49:18 +0000

    ed6c09ae45 fix(localize): trim extracted `equiv-text` values (#41180)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+6.sha-78e6713
+ * @license Angular v11.2.5+7.sha-ed6c09a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+6.sha-78e6713');
+const VERSION = new Version('11.2.5+7.sha-ed6c09a');
 
 /**
  * @license
