67693555f7a70b319975ee75f4627244348daceb
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:52:49 +0000

    78e67130f7 docs: add stepan suvorov to GDE resources (#41178)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+4.sha-2fa231a
+ * @license Angular v11.2.5+6.sha-78e6713
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+4.sha-2fa231a');
+const VERSION = new Version('11.2.5+6.sha-78e6713');
 
 /**
  * @license
