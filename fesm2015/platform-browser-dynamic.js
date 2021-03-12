9fcd62780819a99a8b62cb3c2fd200f6afe6d379
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 22:41:01 +0000

    60ac964333 fix(forms): properly handle the change to the FormGroup shape (#40829)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+16.sha-b37296a
+ * @license Angular v11.2.5+17.sha-60ac964
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+16.sha-b37296a');
+const VERSION = new Version('11.2.5+17.sha-60ac964');
 
 /**
  * @license
