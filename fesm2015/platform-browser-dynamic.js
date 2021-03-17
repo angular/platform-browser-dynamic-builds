ffc795027d942da43d61e7dc35a3518e61e8f1d8
GitHub <noreply@github.com>
2021-03-17 18:06:07 +0000

    cbba7152fd release: cut the v11.2.6 release (#41247)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+30.sha-7803802
+ * @license Angular v11.2.5+31.sha-cbba715
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+30.sha-7803802');
+const VERSION = new Version('11.2.5+31.sha-cbba715');
 
 /**
  * @license
