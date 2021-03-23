8b98bc53776a62faddad16e580a1a148ab81b0fc
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:34:47 +0000

    0516fbb180 refactor(dev-infra): use conventional-commits-parser for commit parsing (#41286)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+36.sha-eba1289
+ * @license Angular v12.0.0-next.5+37.sha-0516fbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+36.sha-eba1289');
+const VERSION = new Version('12.0.0-next.5+37.sha-0516fbb');
 
 /**
  * @license
