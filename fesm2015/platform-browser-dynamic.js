36d8d0c802a5bbc5da51edb689115139e6142ad2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 17:16:49 +0000

    723bfc717b Revert "feat(bazel): allow setting `compilationMode` in `ng_module` rule (#41366)"

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+9.sha-06e5132
+ * @license Angular v11.2.7+13.sha-723bfc7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.7+9.sha-06e5132');
+const VERSION = new Version('11.2.7+13.sha-723bfc7');
 
 /**
  * @license
