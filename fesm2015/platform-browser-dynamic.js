69839213edea33751541039c9bbfafc60b284360
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:27:59 +0000

    c4af49335c docs(forms): fix the short description for `DefaultValueAccessor` (#41409)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+44.sha-a5a3752
+ * @license Angular v12.0.0-next.6+48.sha-c4af493
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+44.sha-a5a3752');
+const VERSION = new Version('12.0.0-next.6+48.sha-c4af493');
 
 /**
  * @license
