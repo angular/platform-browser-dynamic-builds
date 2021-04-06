bf5b925b6e4e7b922d651ed3b15187c393b32d9e
atscott <atscott01@gmail.com>
2021-04-06 20:06:28 +0000

    3f12a0dbfa build: update package.json engines range for nodes in preparation for upgrade (#41472)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+23.sha-bc74735
+ * @license Angular v11.2.8+26.sha-3f12a0d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+23.sha-bc74735');
+const VERSION = new Version('11.2.8+26.sha-3f12a0d');
 
 /**
  * @license
