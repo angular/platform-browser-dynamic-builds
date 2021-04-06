14eec2aa20f1eeba37b640c343f682c8f5ca45c9
atscott <atscott01@gmail.com>
2021-04-06 00:01:04 +0000

    6eb3dc770d build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+10.sha-754247f
+ * @license Angular v11.2.8+11.sha-6eb3dc7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+10.sha-754247f');
+const VERSION = new Version('11.2.8+11.sha-6eb3dc7');
 
 /**
  * @license
