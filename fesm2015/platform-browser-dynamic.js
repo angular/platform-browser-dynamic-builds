7e5512f5bd35b95e0f9e6c484da755f0b59f8411
atscott <atscott01@gmail.com>
2021-04-06 17:30:02 +0000

    0eecbed69f docs(router): make getCurrentNavigation() jsdoc more explicit (#41417)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+11.sha-6eb3dc7
+ * @license Angular v11.2.8+17.sha-0eecbed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+11.sha-6eb3dc7');
+const VERSION = new Version('11.2.8+17.sha-0eecbed');
 
 /**
  * @license
