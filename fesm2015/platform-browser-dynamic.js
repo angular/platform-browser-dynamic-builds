7f3fb6085013be9c70decc8971ba4fa3336e3ed7
Zach Arend <zachzach@google.com>
2021-04-08 17:43:31 +0000

    44a7fae00f fix(router): handle new navigations from a NavigationEnd event (#41262)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+16.sha-10a7c87
+ * @license Angular v12.0.0-next.8+18.sha-44a7fae
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+16.sha-10a7c87');
+const VERSION = new Version('12.0.0-next.8+18.sha-44a7fae');
 
 /**
  * @license
