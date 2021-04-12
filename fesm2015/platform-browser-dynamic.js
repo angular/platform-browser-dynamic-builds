15fd9418ccb4eac71847e2ede0d4378822dd5481
Zach Arend <zachzach@google.com>
2021-04-12 17:52:16 +0000

    419c189ad3 fix(core): error if DebugRenderer2.destroyNode is called twice in a row (#41565)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+17.sha-7946f63
+ * @license Angular v11.2.9+21.sha-419c189
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+17.sha-7946f63');
+const VERSION = new Version('11.2.9+21.sha-419c189');
 
 /**
  * @license
