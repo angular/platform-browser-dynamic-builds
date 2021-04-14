7b22225f338f67bd5a0beb0770599ce7b3754643
Zach Arend <zachzach@google.com>
2021-04-14 23:22:40 +0000

    b616f731b2 docs: add rx-angular to resources.json (#39291)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10
+ * @license Angular v11.2.10+4.sha-b616f73
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10');
+const VERSION = new Version('11.2.10+4.sha-b616f73');
 
 /**
  * @license
