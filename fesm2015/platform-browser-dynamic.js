8819065040b5465a4e5ce1248c5d11064ce5b825
Zach Arend <zachzach@google.com>
2021-04-14 17:40:23 +0000

    b8c4da9ae8 Revert "fix(compiler-cli): autocomplete literal types in templates. (#41456)" (#41623)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+37.sha-04235fa
+ * @license Angular v11.2.9+41.sha-b8c4da9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+37.sha-04235fa');
+const VERSION = new Version('11.2.9+41.sha-b8c4da9');
 
 /**
  * @license
