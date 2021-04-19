649ae274760946065f66fd7bc537d2fc552db314
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:18:37 +0000

    a76f07bd13 docs: documentation of descendants property of @ContentChildren (#35927)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+31.sha-3631934
+ * @license Angular v11.2.10+35.sha-a76f07b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+31.sha-3631934');
+const VERSION = new Version('11.2.10+35.sha-a76f07b');
 
 /**
  * @license
