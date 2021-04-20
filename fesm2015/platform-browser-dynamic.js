23187fc6d88e1de5a6ae7ae8c15e802e7be58327
Andrew Kushnir <akushnir@google.com>
2021-04-20 20:33:17 +0000

    8f3e5a5982 build(docs-infra): update docs examples to Angular v11.2.10 (#41689) (#41716)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+39.sha-20e0917
+ * @license Angular v11.2.10+41.sha-8f3e5a5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+39.sha-20e0917');
+const VERSION = new Version('11.2.10+41.sha-8f3e5a5');
 
 /**
  * @license
