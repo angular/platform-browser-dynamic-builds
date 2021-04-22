0a15cdf115579aab0ccaf6976267600782891b2f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:51:16 +0000

    2acb8d1865 fix(dev-infra): verify python3 is available before release (#41753)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+2.sha-0830885
+ * @license Angular v12.0.0-rc.0+6.sha-2acb8d1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.0+2.sha-0830885');
+const VERSION = new Version('12.0.0-rc.0+6.sha-2acb8d1');
 
 /**
  * @license
