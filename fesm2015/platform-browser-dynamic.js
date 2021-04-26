58854f278c285065d7ca94397fdbb0ba90d3fd1b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 23:20:18 +0000

    7756ead0bf docs: add metadata documentation to httpClient (#41706)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+18.sha-aa578f5
+ * @license Angular v11.2.11+20.sha-7756ead
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+18.sha-aa578f5');
+const VERSION = new Version('11.2.11+20.sha-7756ead');
 
 /**
  * @license
