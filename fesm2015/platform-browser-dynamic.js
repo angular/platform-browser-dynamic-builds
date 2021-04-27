ee7f57ccbf97ac8a82c457d9321252e3b181d794
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:56:53 +0000

    e7586581af ci: add Zach Arend to pullapprove groups (#41839)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+23.sha-84a44a9
+ * @license Angular v11.2.11+24.sha-e758658
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+23.sha-84a44a9');
+const VERSION = new Version('11.2.11+24.sha-e758658');
 
 /**
  * @license
