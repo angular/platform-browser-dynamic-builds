6f8491889c5d5a7164e457bb91df52606239d0d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:12:47 +0000

    4fac102966 build: update release configuration to use new release notes system (#41905)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13
+ * @license Angular v11.2.13+3.sha-4fac102
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13');
+const VERSION = new Version('11.2.13+3.sha-4fac102');
 
 /**
  * @license
