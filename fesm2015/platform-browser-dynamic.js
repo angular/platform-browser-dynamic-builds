8412347ad488d4738d678ea84e013a5da608db8c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:30:33 +0000

    5f7a401fef release: cut the v11.2.14 release (#42070)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+29.sha-5f7a401
+ * @license Angular v11.2.14
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.13+29.sha-5f7a401');
+const VERSION = new Version('11.2.14');
 
 /**
  * @license
