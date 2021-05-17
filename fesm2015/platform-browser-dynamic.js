d8486cccd766e0e5d8f2be8d90b350c62271b860
atscott <atscott01@gmail.com>
2021-05-17 18:15:58 +0000

    fc1bc0e0e8 docs: update examples to use correct `Injector.create()` overload (#42006)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+417.sha-e86a1d3
+ * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+417.sha-e86a1d3');
+const VERSION = new Version('12.0.0-next.8+418.sha-fc1bc0e');
 
 /**
  * @license
