0143e86cad2243a6798f37b036b95fd263d8787d
Andrew Scott <atscott@google.com>
2021-05-18 22:59:40 +0000

    aa9a86cfd5 build: remove publishConfig entry from package.json entries (#42104)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+34.sha-a20ef16
+ * @license Angular v12.0.0+37.sha-aa9a86c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+34.sha-a20ef16');
+const VERSION = new Version('12.0.0+37.sha-aa9a86c');
 
 /**
  * @license
