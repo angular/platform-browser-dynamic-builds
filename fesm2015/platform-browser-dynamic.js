937c664728ef84f5b9db0a4eb29479a5b675abdf
Joey Perrott <josephperrott@gmail.com>
2021-05-21 21:01:15 +0000

    99802dc132 ci: update to latest feature request triage bot commit (#42229)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+10.sha-ce6d49f
+ * @license Angular v12.0.1+15.sha-99802dc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+10.sha-ce6d49f');
+const VERSION = new Version('12.0.1+15.sha-99802dc');
 
 /**
  * @license
