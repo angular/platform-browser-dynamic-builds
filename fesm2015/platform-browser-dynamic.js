7c85e764157574cd75f9ddabf02b3be9592520bc
Zach Arend <zachzach@google.com>
2021-05-25 17:44:20 +0000

    12b1e87e6c build: update CLI packages to 12.0.1 (#42307)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+15.sha-99802dc
+ * @license Angular v12.0.1+38.sha-12b1e87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+15.sha-99802dc');
+const VERSION = new Version('12.0.1+38.sha-12b1e87');
 
 /**
  * @license
