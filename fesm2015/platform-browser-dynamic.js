3c970495e86c8b7f6f88e47d7e08326decbf3204
Zach Arend <zachzach@google.com>
2021-05-26 18:55:24 +0000

    5a51884069 fix(dev-infra): build with stamping for release builds (#42354)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+84.sha-037b636
+ * @license Angular v12.0.2+2.sha-5a51884
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+84.sha-037b636');
+const VERSION = new Version('12.0.2+2.sha-5a51884');
 
 /**
  * @license
