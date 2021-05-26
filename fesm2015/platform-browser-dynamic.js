ea7075fe7a6f309f7b547bf137dccbe168c53fbc
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:45:29 +0000

    765854ed8f build: allow node v15 and above for aio (#42367)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+15.sha-12e7968
+ * @license Angular v12.0.2+21.sha-765854e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+15.sha-12e7968');
+const VERSION = new Version('12.0.2+21.sha-765854e');
 
 /**
  * @license
