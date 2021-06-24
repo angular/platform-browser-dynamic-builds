733aa1a607bfa4e1275694a5fbb7dac561ccc9e2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:15:27 +0000

    b31e905754 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+39.sha-4459741
+ * @license Angular v12.0.5+42.sha-b31e905
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.5+39.sha-4459741');
+const VERSION = new Version('12.0.5+42.sha-b31e905');
 
 /**
  * @license
