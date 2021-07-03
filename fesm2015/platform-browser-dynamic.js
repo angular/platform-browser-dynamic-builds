6d228c85e3a378ad2e4c056fea0c59d5a951aaf1
atscott <atscott01@gmail.com>
2021-07-03 00:35:53 +0000

    9557e87ad8 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+12.sha-6da24e6
+ * @license Angular v12.1.1+16.sha-9557e87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+12.sha-6da24e6');
+const VERSION = new Version('12.1.1+16.sha-9557e87');
 
 /**
  * @license
