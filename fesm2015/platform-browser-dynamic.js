43d9e5094c69237fc4f35d52217be02a76564c0b
atscott <atscott01@gmail.com>
2021-07-08 21:27:52 +0000

    f6bce3158a ci: use issue forms for issue creation (#42099)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+39.sha-d879175
+ * @license Angular v12.1.1+41.sha-f6bce31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+39.sha-d879175');
+const VERSION = new Version('12.1.1+41.sha-f6bce31');
 
 /**
  * @license
