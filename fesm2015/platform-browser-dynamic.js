3114223ccd14ca1454f4ea5d3ce51ed8621ef00a
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:55:24 +0000

    d654c7933a fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- fesm2015/platform-browser-dynamic.js
+++ fesm2015/platform-browser-dynamic.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+92.sha-b101ad0
+ * @license Angular v12.1.1+93.sha-d654c79
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -451,7 +451,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.1+92.sha-b101ad0');
+const VERSION = new Version('12.1.1+93.sha-d654c79');
 
 /**
  * @license
