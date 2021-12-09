d00aaea3e42e75bcb28ade4dc416d8287efdfc27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 18:43:12 +0000

    a7c117ab44 release: cut the v13.2.0-next.0 release (#44422)

--- esm2020/testing/src/testing.mjs
+++ esm2020/testing/src/testing.mjs
@@ -24,12 +24,12 @@ export const platformBrowserDynamicTesting = createPlatformFactory(platformCoreD
  */
 export class BrowserDynamicTestingModule {
 }
-BrowserDynamicTestingModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.3+27.sha-79baf7a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-BrowserDynamicTestingModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "12.0.0", version: "13.1.0-next.3+27.sha-79baf7a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, exports: [BrowserTestingModule] });
-BrowserDynamicTestingModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "13.1.0-next.3+27.sha-79baf7a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, providers: [
+BrowserDynamicTestingModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.3+28.sha-a7c117a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+BrowserDynamicTestingModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "12.0.0", version: "13.1.0-next.3+28.sha-a7c117a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, exports: [BrowserTestingModule] });
+BrowserDynamicTestingModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "13.1.0-next.3+28.sha-a7c117a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, providers: [
         { provide: TestComponentRenderer, useClass: DOMTestComponentRenderer },
     ], imports: [BrowserTestingModule] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.3+27.sha-79baf7a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.3+28.sha-a7c117a.with-local-changes", ngImport: i0, type: BrowserDynamicTestingModule, decorators: [{
             type: NgModule,
             args: [{
                     exports: [BrowserTestingModule],
