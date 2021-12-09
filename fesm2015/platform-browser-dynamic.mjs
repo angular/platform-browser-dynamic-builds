04cb2572c5533f42f3569de751a055dd4f76ab69
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 17:30:55 +0000

    532d7d436f docs: release notes for the v13.1.0 release (#44419)

--- fesm2015/platform-browser-dynamic.mjs
+++ fesm2015/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+24.sha-cab7a84.with-local-changes
+ * @license Angular v13.1.0-next.3+25.sha-532d7d4.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -154,9 +154,9 @@ class ResourceLoaderImpl extends ResourceLoader {
         return promise;
     }
 }
-ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.3+24.sha-cab7a84.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.1.0-next.3+24.sha-cab7a84.with-local-changes", ngImport: i0, type: ResourceLoaderImpl });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.3+24.sha-cab7a84.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
+ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.1.0-next.3+25.sha-532d7d4.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.1.0-next.3+25.sha-532d7d4.with-local-changes", ngImport: i0, type: ResourceLoaderImpl });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.1.0-next.3+25.sha-532d7d4.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
             type: Injectable
         }] });
 
@@ -232,7 +232,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.1.0-next.3+24.sha-cab7a84.with-local-changes');
+const VERSION = new Version('13.1.0-next.3+25.sha-532d7d4.with-local-changes');
 
 /**
  * @license
