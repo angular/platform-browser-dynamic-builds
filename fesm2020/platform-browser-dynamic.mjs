19af2f80fae45338e72cd406a3b5e1fc67727887
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:40:55 +0000

    05b8de7523 build: update angular (#44771)

--- fesm2020/platform-browser-dynamic.mjs
+++ fesm2020/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+74.sha-6a68bab.with-local-changes
+ * @license Angular v13.2.0-next.2+75.sha-05b8de7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -154,9 +154,9 @@ class ResourceLoaderImpl extends ResourceLoader {
         return promise;
     }
 }
-ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.2.0-next.2+74.sha-6a68bab.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.2.0-next.2+74.sha-6a68bab.with-local-changes", ngImport: i0, type: ResourceLoaderImpl });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.2.0-next.2+74.sha-6a68bab.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
+ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.2.0-next.2+75.sha-05b8de7.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.2.0-next.2+75.sha-05b8de7.with-local-changes", ngImport: i0, type: ResourceLoaderImpl });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.2.0-next.2+75.sha-05b8de7.with-local-changes", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
             type: Injectable
         }] });
 
@@ -235,7 +235,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.2.0-next.2+74.sha-6a68bab.with-local-changes');
+const VERSION = new Version('13.2.0-next.2+75.sha-05b8de7.with-local-changes');
 
 /**
  * @license
