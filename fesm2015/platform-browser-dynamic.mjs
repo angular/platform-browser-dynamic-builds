e9a3976dfb1cc6d8ca08b512e36757046f61ad1d
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 20:01:08 +0000

    7ee121f595 feat(forms): Add untyped versions of the model classes for use in migration. (#45205)

--- fesm2015/platform-browser-dynamic.mjs
+++ fesm2015/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+33.sha-dfc4301
+ * @license Angular v14.0.0-next.4+32.sha-7ee121f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -154,9 +154,9 @@ class ResourceLoaderImpl extends ResourceLoader {
         return promise;
     }
 }
-ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.4+33.sha-dfc4301", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.4+33.sha-dfc4301", ngImport: i0, type: ResourceLoaderImpl });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.4+33.sha-dfc4301", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
+ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.4+32.sha-7ee121f", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.0.0-next.4+32.sha-7ee121f", ngImport: i0, type: ResourceLoaderImpl });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.4+32.sha-7ee121f", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
             type: Injectable
         }] });
 
@@ -235,7 +235,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('14.0.0-next.4+33.sha-dfc4301');
+const VERSION = new Version('14.0.0-next.4+32.sha-7ee121f');
 
 /**
  * @license
