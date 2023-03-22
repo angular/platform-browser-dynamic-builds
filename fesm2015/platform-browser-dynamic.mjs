fde4f4f6931608592286205706aca776fd7b95f6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 16:03:49 +0000

    ff9d3b0f02 refactor(router): Remove legacy url creation code (#49360)

--- fesm2015/platform-browser-dynamic.mjs
+++ fesm2015/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-41f27ad
+ * @license Angular v16.0.0-next.3+sha-ff9d3b0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -132,9 +132,9 @@ class ResourceLoaderImpl extends ResourceLoader {
         return promise;
     }
 }
-ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-41f27ad", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-41f27ad", ngImport: i0, type: ResourceLoaderImpl });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-41f27ad", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
+ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-ff9d3b0", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-ff9d3b0", ngImport: i0, type: ResourceLoaderImpl });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-ff9d3b0", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
             type: Injectable
         }] });
 
@@ -189,7 +189,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('16.0.0-next.3+sha-41f27ad');
+const VERSION = new Version('16.0.0-next.3+sha-ff9d3b0');
 
 /**
  * @publicApi
