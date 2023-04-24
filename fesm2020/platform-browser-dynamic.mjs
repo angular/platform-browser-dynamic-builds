e05dcedde35c7959d9083ddaf04dd3cd12aa3f2f
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 17:08:07 +0000

    c17b84a33b docs: applicability of NgModule providers (#49966)

--- fesm2020/platform-browser-dynamic.mjs
+++ fesm2020/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-898acfa
+ * @license Angular v15.2.8+sha-c17b84a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -134,9 +134,9 @@ class ResourceLoaderImpl extends ResourceLoader {
         return promise;
     }
 }
-ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.8+sha-898acfa", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.2.8+sha-898acfa", ngImport: i0, type: ResourceLoaderImpl });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.8+sha-898acfa", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
+ResourceLoaderImpl.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.8+sha-c17b84a", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+ResourceLoaderImpl.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.2.8+sha-c17b84a", ngImport: i0, type: ResourceLoaderImpl });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.8+sha-c17b84a", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
             type: Injectable
         }] });
 
@@ -191,7 +191,7 @@ class CachedResourceLoader extends ResourceLoader {
 /**
  * @publicApi
  */
-const VERSION = new Version('15.2.8+sha-898acfa');
+const VERSION = new Version('15.2.8+sha-c17b84a');
 
 /**
  * @publicApi
