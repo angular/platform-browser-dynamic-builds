cf2a193c72f32dc2ed837dfb424bc96805d9647f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-01-08 18:29:58 +0000

    77b24cd release: cut the v19.1.0-rc.0 release

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.1.0-next.4+sha-c69f12f
+ * @license Angular v19.1.0-rc.0+sha-77b24cd
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -113,10 +113,10 @@ class ResourceLoaderImpl extends ResourceLoader {
         xhr.send();
         return promise;
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.1.0-next.4+sha-c69f12f", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.1.0-next.4+sha-c69f12f", ngImport: i0, type: ResourceLoaderImpl });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.1.0-rc.0+sha-77b24cd", ngImport: i0, type: ResourceLoaderImpl, deps: null, target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.1.0-rc.0+sha-77b24cd", ngImport: i0, type: ResourceLoaderImpl });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.1.0-next.4+sha-c69f12f", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.1.0-rc.0+sha-77b24cd", ngImport: i0, type: ResourceLoaderImpl, decorators: [{
             type: Injectable
         }] });
 
@@ -141,7 +141,7 @@ const INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS = [
 /**
  * @publicApi
  */
-const VERSION = new Version('19.1.0-next.4+sha-c69f12f');
+const VERSION = new Version('19.1.0-rc.0+sha-77b24cd');
 
 /**
  * @publicApi
