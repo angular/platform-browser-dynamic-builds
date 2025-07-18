e0d7d4b7c17cd6c77a8fbc85098a96dbd31d065b
Kirill Cherkashin <kirjs@google.com>
2025-07-18 11:09:40 +0000

    d3b770d refactor(devtools): restyle the signal details panel (#62681)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-23d5877
+ * @license Angular v20.2.0-next.1+sha-d3b770d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,11 +22,11 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
  * @deprecated Use the `BrowserTestingModule` from `@angular/platform-browser/testing` instead.
  */
 class BrowserDynamicTestingModule {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.2.0-next.1+sha-23d5877", ngImport: i0, type: BrowserDynamicTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.2.0-next.1+sha-23d5877", ngImport: i0, type: BrowserDynamicTestingModule, exports: [BrowserTestingModule] });
-    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.2.0-next.1+sha-23d5877", ngImport: i0, type: BrowserDynamicTestingModule, imports: [BrowserTestingModule] });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.2.0-next.1+sha-d3b770d", ngImport: i0, type: BrowserDynamicTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.2.0-next.1+sha-d3b770d", ngImport: i0, type: BrowserDynamicTestingModule, exports: [BrowserTestingModule] });
+    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.2.0-next.1+sha-d3b770d", ngImport: i0, type: BrowserDynamicTestingModule, imports: [BrowserTestingModule] });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.1+sha-23d5877", ngImport: i0, type: BrowserDynamicTestingModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.1+sha-d3b770d", ngImport: i0, type: BrowserDynamicTestingModule, decorators: [{
             type: NgModule,
             args: [{
                     exports: [BrowserTestingModule],
