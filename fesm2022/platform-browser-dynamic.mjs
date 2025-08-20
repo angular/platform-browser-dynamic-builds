4fecb45139eb0d905dfe1c06486715b4b7b55608
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:13:26 +0000

    9bdb4fe build: reenable firefox tests for devtools (#63288)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-11c6385
+ * @license Angular v20.2.0+sha-9bdb4fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-11c6385');
+const VERSION = new Version('20.2.0+sha-9bdb4fe');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
