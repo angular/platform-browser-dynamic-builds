98b5862c6a75682b497d924a67d6ec556ad6b312
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-20 12:49:04 +0000

    a0ee6bd release: cut the v21.0.0-next.0 release

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d1df128
+ * @license Angular v21.0.0-next.0+sha-a0ee6bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-d1df128');
+const VERSION = new Version('21.0.0-next.0+sha-a0ee6bd');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
