653705317325d117c6949033f7e0ef3290e0a254
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 13:10:07 +0000

    4dfa6f1 docs(docs-infra): auto-links to API entries (#63162)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a0ee6bd
+ * @license Angular v21.0.0-next.0+sha-4dfa6f1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a0ee6bd');
+const VERSION = new Version('21.0.0-next.0+sha-4dfa6f1');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
