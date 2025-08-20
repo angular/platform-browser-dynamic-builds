7f9475c671e36cfdd8bdc15ae570765b01d2bcc1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 15:53:57 +0000

    d8bdf56 refactor(core): remove deprecated `PACKAGE_ROOT_URL` token (#63274)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-25f593c
+ * @license Angular v21.0.0-next.0+sha-d8bdf56
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-25f593c');
+const VERSION = new Version('21.0.0-next.0+sha-d8bdf56');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
