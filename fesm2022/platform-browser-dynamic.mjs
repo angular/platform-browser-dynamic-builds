d8eec64d35f02932d8ec31596046d0fcdebbc069
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:05:50 +0000

    a842272 docs(docs-infra): fix spelling (#63334)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-56a8408
+ * @license Angular v21.0.0-next.0+sha-a842272
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-56a8408');
+const VERSION = new Version('21.0.0-next.0+sha-a842272');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
