6fe5598b88c2c71095c759441e2fc35f3580d125
Andrew Kushnir <akushnir@google.com>
2025-08-26 21:57:35 +0000

    a43057c refactor(core): Create a base effect interface and prototype to be used by both angular and wiz. (#62931)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-dfa2044
+ * @license Angular v21.0.0-next.0+sha-a43057c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-dfa2044');
+const VERSION = new Version('21.0.0-next.0+sha-a43057c');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
