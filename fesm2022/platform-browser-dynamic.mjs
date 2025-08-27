51d1390fb864dbf34289b6533db2a7d98fe4c55c
Miles Malerba <mmalerba@google.com>
2025-08-27 23:22:27 +0000

    059e0d3 docs: add customizing route behavior guide for router (#63398)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1bb30c7
+ * @license Angular v21.0.0-next.1+sha-059e0d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1bb30c7');
+const VERSION = new Version('21.0.0-next.1+sha-059e0d3');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
