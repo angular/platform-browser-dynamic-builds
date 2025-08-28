594615d1a029e7d9ea533b85aeff2fd812751756
Miles Malerba <mmalerba@google.com>
2025-08-28 16:15:58 +0000

    cac0852 docs: update Angular CLI help (#63445)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-b8314bd
+ * @license Angular v21.0.0-next.1+sha-cac0852
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-b8314bd');
+const VERSION = new Version('21.0.0-next.1+sha-cac0852');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
