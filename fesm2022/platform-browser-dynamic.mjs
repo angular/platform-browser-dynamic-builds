063bf2d1eeae1f31c2f619ce32879c6921c446d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 09:59:35 +0000

    87bfe16 build: define typescript version via string in module.bazel file (#63434)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-a6a46e9
+ * @license Angular v20.2.2+sha-87bfe16
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-a6a46e9');
+const VERSION = new Version('20.2.2+sha-87bfe16');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
