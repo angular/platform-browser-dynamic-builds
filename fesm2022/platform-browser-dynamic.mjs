2b48590bb6870f86f983cc7f21d2d33497eefedb
Miles Malerba <mmalerba@google.com>
2025-08-29 21:47:58 +0000

    db95a5c refactor(forms): loosen the error and disabled type on FormUiControl (#63455)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-862495c
+ * @license Angular v21.0.0-next.1+sha-db95a5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-862495c');
+const VERSION = new Version('21.0.0-next.1+sha-db95a5c');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
