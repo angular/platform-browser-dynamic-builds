e1e5098e89909bea640bc198cb21ea81ae42c66c
Miles Malerba <mmalerba@google.com>
2025-08-29 21:10:53 +0000

    e9096b9 docs(docs-infra): properties should be readonly (#63492)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-5bb4b75
+ * @license Angular v20.2.3+sha-e9096b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-5bb4b75');
+const VERSION = new Version('20.2.3+sha-e9096b9');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
