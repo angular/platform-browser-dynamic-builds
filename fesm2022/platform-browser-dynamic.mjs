f935fb8060752319c590f38d6017a87ce58d0f1f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:05:49 +0000

    a3938d5 docs(docs-infra): makes signal readonly (#63449)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1875d5c
+ * @license Angular v21.0.0-next.1+sha-a3938d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1875d5c');
+const VERSION = new Version('21.0.0-next.1+sha-a3938d5');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
