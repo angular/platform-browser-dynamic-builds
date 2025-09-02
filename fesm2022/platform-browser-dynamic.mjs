468833dc2180383cdf07a70b56083bbb131a5174
Miles Malerba <mmalerba@google.com>
2025-09-02 16:48:58 +0000

    d6f7f68 docs(docs-infra): signal property should be readonly (#63166)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-3b42314
+ * @license Angular v20.2.3+sha-d6f7f68
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-3b42314');
+const VERSION = new Version('20.2.3+sha-d6f7f68');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
