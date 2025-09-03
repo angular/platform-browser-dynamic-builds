3b3e194f4f9c9b56ab4f4f1ec05c1751c550cff3
Miles Malerba <mmalerba@google.com>
2025-09-03 04:07:54 +0000

    d006721 feat(devtools): clean up router tree for stable release (#63081)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-475ed59
+ * @license Angular v21.0.0-next.1+sha-d006721
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-475ed59');
+const VERSION = new Version('21.0.0-next.1+sha-d006721');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
