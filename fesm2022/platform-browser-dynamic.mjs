e2f6f34b6d7b791a440177822cd17c16f7bc01f3
Miles Malerba <mmalerba@google.com>
2025-09-03 16:04:59 +0000

    e6949ad docs: fix #63555 (#63560)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-d006721
+ * @license Angular v21.0.0-next.1+sha-e6949ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-d006721');
+const VERSION = new Version('21.0.0-next.1+sha-e6949ad');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
