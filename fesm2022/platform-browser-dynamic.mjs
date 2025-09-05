44ad18f4ea1efb0fc0e034271f2bb6b3adda4f42
Andrew Scott <atscott@google.com>
2025-09-05 19:57:36 +0000

    f560536 test: increase mermaid spec timeout to reduce flakes (#63610)

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-8401f89
+ * @license Angular v21.0.0-next.2+sha-f560536
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17,7 +17,7 @@ import { platformBrowser } from '@angular/platform-browser';
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-8401f89');
+const VERSION = new Version('21.0.0-next.2+sha-f560536');
 
 const COMPILER_PROVIDERS = [
     { provide: Compiler, useFactory: () => new Compiler() },
