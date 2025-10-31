10d582b750c7cbd1380c6d4a93aa90e7ec5037eb
Andrew Scott <atscott01@gmail.com>
2025-10-31 21:59:59 +0000

    334f856 build: update dependency bazel to v7.7.0

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a0fe177
+ * @license Angular v21.1.0-next.0+sha-334f856
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = new Version('21.1.0-next.0+sha-a0fe177');
+const VERSION = new Version('21.1.0-next.0+sha-334f856');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-a0fe177",
+    version: "21.1.0-next.0+sha-334f856",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-a0fe177",
+    version: "21.1.0-next.0+sha-334f856",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-a0fe177",
+  version: "21.1.0-next.0+sha-334f856",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
