211ebcbd518aa97df8d9c40efb0253b169a3979c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:40:19 +0000

    129e16d docs: correct code block language in error handling and signals interop

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-18cc260
+ * @license Angular v21.0.0-rc.2+sha-129e16d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-18cc260');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-129e16d');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-18cc260",
+    version: "21.0.0-rc.2+sha-129e16d",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-18cc260",
+    version: "21.0.0-rc.2+sha-129e16d",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-18cc260",
+  version: "21.0.0-rc.2+sha-129e16d",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
