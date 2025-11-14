c151cb900de0614a1dd92c67919077ee7805a584
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:02:02 +0000

    6d8c3fc docs(docs-infra): hide file tabs when code example is hidden

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-024c939
+ * @license Angular v21.1.0-next.0+sha-6d8c3fc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-024c939');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6d8c3fc');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-024c939",
+    version: "21.1.0-next.0+sha-6d8c3fc",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-024c939",
+    version: "21.1.0-next.0+sha-6d8c3fc",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-024c939",
+  version: "21.1.0-next.0+sha-6d8c3fc",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
