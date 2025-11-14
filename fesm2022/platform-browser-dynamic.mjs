3a2e286cdf5032c845d6988011de253386eaa753
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 19:32:04 +0000

    8a5b284 docs: add draft of aria component guides

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-966bfae
+ * @license Angular v21.0.0-rc.2+sha-8a5b284
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-966bfae');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-8a5b284');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-966bfae",
+    version: "21.0.0-rc.2+sha-8a5b284",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-966bfae",
+    version: "21.0.0-rc.2+sha-8a5b284",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-966bfae",
+  version: "21.0.0-rc.2+sha-8a5b284",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
