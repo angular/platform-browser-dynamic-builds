296107cbb0589c49a1f774d9e1ff0f6663b105ba
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:07:00 +0000

    c2a52fd docs: add aria toolbar component guide

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-750da17
+ * @license Angular v21.0.0-rc.2+sha-c2a52fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-750da17",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.0-rc.2+sha-750da17",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-750da17",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-750da17",
+  version: "21.0.0-rc.2+sha-c2a52fd",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
