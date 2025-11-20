ad156075eb6a11dfe37d2947a64a1b7d304edbf1
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 20:04:45 +0000

    396c25e docs: prioritize ng add for Tailwind CSS and clarify manual steps

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5ddeafc
+ * @license Angular v21.1.0-next.0+sha-396c25e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5ddeafc');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-396c25e');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-5ddeafc",
+    version: "21.1.0-next.0+sha-396c25e",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-5ddeafc",
+    version: "21.1.0-next.0+sha-396c25e",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-5ddeafc",
+  version: "21.1.0-next.0+sha-396c25e",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
