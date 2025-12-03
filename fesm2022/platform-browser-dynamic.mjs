bc45e4685b32b38f35a98d4622ca9cb05afaa610
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 12:43:05 +0000

    dfd7d3b build: lock file maintenance

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3e89577
+ * @license Angular v21.1.0-next.0+sha-dfd7d3b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3e89577');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-dfd7d3b');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-3e89577",
+    version: "21.1.0-next.0+sha-dfd7d3b",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-3e89577",
+    version: "21.1.0-next.0+sha-dfd7d3b",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-3e89577",
+  version: "21.1.0-next.0+sha-dfd7d3b",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
