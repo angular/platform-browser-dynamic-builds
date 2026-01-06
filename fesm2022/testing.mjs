e395f5be5c3e5a237d16d31423dd5ea1bf5c9449
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 17:27:07 +0000

    10da2f9 fix(compiler): better types for a few expression AST nodes

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-b93d5ec
+ * @license Angular v21.1.0-next.4+sha-10da2f9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.4+sha-b93d5ec",
+    version: "21.1.0-next.4+sha-10da2f9",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0-next.4+sha-b93d5ec",
+    version: "21.1.0-next.4+sha-10da2f9",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0-next.4+sha-b93d5ec",
+    version: "21.1.0-next.4+sha-10da2f9",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.4+sha-b93d5ec",
+  version: "21.1.0-next.4+sha-10da2f9",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
