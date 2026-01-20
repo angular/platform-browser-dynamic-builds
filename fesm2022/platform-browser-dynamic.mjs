c0277a4f8ad6b5d5cd5f72259431377b30a1b91d
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 17:42:03 +0000

    82593de fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-6b34e5f-with-local-changes
+ * @license Angular v21.1.0+sha-82593de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-6b34e5f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-82593de');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-6b34e5f-with-local-changes",
+    version: "21.1.0+sha-82593de",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.0+sha-6b34e5f-with-local-changes",
+    version: "21.1.0+sha-82593de",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0+sha-6b34e5f-with-local-changes",
+  version: "21.1.0+sha-82593de",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
