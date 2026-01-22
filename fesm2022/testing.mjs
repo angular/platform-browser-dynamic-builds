9fc7fbc9be7430d079666c63631f71af9704d899
Andrew Scott <atscott01@gmail.com>
2026-01-22 23:36:35 +0000

    d1135f5 build: update dependency aspect_rules_js to v2.9.2

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-30b50e7
+ * @license Angular v21.2.0-next.0+sha-d1135f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-30b50e7",
+    version: "21.2.0-next.0+sha-d1135f5",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.2.0-next.0+sha-30b50e7",
+    version: "21.2.0-next.0+sha-d1135f5",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-30b50e7",
+    version: "21.2.0-next.0+sha-d1135f5",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-30b50e7",
+  version: "21.2.0-next.0+sha-d1135f5",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
