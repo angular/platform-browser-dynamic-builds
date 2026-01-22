17776826c0b14aae4995267bbed186af2a80401a
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:11:38 +0000

    c9043fb refactor(compiler-cli): remove deprecated signature usage

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-61dbdee
+ * @license Angular v21.1.1+sha-c9043fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-61dbdee');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-c9043fb');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-61dbdee",
+    version: "21.1.1+sha-c9043fb",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-61dbdee",
+    version: "21.1.1+sha-c9043fb",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-61dbdee",
+  version: "21.1.1+sha-c9043fb",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
