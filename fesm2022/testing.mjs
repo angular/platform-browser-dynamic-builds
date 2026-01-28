875a10b6c8cf51a5187de6b117b00f21f2e44222
Andrew Scott <atscott01@gmail.com>
2026-01-28 19:35:40 +0000

    290b281 build: update cross-repo angular dependencies

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-9f99b14
+ * @license Angular v21.1.1+sha-290b281
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-9f99b14",
+    version: "21.1.1+sha-290b281",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.1+sha-9f99b14",
+    version: "21.1.1+sha-290b281",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-9f99b14",
+    version: "21.1.1+sha-290b281",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-9f99b14",
+  version: "21.1.1+sha-290b281",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
