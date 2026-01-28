71789c1b89373d207cbdedcde4154e6d7a0e55ca
Andrew Scott <atscott01@gmail.com>
2026-01-28 21:02:07 +0000

    cfe7340 refactor(core): remove unused restriction parameter

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-4f0d29d
+ * @license Angular v21.1.2+sha-cfe7340
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.2+sha-4f0d29d",
+    version: "21.1.2+sha-cfe7340",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.2+sha-4f0d29d",
+    version: "21.1.2+sha-cfe7340",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.2+sha-4f0d29d",
+    version: "21.1.2+sha-cfe7340",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.2+sha-4f0d29d",
+  version: "21.1.2+sha-cfe7340",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
