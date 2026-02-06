f35dffa456e9d003c1121fe147671fcb44e0ad6a
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 15:47:55 +0000

    24c0c5a feat(forms): support signal-based schemas in validateStandardSchema

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-6fa2270
+ * @license Angular v21.2.0-next.2+sha-24c0c5a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-6fa2270');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-24c0c5a');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-6fa2270",
+    version: "21.2.0-next.2+sha-24c0c5a",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-6fa2270",
+    version: "21.2.0-next.2+sha-24c0c5a",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.2+sha-6fa2270",
+  version: "21.2.0-next.2+sha-24c0c5a",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
