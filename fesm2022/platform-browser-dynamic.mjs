3cfd51d98591b962d3a8d166db249e8fcf97a702
Matthew Beck <me@mattrb.com>
2026-02-17 22:42:46 +0000

    76d1a37 docs(docs-infra): Signal forms docs Update docs signal forms errors input wrong typing

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-49f0cc0
+ * @license Angular v21.1.4+sha-76d1a37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-49f0cc0');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-76d1a37');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.4+sha-49f0cc0",
+    version: "21.1.4+sha-76d1a37",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.1.4+sha-49f0cc0",
+    version: "21.1.4+sha-76d1a37",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.4+sha-49f0cc0",
+  version: "21.1.4+sha-76d1a37",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
