ea6c5a343b57bb98b729bd0870da798d5de0d8bd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 19:07:45 +0000

    d7ddebc ci: update ng-dev config and pullapprove

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-fe25c57
+ * @license Angular v22.0.0-next.0+sha-d7ddebc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-fe25c57');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-d7ddebc');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-fe25c57",
+    version: "22.0.0-next.0+sha-d7ddebc",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-fe25c57",
+    version: "22.0.0-next.0+sha-d7ddebc",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-fe25c57",
+  version: "22.0.0-next.0+sha-d7ddebc",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
