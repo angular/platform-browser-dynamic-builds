c0d2ca75ff0724d8486563f959bf9f3fcb07537a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-02 19:17:13 +0000

    a2d5048 docs: update doc for `provideZoneChangeDetection`

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-2206efa
+ * @license Angular v22.0.0-next.0+sha-a2d5048
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, Injector, Compiler, ViewEncapsulation, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-2206efa');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-a2d5048');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-2206efa",
+    version: "22.0.0-next.0+sha-a2d5048",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-2206efa",
+    version: "22.0.0-next.0+sha-a2d5048",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-2206efa",
+  version: "22.0.0-next.0+sha-a2d5048",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
