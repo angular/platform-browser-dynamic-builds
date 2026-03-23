849ebc36af4cdc6d3cc53ed7b3c7c7d9b340e156
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:43:12 +0000

    f46e8bd refactor(compiler): remove `fullTemplateTypeCheck` compiler option.

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-2615f35
+ * @license Angular v22.0.0-next.4+sha-f46e8bd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-2615f35');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-f46e8bd');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.4+sha-2615f35",
+    version: "22.0.0-next.4+sha-f46e8bd",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.4+sha-2615f35",
+    version: "22.0.0-next.4+sha-f46e8bd",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.4+sha-2615f35",
+  version: "22.0.0-next.4+sha-f46e8bd",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
