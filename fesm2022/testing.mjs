24b05f5b46e7860796223f7e0cf66abf7b0bbc57
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:45:45 +0000

    d268df6 build: remove unused `@nginfra/angular-linking` dependency

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-d4c085d
+ * @license Angular v22.0.0-next.5+sha-d268df6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.5+sha-d4c085d",
+    version: "22.0.0-next.5+sha-d268df6",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.0-next.5+sha-d4c085d",
+    version: "22.0.0-next.5+sha-d268df6",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.0-next.5+sha-d4c085d",
+    version: "22.0.0-next.5+sha-d268df6",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.5+sha-d4c085d",
+  version: "22.0.0-next.5+sha-d268df6",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
