d9c81fcf6176a8b011b5e9b6ecc7d2bf3dcee037
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 17:50:03 +0000

    358d2e6 docs: move Angular 19 to unsupported versions

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-71f8d48
+ * @license Angular v22.1.0-next.0+sha-358d2e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-71f8d48');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-358d2e6');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-71f8d48",
+    version: "22.1.0-next.0+sha-358d2e6",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-71f8d48",
+    version: "22.1.0-next.0+sha-358d2e6",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-71f8d48",
+  version: "22.1.0-next.0+sha-358d2e6",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
