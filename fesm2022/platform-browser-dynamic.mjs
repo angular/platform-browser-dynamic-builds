753ed729ae0553027e781870711d97eda669d259
Andrew Scott <atscott01@gmail.com>
2026-06-04 17:13:00 +0000

    c5293c4 refactor(core): Also throw an error on iframe attributes set to `undefined`

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-6bde84f
+ * @license Angular v22.1.0-next.0+sha-c5293c4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6bde84f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-c5293c4');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-6bde84f",
+    version: "22.1.0-next.0+sha-c5293c4",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.0-next.0+sha-6bde84f",
+    version: "22.1.0-next.0+sha-c5293c4",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.0-next.0+sha-6bde84f",
+  version: "22.1.0-next.0+sha-c5293c4",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
