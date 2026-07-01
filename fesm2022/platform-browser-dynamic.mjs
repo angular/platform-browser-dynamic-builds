8f872ba6029c142668f9e3dd934d0eb55e3188fb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 00:49:33 +0000

    5a693ba fix(core): reject dynamic script host elements

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-6bcce11
+ * @license Angular v21.2.17+sha-5a693ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-6bcce11');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-5a693ba');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.17+sha-6bcce11",
+    version: "21.2.17+sha-5a693ba",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.17+sha-6bcce11",
+    version: "21.2.17+sha-5a693ba",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.17+sha-6bcce11",
+  version: "21.2.17+sha-5a693ba",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-6bcce11
+ * @license Angular v21.2.17+sha-5a693ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.17+sha-6bcce11",
+    version: "21.2.17+sha-5a693ba",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.2.17+sha-6bcce11",
+    version: "21.2.17+sha-5a693ba",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.2.17+sha-6bcce11",
+    version: "21.2.17+sha-5a693ba",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.17+sha-6bcce11",
+  version: "21.2.17+sha-5a693ba",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
