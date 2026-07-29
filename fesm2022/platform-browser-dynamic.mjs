33dccff62ba7768568585eb7c46248bfc7876bc8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:54:57 +0000

    21eed5f docs: fix self-referential link in defer block description

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-9182253
+ * @license Angular v22.2.0-next.0+sha-21eed5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-9182253');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-21eed5f');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.0+sha-9182253",
+    version: "22.2.0-next.0+sha-21eed5f",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.0+sha-9182253",
+    version: "22.2.0-next.0+sha-21eed5f",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.0+sha-9182253",
+  version: "22.2.0-next.0+sha-21eed5f",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
