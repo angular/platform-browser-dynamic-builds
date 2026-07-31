7380f82fb1306df81d023110045b668161f9676c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 22:22:49 +0000

    caf6166 fix(http): run root interceptors in the terminal request chain

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-6afe6fa
+ * @license Angular v21.2.19+sha-caf6166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-6afe6fa');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-caf6166');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.19+sha-6afe6fa",
+    version: "21.2.19+sha-caf6166",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "21.2.19+sha-6afe6fa",
+    version: "21.2.19+sha-caf6166",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.19+sha-6afe6fa",
+  version: "21.2.19+sha-caf6166",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
