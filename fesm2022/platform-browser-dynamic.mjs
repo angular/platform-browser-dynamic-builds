a80fbebc989fda25df97c4637735acd59fb74a02
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:39:59 +0000

    c04931c fix(core): throw a descriptive error instead of crashing when a hydration node is missing

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-0cd243d
+ * @license Angular v22.1.2+sha-c04931c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-0cd243d');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-c04931c');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.2+sha-0cd243d",
+    version: "22.1.2+sha-c04931c",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.2+sha-0cd243d",
+    version: "22.1.2+sha-c04931c",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.2+sha-0cd243d",
+  version: "22.1.2+sha-c04931c",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
