a1e52788ef7fac5b2a1fdbbf6b0607d7675a35bd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 21:16:25 +0000

    38d0932 fix(forms): warn in dev mode when ngModel cannot reach parent NgForm across component boundary

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-1c2b057
+ * @license Angular v22.2.0-next.2+sha-38d0932
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-1c2b057');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-38d0932');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-1c2b057",
+    version: "22.2.0-next.2+sha-38d0932",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-1c2b057",
+    version: "22.2.0-next.2+sha-38d0932",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.2+sha-1c2b057",
+  version: "22.2.0-next.2+sha-38d0932",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
