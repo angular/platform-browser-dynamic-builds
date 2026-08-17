2f2099f9f28167323c6cba27f1e538c39de2600b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:41:57 +0000

    1c2b057 feat(docs-infra): add Angie to the roadmap illustration

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-5980138
+ * @license Angular v22.2.0-next.2+sha-1c2b057
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -15,7 +15,7 @@ const platformBrowserDynamicTesting = createPlatformFactory(platformBrowserDynam
 class BrowserDynamicTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-5980138",
+    version: "22.2.0-next.2+sha-1c2b057",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     deps: [],
@@ -23,14 +23,14 @@ class BrowserDynamicTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.2.0-next.2+sha-5980138",
+    version: "22.2.0-next.2+sha-1c2b057",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     exports: [BrowserTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.2.0-next.2+sha-5980138",
+    version: "22.2.0-next.2+sha-1c2b057",
     ngImport: i0,
     type: BrowserDynamicTestingModule,
     imports: [BrowserTestingModule]
@@ -38,7 +38,7 @@ class BrowserDynamicTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.2+sha-5980138",
+  version: "22.2.0-next.2+sha-1c2b057",
   ngImport: i0,
   type: BrowserDynamicTestingModule,
   decorators: [{
