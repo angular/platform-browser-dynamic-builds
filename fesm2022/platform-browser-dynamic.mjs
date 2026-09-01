0326f34b7ec16be6b0085a82001530bff43de5c3
Matthew Beck <me@mattrb.com>
2026-09-01 03:42:29 +0000

    d8e74e7 docs(docs-infra): improve update checkbox accessibility

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-87d38ec
+ * @license Angular v22.2.0-next.4+sha-d8e74e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-87d38ec');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-d8e74e7');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-next.4+sha-87d38ec",
+    version: "22.2.0-next.4+sha-d8e74e7",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-next.4+sha-87d38ec",
+    version: "22.2.0-next.4+sha-d8e74e7",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-next.4+sha-87d38ec",
+  version: "22.2.0-next.4+sha-d8e74e7",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
