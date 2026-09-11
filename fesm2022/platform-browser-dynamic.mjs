20b5e48b199675b65de1e421614449d573c2e608
Andrew Scott <atscott01@gmail.com>
2026-09-11 17:50:09 +0000

    17d6c1b fix(docs-infra): raise SSR fetch limit so the menubar guide prerenders

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-8a837f6
+ * @license Angular v22.1.6+sha-17d6c1b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-8a837f6');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-17d6c1b');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.6+sha-8a837f6",
+    version: "22.1.6+sha-17d6c1b",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.1.6+sha-8a837f6",
+    version: "22.1.6+sha-17d6c1b",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.6+sha-8a837f6",
+  version: "22.1.6+sha-17d6c1b",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
