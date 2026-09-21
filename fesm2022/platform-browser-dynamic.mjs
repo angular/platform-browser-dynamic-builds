74abc694108959145bb67328db60a77beb7e5ab6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:34:01 +0000

    7a58550 fix(forms): create controls before listeners

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-51f056f
+ * @license Angular v22.2.0-rc.0+sha-7a58550
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-51f056f');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-7a58550');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0-rc.0+sha-51f056f",
+    version: "22.2.0-rc.0+sha-7a58550",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0-rc.0+sha-51f056f",
+    version: "22.2.0-rc.0+sha-7a58550",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0-rc.0+sha-51f056f",
+  version: "22.2.0-rc.0+sha-7a58550",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
