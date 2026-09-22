1097a101aacb2d045c324689dc94d26d2818ad21
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 16:16:25 +0000

    8d8859c fix(core): don't use plain objects as maps for jsaction data

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-d5f409c
+ * @license Angular v22.3.0-next.0+sha-8d8859c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-d5f409c');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-8d8859c');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-d5f409c",
+    version: "22.3.0-next.0+sha-8d8859c",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-d5f409c",
+    version: "22.3.0-next.0+sha-8d8859c",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-d5f409c",
+  version: "22.3.0-next.0+sha-8d8859c",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
