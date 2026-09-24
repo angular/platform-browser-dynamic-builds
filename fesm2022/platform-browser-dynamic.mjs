492a02bd3c02619f511d74b55896e9609f37ce7e
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:45:14 +0000

    21851fc fix(core): add debugName on afterRenderEffect

--- fesm2022/platform-browser-dynamic.mjs
+++ fesm2022/platform-browser-dynamic.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-7f9c14d
+ * @license Angular v22.2.0+sha-21851fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -9,7 +9,7 @@ import { Version, ViewEncapsulation, Injector, Compiler, Injectable, createPlatf
 import { CompilerConfig, ResourceLoader } from '@angular/compiler';
 import { platformBrowser } from '@angular/platform-browser';
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-7f9c14d');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-21851fc');
 
 const COMPILER_PROVIDERS = [{
   provide: Compiler,
@@ -92,7 +92,7 @@ class ResourceLoaderImpl extends ResourceLoader {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.2.0+sha-7f9c14d",
+    version: "22.2.0+sha-21851fc",
     ngImport: i0,
     type: ResourceLoaderImpl,
     deps: null,
@@ -100,14 +100,14 @@ class ResourceLoaderImpl extends ResourceLoader {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.2.0+sha-7f9c14d",
+    version: "22.2.0+sha-21851fc",
     ngImport: i0,
     type: ResourceLoaderImpl
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.2.0+sha-7f9c14d",
+  version: "22.2.0+sha-21851fc",
   ngImport: i0,
   type: ResourceLoaderImpl,
   decorators: [{
